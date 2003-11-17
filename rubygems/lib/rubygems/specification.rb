a9a41727e43116fa65f2cccc277dcaa4fa511b4a
Richard Kilmer <rich@infoether.com>
2003-11-17 16:31:32 +0000

    rdoc added
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@35 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/lib/rubygems/specification.rb
+++ rubygems/lib/rubygems/specification.rb
@@ -1,5 +1,9 @@
 module Gem
-
+  
+  ##
+  # Available list of platforms for targeting Gem installations.
+  # Platform::RUBY is the default platform (pure Ruby Gem).
+  #
   module Platform
     RUBY = nil
     WIN32 = 'mswin32'
@@ -7,45 +11,91 @@ module Gem
     DARWIN = 'powerpc-darwin'
   end
   
+  ##
+  # The Specification class contains the metadata for a Gem.  A
+  # .gemspec file consists of the defintion of a Specification object
+  # and bootstrap code to build the Gem.
+  #
   class Specification
     @@list = []
     
+    ##
+    # A list of Specifcation instances that have been defined in this
+    # Ruby instance.
+    #
     def self.list
       @@list
     end
     
-    attr_accessor :rubygems_version, :name, :platform, :date, :summary, :require_paths # required
-    attr_reader :version # required
+    ##
+    # This attributes are required
+    #
+    attr_accessor :rubygems_version, :name, :platform, :date, :summary, :require_paths
+    attr_reader :version
     
+    ##
+    # These attributes are optional
+    #
     attr_accessor :autorequire, :author, :email, :homepage, :description, :files
     attr_accessor :rubyforge_project
     
-    attr_reader :dependencies, :requirements # lists
+    attr_reader :dependencies, :requirements
     
+    ##
+    # Runtime attributes (not persisted)
+    #
     attr_writer :loaded
     
+    ##
+    # Constructs instance of a Specification
+    #
     def initialize
       @date = Time.now
       @@list << self
       yield self if block_given?
     end
     
-    def rubygems_version
+    ##
+    # Marks the rubygems_version to the Gem::RubyGemsVersion
+    #
+    def mark_version
       @rubygems_version = RubyGemsVersion
     end
     
+    ##
+    # Returns dependency array
+    #
+    # return:: [Array] array of Gem::Dependency instances
+    #
     def dependencies
       @dependencies ||= []
     end
     
+    ##
+    # Returns the requirements arrays.  Requirements are text requirements
+    # that are output to the screen if a Gem could not be loaded for some
+    # reason.
+    #
+    # return:: [Array] array of Strings
+    #
     def requirements
       @requirements ||= []
     end
     
+    ##
+    # Returns if the Gem represented by the specification is loaded.
+    #
+    # return:: [Boolean] true if Gem is loaded
+    #
     def loaded?
       @loaded
     end
     
+    ##
+    # Sets the version of the Specification
+    #
+    # version:: [String or Gem::Version] The version
+    #
     def version=(version)
       unless version.respond_to? :version
         version = Version.new(version)
@@ -53,28 +103,56 @@ module Gem
       @version = version
     end
     
+    ##
+    # Helper method if the require path is singular
+    #
+    # path:: [String] The require path.
+    #
     def require_path=(path)
       @require_paths = [path]
     end
     
-    def add_dependency(gem, requirement=nil)
+    ##
+    # Adds a dependency to this Gem
+    #
+    # gem:: [String or Gem::Dependency] The Gem name/dependency.
+    # requirement:: [default="> 0.0.0"] The version requirement.
+    #
+    def add_dependency(gem, requirement="> 0.0.0")
       unless gem.respond_to?(:name) && gem.respond_to?(:version_requirement)
         gem = Dependency.new(gem, requirement)
       end
       dependencies << gem
     end
     
+    ##
+    # The full name (name-version) of this Gem
+    #
+    # return:: [String] The full name name-version
+    #
     def full_name
       "#{@name}-#{@version}"
     end
     
+    ##
+    # Checks if this Specification meets the requirement of the supplied
+    # dependency
+    # 
+    # dependency:: [Gem::Dependency] the dependency to check
+    # return:: [Boolean] true if dependency is met, otherwise false
+    #
     def satisfies_requirement?(dependency)
       return @name==dependency.name && 
         dependency.version_requirement.satisfied_by?(@version)
     end
     
+    ##
+    # Order the YAML properties
+    #
+    # return:: [Array] list of string attributes for YAML
+    #
     def to_yaml_properties
-      rubygems_version
+      mark_version
       result = ['@rubygems_version', '@name', '@version', '@date', '@platform', '@summary', '@require_paths']
       result << '@autorequire' if @autorequire
       result << '@author' if @author

9ac0e9149295f356f3aee2e6a7c3a4e22d0a904e
Chad Fowler <chad@chadfowler.com>
2003-11-23 01:53:27 +0000

    Generate MD5 checksum for gem and store it in the file.  Will be used to validate gem file before installation.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@66 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/lib/rubygems/builder.rb
+++ rubygems/lib/rubygems/builder.rb
@@ -5,7 +5,65 @@ module Gem
   # to produce a .gem file.
   #
   class Builder
+  require 'stringio'
+
+    ##
+    # Builder::FileContents is the file contents
+    #
+    class FileContents < StringIO
+      def add_ruby_header
+        self.puts <<-EOS
+        MD5SUM = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+        if $0 == __FILE__
+          require 'optparse'
+        
+          options = {}
+          ARGV.options do |opts|
+            opts.on_tail("--help", "show this message") {puts opts; exit}
+            opts.on('--dir=DIRNAME', "Installation directory for the Gem") {|options[:directory]|}
+            opts.on('--force', "Force Gem to intall, bypassing dependency checks") {|options[:force]|}
+            opts.parse!
+          end
+
+          require 'rubygems'
+          @directory = options[:directory] || Gem.dir  
+          @force = options[:force]
   
+          Gem::Installer.new(__FILE__).install(@force, @directory)      
+end
+
+__END__
+        EOS
+      end
+            
+      ##
+      # Reads the files listed in the specification and encodes
+      # them into the provided file (IO)
+      #
+      # file:: [IO] the file to write the encoded data into
+      #
+      def write_files_to(files)
+        require 'zlib'
+        file_header = []
+        files.each do |file_name|
+          next if File.directory? file_name
+          file_header << { "path" => file_name,
+                          "size" => File.size(file_name),
+                          "mode" => File.stat(file_name).mode & 0777
+          }
+        end
+        self.puts file_header.to_yaml
+        file_header.each do |entry|
+          data = [Zlib::Deflate.deflate(File.read(entry['path']))].pack("m")
+          self.puts "---"
+          self.puts data
+        end
+      end
+      def md5
+        MD5.md5(string)
+      end    
+    end
+
     ##
     # Constructs a builder instance for the provided specification
     #
@@ -20,60 +78,29 @@ module Gem
     #
     def build
       require 'yaml'
-      File.open(@spec.full_name+".gem", "w") do |file|
-        ruby_header(file)
-        file.puts @spec.to_yaml
-        write_files_to(file)
-        puts "Successfully built RubyGem\n  Name: #{@spec.name}\n  Version: #{@spec.version}\n  File: #{@spec.full_name+'.gem'}"
-      end
+      require 'md5'
+      require 'stringio' 
+      
+      file_name = @spec.full_name+".gem"
+      file_contents = FileContents.new
+      
+      file_contents.add_ruby_header
+      file_contents.puts(@spec.to_yaml)
+      file_contents.write_files_to(@spec.files)
+      puts success
+      md5 = file_contents.md5
+      File.open(file_name, "w") do |file|
+        file.write(file_contents.string.gsub(/MD5SUM =.*$/, "MD5SUM = \"#{md5.to_s}\""))
+      end 
     end
     
-    ##
-    # Reads the files listed in the specification and encodes
-    # them into the provided file (IO)
-    #
-    # file:: [IO] the file to write the encoded data into
-    #
-    def write_files_to(file)
-      require 'zlib'
-      file_header = []
-      @spec.files.each do |file_name|
-        next if File.directory? file_name
-        file_header << { "path" => file_name,
-                         "size" => File.size(file_name),
-                         "mode" => File.stat(file_name).mode & 0777
-                       }
-      end
-      file.puts file_header.to_yaml
-      file_header.each do |entry|
-        data = [Zlib::Deflate.deflate(File.read(entry['path']))].pack("m")
-        file.puts "---"
-        file.puts data
-      end
-    end
-    
-    def ruby_header(file)
-      file.puts <<-EOS
-if $0 == __FILE__
-  require 'optparse'
-
-  options = {}
-  ARGV.options do |opts|
-    opts.on_tail("--help", "show this message") {puts opts; exit}
-    opts.on('--dir=DIRNAME', "Installation directory for the Gem") {|options[:directory]|}
-    opts.on('--force', "Force Gem to intall, bypassing dependency checks") {|options[:force]|}
-    opts.parse!
-  end
-
-  require 'rubygems'
-  @directory = options[:directory] || Gem.dir  
-  @force = options[:force]
-  
-  Gem::Installer.new(__FILE__).install(@force, @directory)      
-end
-__END__
-EOS
+    def success
+      <<-EOM
+  Successfully built RubyGem
+  Name: #{@spec.name}
+  Version: #{@spec.version}
+  File: #{@spec.full_name+'.gem'}"
+EOM
     end
   end
-  
 end

b87d28f61badf5a1da58d97b9d4591383942c4ef
Chad Fowler <chad@chadfowler.com>
2003-12-03 13:05:25 +0000

    Gavin's patch for installing the files in bin/ on rubygems installation.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@92 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/install.rb
+++ rubygems/install.rb
@@ -11,34 +11,55 @@ $archdir = File.join($libdir, CONFIG["arch"])
 $site_libdir = $:.find {|x| x =~ /site_ruby$/}
 if !$site_libdir
   $site_libdir = File.join($libdir, "site_ruby")
-elsif $site_libdir !~ Regexp.quote($version)
+elsif $site_libdir !~ Regexp.new(Regexp.quote($version))
   $site_libdir = File.join($site_libdir, $version)
 end
 
 def install_rb(srcdir = nil)
+  ### Install 'lib' files.
+
   libdir = "lib"
   libdir = File.join(srcdir, libdir) if srcdir
-  path = []
-  dir = []
+  paths = []
+  dirs = []
   Find.find(libdir) do |f|
     next unless FileTest.file?(f)
     next if (f = f[libdir.length+1..-1]) == nil
     next if (/CVS$/ =~ File.dirname(f))
-    path.push f
-    dir |= [File.dirname(f)]
+    paths.push f
+    dirs |= [File.dirname(f)]
   end
-  for f in dir
+
+  # Create the necessary directories.
+  for f in dirs
     next if f == "."
     next if f == "CVS"
     File::makedirs(File.join($site_libdir, f))
   end
-  for f in path
+
+  # Install the files.
+  for f in paths
     File::install(File.join("lib", f), File.join($site_libdir, f), 0644, true)
   end
   gem_dir = File.join(Config::CONFIG['libdir'], 'ruby', 'gems', Config::CONFIG['ruby_version'])
   ["specifications", "cache"].each do |subdir|
     File::makedirs(File.join(gem_dir, subdir))
   end
+
+  ## Install the 'bin' files.
+
+  bindir = CONFIG['bindir']
+  is_windows_platform = CONFIG["arch"] =~ /dos|win32/i
+  Find.find('bin') do |f|
+    next if f =~ /\bCVS\b/
+    next if f =~ /\.cmd$/ and not is_windows_platform
+    next if FileTest.directory?(f)
+    source = f
+    target = File.join(bindir, File.basename(f))
+    File::install(source, target, 0755, true)
+  end
+
+  ## Install the 'sources' package bundled with RubyGems.
 
   Dir.chdir("packages/sources")
     load("sources.gemspec")

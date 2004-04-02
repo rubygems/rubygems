c61cabd5b8b2f8a290e3d2aab27c4db1a74a1755
Chad Fowler <chad@chadfowler.com>
2004-04-02 12:34:22 +0000

    Some things I forgot to cvs add earlier.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@172 3d4018f9-ac1a-0410-99e9-8a154d859a19

---
+++ rubygems/test/test_format.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'test/unit'
+require 'stringio'
+
+class TestFormat < Test::Unit::TestCase
+  def setup
+    require File.dirname(__FILE__) + "/simple_gem.rb"
+    @simple_gem = SIMPLE_GEM
+  end
+
+  def test_from_io_returns_format_object
+    format = Gem::Format.from_io(StringIO.new(@simple_gem))
+
+    assert_equal(Gem::Format, format.class)
+    assert_equal(Gem::Specification, format.spec.class) 
+    # simple_gem.rb's gem has 3 files in it
+    assert_equal(3, format.file_entries.size)
+  end
+
+  def test_garbled_gem_throws_format_exception
+    assert_raise(Gem::FormatException) {
+      # subtly bogus input
+      Gem::Format.from_io(StringIO.new(@simple_gem.upcase))
+    }
+    assert_raise(Gem::FormatException) {
+      # Totally bogus input
+      Gem::Format.from_io(StringIO.new(@simple_gem.reverse))
+    }
+    assert_raise(Gem::FormatException) {
+      # This was intentionally screws up YAML parsing.
+      Gem::Format.from_io(StringIO.new(@simple_gem.gsub(/:/, "boom")))
+    }
+  end
+
+  def test_passing_nonexistent_files_throws_sensible_exception
+    assert_raise(Gem::FormatException) {
+      Gem::Format.from_file_by_path("/this/path/almost/definitely/will/not/exist")
+    }
+  end
+end

f3b516cde75791703f60e499c64d74a316652130
Jim Weirich <jim.weirich@gmail.com>
2004-04-05 04:48:20 +0000

    Moved session gem detection to insure_session file. Modified functional tests to use gem command. Removed some directory stuff because rake guarantees the starting directory. Added test_info functional test. Added test_build functional test. Added assert status.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@191 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/functional.rb
+++ rubygems/test/functional.rb
@@ -1,50 +1,89 @@
 require 'test/unit'
 require 'rubygems'
+require 'yaml'
 
-begin
-  require_gem 'session'
-rescue LoadError => e
-  print "Required Gem 'Session' missing.  Install now from RubyGems distribution? [Yn]"
-  answer = gets
-  if(answer =~ /^y/i || answer =~ /^[^a-zA-Z0-9]$/) then
-    path_to_gem = File.join(File.dirname(File.expand_path(__FILE__)), "..", "redist", "session.gem")
-    Gem::Installer.new(path_to_gem).install
-    require_gem 'session'
-  else
-    puts "Test cancelled...quitting"
-    exit(1)
-  end
-end
-
+require 'test/insure_session'
 
 class FunctionalTest < Test::Unit::TestCase
   def setup
-    @shell = Session::Shell.new
-    Dir.chdir(File.join(
-      File.dirname(File.expand_path(__FILE__)),
-      "..")
-      )
+    @gem_path = File.expand_path("bin/gem")
+    lib_path = File.expand_path("lib")
+    @ruby_options = "-I#{lib_path} -I."
   end
 
   def test_gem_help
-    out,err = @shell.execute "ruby -I lib bin/gem --help"
-    assert_match(/Usage:/, out)
+    gem '--help'
+    assert_match(/Usage:/, @out)
+    assert_status
   end
 
   def test_gem_no_args_shows_help
-    out,err = @shell.execute "ruby -I lib bin/gem"
-    assert_match(/Usage:/, out)
+    gem
+    assert_match(/Usage:/, @out)
+    assert_status 1
   end
 
-  def test_bogus_source_hoses_up_remote_search_but_gem_command_gives_decent_error_message
-    out,err = @shell.execute "ruby -I lib -rtest/bogussources bin/gem --remote --search=asdf"
-    assert_match(/^Error fetching remote gem cache/, err)
+  def test_info
+    gem '--rubygems-info'
+    assert_match /VERSION:\s+\d+\.\d+$/, @out
+    assert_match /INSTALLATION DIRECTORY:/, @out
+    assert_match /GEM PATH:/, @out
+    assert_match /REMOTE SOURCES:/, @out
+    assert_status
+  end
+
+  ONEDIR = "test/data/one"
+  ONENAME = "one-ruby-0.0.1.gem"
+  ONEGEM = "#{ONEDIR}/#{ONENAME}"
+
+  def test_build
+    FileUtils.rm_f ONEGEM
+    Dir.chdir(ONEDIR) do
+      gem "-b one.gemspec"
+    end
+    assert File.exist?(ONEGEM), "Gem file (#{ONEGEM}) should exist"
+    assert_match /Successfully built RubyGem/, @out
+    assert_match /Name: one$/, @out
+    assert_match /Version: 0.0.1$/, @out
+    assert_match /File: #{ONENAME}/, @out
+    spec = read_gem_file(ONEGEM)
+    assert_equal "one", spec.name
+    assert_equal "Test GEM One", spec.summary
   end
 
   def test_bogus_source_hoses_up_remote_install_but_gem_command_gives_decent_error_message
-    out,err = @shell.execute "ruby -I lib -rtest/bogussources bin/gem --remote --install=asdf"
-    assert_match(/^Error fetching remote gem cache/, err)
+    @ruby_options << " -rtest/bogussources"
+    gem "--remote --install=asdf"
+    assert_match(/^Error fetching remote gem cache/, @err)
+    assert_status 1
   end
 
+  private
+
+  def gem(options="")
+    shell = Session::Shell.new
+    command = "ruby #{@ruby_options} #{@gem_path} #{options}"
+    puts "COMMAND: [#{command}]" if @verbose
+    @out, @err = shell.execute command
+    @status = shell.exit_status
+    puts "STATUS:  [#{@status}]" if @verbose
+    puts "OUTPUT:  [#{@out}]" if @verbose
+    puts "ERROR:   [#{@err}]" if @verbose
+    puts "PWD:     [#{Dir.pwd}]" if @verbose
+    shell.close
+  end
+
+  def assert_status(expected_status=0)
+    assert_equal expected_status, @status
+  end
+
+  def read_gem_file(filename)
+    open(filename) { |f|
+      while line = f.gets
+	break if line == "__END__\n"
+      end
+      YAML.load(f)
+    }
+  end
 
 end

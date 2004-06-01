0d2d9acbc4cc59e267fa6853bc892c9433033f79
Jim Weirich <jim.weirich@gmail.com>
2004-06-01 02:59:25 +0000

    Reworked the version tests to not depend on dynamic test method generation.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@268 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/test_version_comparison.rb
+++ rubygems/test/test_version_comparison.rb
@@ -1,105 +1,120 @@
 require 'test/unit'
 require 'rubygems'
 
+module VersionTestingHelp
+  def assert_adequate(version, requirement)
+    ver = Gem::Version.new(version)
+    req = Gem::Version::Requirement.new(requirement)
+    assert req.satisfied_by?(ver),
+      "Version #{version} should be adequate for Requirement #{requirement}"
+  end
+
+  def assert_inadequate(version, requirement)
+    ver = Gem::Version.new(version)
+    req = Gem::Version::Requirement.new(requirement)
+    assert ! req.satisfied_by?(ver),
+      "Version #{version} should not be adequate for Requirement #{requirement}"
+  end
+end
+
+
 class Versions < Test::Unit::TestCase
+  include VersionTestingHelp
 
-  WRONG_NUMS = [
-    "blah", "1.3.a", "1.3.5."
-  ]
-
-  WRONG_REQS = [
-    ">>> 1.3.5", "> blah"
-  ]
-
-  OK = [
-                # Required	Given
-        [	"= 0.2.33",	"0.2.33"	],
-        [	"> 0.2.33",	"0.2.34"	],
-        [	"= 1.0",	"1.0"		],
-        [	"1.0",	"1.0"		],
-        [	"> 1.8.0",	"1.8.2"		],
-        [	"> 1.111", 	"1.112"		],
-        [	"> 0.0.0", 	"0.2"		],
-        [	"> 0.0.0", 	"0.0.0.0.0.2"	],
-        [	"> 0.0.0.1", 	"0.0.1.0"	],
-        [	"> 9.3.2", 	"10.3.2"	],
-        [	"= 1.0", 	"1.0.0.0"	],
-        [	"!= 9.3.4", 	"10.3.2"	],
-        [	"> 9.3.2", 	"10.3.2"	],
-        [	"> 9.3.2", 	"10.3.2"	],
-        [	">= 9.3.2", 	" 9.3.2"        ],
-        [	">= 9.3.2", 	"9.3.2 "        ],
-        [       "= 0",          ""              ],
-        [       "< 0.1",        ""              ], 
-        [       "< 0.1 ",        "  "           ], 
-        [       " <  0.1",        ""            ], 
-        [       "=",            "0"             ], 
-        [       ">=",            "0"            ], 
-        [       "<=",            "0"            ], 
-  ]
-
-  BAD = [
-             # Required      Given
-         [      "> 0.1",        ""              ], 
-         [      "!= 1.2.3",      "1.2.3"        ],
-         [      "!= 1.02.3",      "1.2.003.0.0" ],
-         [      "< 1.2.3",      "4.5.6"         ],
-         [	"> 1.1",	"1.0"		],
-         [      ">",            "0"             ], 
-         [      "<",            "0"             ], 
-         [      "= 0.1",        ""              ], 
-         [	"> 1.1.1",	"1.1.1"		],
-         [	"= 1.1",	"1.2"		],
-         [	"= 1.1",	"1.40"		],
-         [	"= 1.40",	"1.3"		],
-         [	"<= 9.3.2", 	"9.3.3"	        ],
-         [	">= 9.3.2", 	"9.3.1"	        ],
-         [	"<= 9.3.2", 	"9.3.03"        ],
-         [	"= 1.0", 	"1.0.0.1"	],
-  ]
-
-  i = 0
-  WRONG_NUMS.each do |wn|
-    class_eval <<-EOE
-    def test_wn_#{i}
-      assert_raises(ArgumentError) { Gem::Version.new(\"#{wn}\") }
+  def test_ok
+      assert_adequate( "0.2.33",      "= 0.2.33")
+      assert_adequate( "0.2.34",      "> 0.2.33")
+      assert_adequate( "1.0",         "= 1.0")
+      assert_adequate( "1.0",         "1.0")
+      assert_adequate( "1.8.2",       "> 1.8.0")
+      assert_adequate( "1.112",       "> 1.111")
+      assert_adequate( "0.2",         "> 0.0.0")
+      assert_adequate( "0.0.0.0.0.2", "> 0.0.0")
+      assert_adequate( "0.0.1.0",     "> 0.0.0.1")
+      assert_adequate( "10.3.2",      "> 9.3.2")
+      assert_adequate( "1.0.0.0",     "= 1.0")
+      assert_adequate( "10.3.2",      "!= 9.3.4")
+      assert_adequate( "10.3.2",      "> 9.3.2")
+      assert_adequate( "10.3.2",      "> 9.3.2")
+      assert_adequate( " 9.3.2",      ">= 9.3.2")
+      assert_adequate( "9.3.2 ",      ">= 9.3.2")
+      assert_adequate( "",            "= 0")
+      assert_adequate( "",            "< 0.1")
+      assert_adequate( "  ",          "< 0.1 ")
+      assert_adequate( "",            " <  0.1")
+      assert_adequate( "0",           "=")
+      assert_adequate( "0",           ">=")
+      assert_adequate( "0",           "<=")
     end
-EOE
-  i += 1
+
+    def test_bad
+      assert_inadequate( "",            "> 0.1")
+      assert_inadequate( "1.2.3",       "!= 1.2.3")
+      assert_inadequate( "1.2.003.0.0", "!= 1.02.3")
+      assert_inadequate( "4.5.6",       "< 1.2.3")
+      assert_inadequate( "1.0",         "> 1.1")
+      assert_inadequate( "0",           ">")
+      assert_inadequate( "0",           "<")
+      assert_inadequate( "",            "= 0.1")
+      assert_inadequate( "1.1.1",       "> 1.1.1")
+      assert_inadequate( "1.2",         "= 1.1")
+      assert_inadequate( "1.40",        "= 1.1")
+      assert_inadequate( "1.3",         "= 1.40")
+      assert_inadequate( "9.3.3",       "<= 9.3.2")
+      assert_inadequate( "9.3.1",       ">= 9.3.2")
+      assert_inadequate( "9.3.03",      "<= 9.3.2")
+      assert_inadequate( "1.0.0.1",     "= 1.0")
+    end
+
+    def test_illformed_version
+      [ "blah", "1.3.a", "1.3.5." ].each do |wn|
+	assert_raises(ArgumentError) { Gem::Version.new(wn) }
+      end
+    end
+
+    def test_illformed_requirements
+      [ ">>> 1.3.5", "> blah" ].each do |rq|
+	assert_raises(ArgumentError, "req [#{rq}] should fail") {
+	  Gem::Version::Requirement.new(rq)
+	}
+      end
+    end
 end
 
-  i = 0
-  WRONG_REQS.each do |wn|
-    class_eval <<-EOE
-    def test_wn_#{i}
-      assert_raises(ArgumentError) { Gem::Version::Requirement.new(\"#{wn}\") }
-    end
-EOE
-  i += 1
-end
-
-  i = 0
-  OK.each do |ok|
-    class_eval <<-EOE
-    def test_ok_#{i}   
-      r = Gem::Version::Requirement.new(\"#{ok[0]}\")
-      v = Gem::Version.new(\"#{ok[1]}\")
-      assert(r.satisfied_by?(v),"Bad: \#{r}, \#{v}")
-    end
-EOE
-  i += 1
+class TestVersion < Test::Unit::TestCase
+  def test_to_s
+    v = Gem::Version.new("5.2.4")
+    assert_equal "5.2.4", v.to_s
   end
-
-  i = 0
-  BAD.each do |bad|
-    class_eval <<-EOE
-    def test_bad_#{i}   
-      r = Gem::Version::Requirement.new(\"#{bad[0]}\")
-      v = Gem::Version.new(\"#{bad[1]}\")
-      assert(!r.satisfied_by?(v), "Good: \#{r.to_ints}, \#{v.to_ints}")
-    end
-EOE
-  i += 1
+  def test_bump
+    v = Gem::Version.new("5.2.4")
+    assert_equal "5.3", v.bump.to_s
+  end
+  def test_bump_one_level
+    v = Gem::Version.new("5")
+    assert_equal "6", v.bump.to_s
   end
 end
+    
 
+class TestExtendedVersionComparisons < Test::Unit::TestCase
+  include VersionTestingHelp
+
+  def test_multiple
+    req = [">= 1.4", "<= 1.6", "!= 1.5"]
+    assert_inadequate("1.3", req)
+    assert_adequate(  "1.4", req)
+    assert_inadequate("1.5", req)
+    assert_adequate(  "1.6", req)
+    assert_inadequate("1.7", req)
+    assert_inadequate("2.0", req)
+  end
+
+  def test_boxed
+    assert_inadequate("1.3", ">* 1.4")
+    assert_adequate(  "1.4", ">* 1.4")
+    assert_adequate(  "1.5", ">* 1.4")
+    assert_inadequate("2.0", ">* 1.4")
+  end
+
+end

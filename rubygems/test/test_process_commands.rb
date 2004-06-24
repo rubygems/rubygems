846308574d1cbf65017a41e8ed5f195e2476e525
Jim Weirich <jim.weirich@gmail.com>
2004-06-24 03:38:41 +0000

    Removd old capture way of doing user interfaces.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@327 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/test_process_commands.rb
+++ rubygems/test/test_process_commands.rb
@@ -3,43 +3,27 @@ $:.unshift '../lib'
 require 'rubygems'
 Gem::manage_gems
 
+require 'test/mockgemui'
+
 class TestProcessCommands < Test::Unit::TestCase
+  include Gem::DefaultUserInteraction
 
   def setup
-    @ui = Gem::UserInteraction.capture
     @cmd_manager = Gem::CommandManager.instance
   end
 
-  def reset_ui
-    @error = ""
-    @warning = ""
-    @output = ""
-    
-    @ui.on_alert_warning do |message, question|
-      @warning << message.to_s
-    end
-    
-    @ui.on_alert_error do |message, question|
-      @error << message.to_s
-    end
-    
-    @ui.on_say do |statement|
-      @output << statement.to_s
-    end
-  end
-
-  
   def test_install_command
-    #fail to specify a name
-    reset_ui
-    @cmd_manager.process_args "install"
-    assert_match /specify a gem name/, @error
+    use_ui(MockGemUi.new) do
+      @cmd_manager.process_args "install"
+      assert_match /specify a gem name/, ui.error
+    end
   end
 
   def test_query_command
-    reset_ui
-    @cmd_manager.process_args "query"
-    puts @output
+    use_ui(MockGemUi.new) do
+      @cmd_manager.process_args "query"
+      puts ui.output
+    end
   end
   
   

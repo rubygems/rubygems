ed4287fa81ee72ec3baf2cd08be6cd4129136848
Jim Weirich <jim.weirich@gmail.com>
2004-06-24 03:38:37 +0000

    Changed from old UI system to new Object based UI.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@325 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/test_command.rb
+++ rubygems/test/test_command.rb
@@ -3,15 +3,19 @@
 require 'test/unit'
 require 'rubygems/command'
 require 'rubygems/cmd_manager'
-require 'test/user_capture'
+require 'rubygems/user_interaction'
+class Noop
+  def method_missing(sym, *args, &block)
+  end
+end
 
 class TestCommand < Test::Unit::TestCase
-  include UserCapture
+  include Gem::DefaultUserInteraction
 
   def setup 
     opt_list = [ [ ['-x', '--exe', 'Execute'], lambda do @xopt = true end] ]
     @cmd = Gem::Command.new("doit", "summary", opt_list)
-    reset_ui
+    self.ui = Noop.new
   end
 
   def test_basic_accessors
@@ -46,40 +50,46 @@ class TestCommand < Test::Unit::TestCase
   end
   
   def test_invode_with_bad_options
-    @cmd.when_invoked do true end
-    @cmd.invoke('-zzz')
-    assert_match /invalid option:/, @error
-    assert @terminated, "Should have terminated app"
+    use_ui(MockGemUi.new) do
+      @cmd.when_invoked do true end
+      @cmd.invoke('-zzz')
+      assert_match /invalid option:/, ui.error
+      assert ui.terminated?, "Should have terminated app"
+    end
   end
 
   def test_overlapping_common_and_local_options
-    @cmd.add_option('-x', '--zip', 'BAD!') do end
-    @cmd.add_option('-z', '--exe', 'BAD!') do end
-    @cmd.add_option('-x', '--exe', 'BAD!') do end
-    @cmd.when_invoked do |opts| false end
-    @cmd.invoke('-x')
-    md = @output =~ /Common.*-exe/m
-    assert ! @xopt, "Should not do xopt"
-    assert_nil md, "Should not have common options"
+    use_ui(MockGemUi.new) do
+      @cmd.add_option('-x', '--zip', 'BAD!') do end
+      @cmd.add_option('-z', '--exe', 'BAD!') do end
+      @cmd.add_option('-x', '--exe', 'BAD!') do end
+      @cmd.when_invoked do |opts| false end
+      @cmd.invoke('-x')
+      md = ui.output =~ /Common.*-exe/m
+      assert ! @xopt, "Should not do xopt"
+      assert_nil md, "Should not have common options"
+    end
   end
 
   # Returning false from the command handler invokes the usage output.
   def test_invoke_with_help
-    @cmd.add_option('-h', '--help [COMMAND]', 'Get help on COMMAND') do |value, options|
-      options[:help] = true
+    use_ui(MockGemUi.new) do
+      @cmd.add_option('-h', '--help [COMMAND]', 'Get help on COMMAND') do |value, options|
+	options[:help] = true
+      end
+      @cmd.when_invoked do |opts| false end
+      @cmd.invoke
+      assert_match /Usage/, ui.output
+      assert_match /gem doit/, ui.output
+      assert_match /\[options\]/, ui.output
+      assert_match /-h/, ui.output
+      assert_match /--help \[COMMAND\]/, ui.output
+      assert_match /Get help on COMMAND/, ui.output
+      assert_match /-x/, ui.output
+      assert_match /--exe/, ui.output
+      assert_match /Execute/, ui.output
+      assert_match /Common Options:/, ui.output
     end
-    @cmd.when_invoked do |opts| false end
-    @cmd.invoke
-    assert_match /Usage/, @output
-    assert_match /gem doit/, @output
-    assert_match /\[options\]/, @output
-    assert_match /-h/, @output
-    assert_match /--help \[COMMAND\]/, @output
-    assert_match /Get help on COMMAND/, @output
-    assert_match /-x/, @output
-    assert_match /--exe/, @output
-    assert_match /Execute/, @output
-    assert_match /Common Options:/, @output
   end
 
   def test_defaults

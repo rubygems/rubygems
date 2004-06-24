2b407f207b6b519069d8ef30a75872c043ba1f22
Jim Weirich <jim.weirich@gmail.com>
2004-06-24 03:38:35 +0000

    Added an object based UI system instead of the ackward when_invoked system.  The old UI structure was a bit flakey.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@324 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/lib/rubygems/user_interaction.rb
+++ rubygems/lib/rubygems/user_interaction.rb
@@ -1,126 +1,148 @@
 module Gem
-  module UserInteraction
 
-    def choose_from_list(question, list)
-      puts question
-      list.each_with_index do |item, index|
-        puts " #{index+1}. #{item}"
+  ##
+  # Module that defines the default UserInteraction.  Any class
+  # including this module will have access to the +ui+ method that
+  # returns the default UI.
+  module DefaultUserInteraction
+
+    # Return the default UI.
+    def ui
+      DefaultUserInteraction.ui
+    end
+
+    # Set the default UI.  If the default UI is never explicity set, a
+    # simple console based UserInteraction will be used automatically.
+    def ui=(new_ui)
+      DefaultUserInteraction.ui = new_ui
+    end
+
+    def use_ui(new_ui, &block)
+      DefaultUserInteraction.use_ui(new_ui, &block)
+    end
+
+    # The default UI is a class variable of the singleton class for
+    # this module.
+    class << self
+      def ui
+	@ui ||= Gem::ConsoleUI.new
       end
-      print "> "
-      result = STDIN.gets.strip.to_i - 1
+      def ui=(new_ui)
+	@ui = new_ui
+      end
+      def use_ui(new_ui)
+	old_ui = @ui
+	@ui = new_ui
+	yield
+      ensure
+	@ui = old_ui
+      end
+    end
+  end
+
+  ##
+  # Make the default UI accessable without the "ui." prefix.  Classes
+  # including this module may use the interaction methods on the
+  # default UI directly.  Classes may also reference the +ui+ and
+  # <tt>ui=</tt> methods.
+  #
+  # Example:
+  #
+  #   class X
+  #     include Gem::UserInteraction
+  #
+  #     def get_answer
+  #       n = ask("What is the meaning of life?")
+  #     end
+  #   end
+  module UserInteraction
+    include DefaultUserInteraction
+    [
+      :choose_from_list, :ask, :say, :alert, :alert_warning,
+      :alert_error, :terminate_interaction!, :terminate_interaction
+    ].each do |methname|
+      class_eval %{
+        def #{methname}(*args)
+          ui.#{methname}(*args)
+        end
+      }
+    end    
+  end
+
+  ##
+  # StreamUI implements a simple stream based user interface.
+  class StreamUI
+    def initialize(in_stream, out_stream, err_stream=STDERR)
+      @ins = in_stream
+      @outs = out_stream
+      @errs = err_stream
+    end
+    
+    # Choose from a list of options.  +question+ is a prompt displayed
+    # above the list.  +list+ is a list of option strings.  Returns
+    # the pair [option_name, option_index].
+    def choose_from_list(question, list)
+      @outs.puts question
+      list.each_with_index do |item, index|
+        @outs.puts " #{index+1}. #{item}"
+      end
+      @outs.print "> "
+      result = @ins.gets.strip.to_i - 1
       return list[result], result
     end
     
+    # Ask a question.  Returns an answer.  
     def ask(question)
-      print(question)
-      STDIN.gets
+      @outs.print(question + "  ")
+      result = @ins.gets
+      result.chomp! if result
+      result
     end
     
+    # Display a statement.
     def say(statement="")
-      puts statement
+      @outs.puts statement
     end
     
+    # Display an informational alert.
     def alert(statement, question=nil)
-      puts "INFO:\n#{statement}"
+      @outs.puts "INFO:\n#{statement}"
       return ask(question) if question 
     end
     
+    # Display a warning in a location expected to get error messages.
     def alert_warning(statement, question=nil)
-      STDERR.puts "WARNING:\n#{statement}"
+      @errs.puts "WARNING:\n#{statement}"
       ask(question) if question 
     end
     
+    # Display an error message in a location expected to get error
+    # messages.
     def alert_error(statement, question=nil)
-      STDERR.puts "ERROR:\n#{statement}"
+      @errs.puts "ERROR:\n#{statement}"
       ask(question) if question
     end
 
+    # Terminate the application immediately without running any exit
+    # handlers.
     def terminate_interaction!(status=-1)
       exit!(status)
     end
     
+    # Terminate the appliation normally, running any exit handlers
+    # that might have been defined.
     def terminate_interaction(status=0)
       exit(status)
     end
+  end
 
-    def self.capture
-      Capture.new
-    end
-        
-    ##
-    # The following class lets you capture IO
-    #
-    class Capture
-      def initialize
-        [:ask, :say, :choose_from_list, :alert, :alert_error, 
-         :alert_warning, :terminate_interaction!, :terminate_interaction].each do |m|
-          UserInteraction.send(:define_method, m, &method(m))
-        end
-      end
-      
-      def on_choose_from_list(&block)
-        @on_choose_from_list = block
-      end
-      
-      def on_ask(&block)
-        @on_ask = block
-      end
-      
-      def on_say(&block)
-        @on_say = block
-      end
-      
-      def on_alert(&block)
-        @on_alert = block
-      end
-      
-      def on_alert_warning(&block)
-        @on_alert_warning = block
-      end
-      
-      def on_alert_error(&block)
-        @on_alert_error = block
-      end
-      
-      def on_terminate_interaction!(&block)
-        @on_terminate_interaction_bang = block
-      end
 
-      def on_terminate_interaction(&block)
-        @on_terminate_interaction = block
-      end
-      
-      def choose_from_list(question, list)
-        @on_choose_from_list.call(question, list) if @on_choose_from_list
-      end
-      
-      def ask(question)
-        @on_ask.call(question) if @on_ask
-      end
-      
-      def say(statement="")
-        @on_say.call(statement) if @on_say
-      end
-      
-      def alert(statement, question=nil)
-        @on_alert.call(statement, question) if @on_alert
-      end
-      
-      def alert_warning(statement, question=nil)
-        @on_alert_warning.call(statement, question) if @on_alert_warning
-      end
-      
-      def alert_error(statement, question=nil)
-        @on_alert_error.call(statement, question) if @on_alert_error
-      end
-
-      def terminate_interaction!(status=-1)
-        @on_terminate_interaction_bang.call(status) if @on_terminate_interaction_bang
-      end
-      
-      def terminate_interaction(status=0)
-        @on_terminate_interaction.call(status) if @on_terminate_interaction
-      end      
+  ##
+  # Subclass of StreamUI that instantiates the user interaction using
+  # standard in, out and error.
+  class ConsoleUI < StreamUI
+    def initialize
+      super(STDIN, STDOUT, STDERR)
     end
   end
 end

3f6f5a526c670c719f202c36554a623ece85bf8d
Jim Weirich <jim.weirich@gmail.com>
2004-06-26 17:59:43 +0000

    added the gemdoc script and data.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@349 3d4018f9-ac1a-0410-99e9-8a154d859a19

---
+++ rubygems/scripts/gemdoc.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+Gem.manage_gems
+require 'rubygems/user_interaction'
+
+include Gem::DefaultUserInteraction
+
+class CaptureSay
+  attr_reader :string
+  def initialize
+    @string = ''
+  end
+  def say(msg)
+    @string << msg << "\n"
+  end
+end
+
+def pre(cmd, opts)
+  puts "<pre>"
+  cmd.invoke opts
+  puts "</pre>"
+end
+
+
+gm = Gem::CommandManager.instance
+hcmd = gm['help']
+
+while line = gets
+  if line =~ /^!/
+    cmd, arg = line.split
+    if cmd == "!usage"
+      begin
+	cmdobj = gm[arg]
+	pre(cmdobj, "--help")
+      rescue NoMethodError
+	puts "Usage of command #{arg} failed"
+      end
+    elsif cmd == "!toc"
+      cs = CaptureSay.new
+      use_ui(cs) do
+	gm['help'].invoke 'commands'
+      end
+      out = cs.string.gsub(/^  gem (\S+)(.*)$/) {
+	'  gem ' + $1 + $2 +
+	  ' [http://rubygems.rubyforge.org/wiki/wiki.pl?GemReference#' +
+	  $1 + " goto]"
+      }
+      out.gsub!(/^/, " ")
+      puts out
+    elsif cmd == "!version"
+      puts Gem::RubyGemsPackageVersion
+    end
+  else
+    puts line
+  end
+end

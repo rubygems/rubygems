019774436b9d073c94548c1ff7f3fccad1faef68
Jim Weirich <jim.weirich@gmail.com>
2004-07-02 03:43:52 +0000

    Tests beefed up to check for redundent files and rdoc files. Added test for validation of rubygem version.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@364 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/test_specification.rb
+++ rubygems/test/test_specification.rb
@@ -35,6 +35,23 @@ class TestSpecification < Test::Unit::TestCase
     }
   end
 
+  def test_rdoc_files_included
+    @gem_spec.files = %w(a b c d)
+    @gem_spec.extra_rdoc_files = %w(x y z)
+    @gem_spec.normalize
+    assert @gem_spec.files.include? 'x'
+  end
+
+  def test_duplicate_files_removed
+    @gem_spec.files = %w(a b c d b)
+    @gem_spec.extra_rdoc_files = %w(x y z x)
+    @gem_spec.normalize
+    assert_equal 1, @gem_spec.files.select{|n| n=='b'}.size
+    assert_equal 1, @gem_spec.extra_rdoc_files.select{|n| n=='x'}.size
+  end
+
   def test_invalid_version_in_gem_spec_makes_spec_invalid
+    @gem_spec.rubygems_version = "3"
+    assert_raises(Gem::InvalidSpecificationException) { @gem_spec.validate }
   end
 end

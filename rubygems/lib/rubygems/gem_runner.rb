c6c1b3119aa37b3fb058a848e59a035f7b06489e
Jim Weirich <jim.weirich@gmail.com>
2004-07-03 15:13:31 +0000

    Configuration handled in do_configuration now.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@376 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/lib/rubygems/gem_runner.rb
+++ rubygems/lib/rubygems/gem_runner.rb
@@ -5,9 +5,16 @@ module Gem
   class GemRunner
 
     def run(args)
-      cfg = Gem::ConfigFile.new(args)
-      Gem.use_paths(cfg[:gemhome], cfg[:gempath])
-      Gem::CommandManager.instance.run(cfg.args)
+      do_configuration(args)
+      Gem::CommandManager.instance.run(@cfg.args)
+    end
+
+    private
+
+    def do_configuration(args)
+      @cfg = Gem::ConfigFile.new(args)
+      Gem.use_paths(@cfg[:gemhome], @cfg[:gempath])
+      Command.extra_args = @cfg[:gem]
     end
 
   end

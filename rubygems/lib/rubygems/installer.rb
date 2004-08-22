f16618971cac45eefff1b7aef690bdd835041523
Richard Kilmer <rich@infoether.com>
2004-08-22 17:41:19 +0000

    fixed stub generation...i added the version stuff that only app stubs should have.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@488 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/lib/rubygems/installer.rb
+++ rubygems/lib/rubygems/installer.rb
@@ -225,14 +225,7 @@ TEXT
 
 require 'rubygems'
 $".delete('#{file_name}')
-version = "> 0"
-if ARGV.size > 0 && ARGV[0][0]==95 && ARGV[0][-1]==95
-  if Gem::Version.correct?(ARGV[0][1..-2])
-    version = ARGV[0][1..-2] 
-    ARGV.shift
-  end
-end
-require_gem '#{name}', version
+require_gem '#{name}'
 TEXT
       text
     end

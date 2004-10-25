6cd8d303484795c1a93f61fce72270bb4dd75863
Jim Weirich <jim.weirich@gmail.com>
2004-10-25 02:09:05 +0000

    Added comments to Fast versions of Gem::Version and Gem::Specification. Added to_s to gem specs. Added paths to get the list of gem paths. Refactored search_loadpath and search_gempath.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@565 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/lib/rubygems/loadpath_manager.rb
+++ rubygems/lib/rubygems/loadpath_manager.rb
@@ -9,6 +9,11 @@ end
 module Gem
   module LoadPathManager
     @paths = nil
+
+    # These local versions of Gem::Version and Gem::Specification are
+    # used by the load path manager because they are faster than the
+    # fully functional ones.  Full functionality is not required
+    # during the load phase.
     module Gem
       class Version
         class Requirement
@@ -46,27 +51,37 @@ module Gem
           r = other.version<=>@version if r == 0
           r
         end
+	def to_s
+	  "#<Gem::Specification name=#{@name} version=#{@version} quick=true>"
+	end
       end
     end
-    
+
+    def self.paths
+      @paths
+    end
+
+    # Prep the list of potential paths for require file resolution.
     def self.build_paths
       @paths = []
       ::Gem.path.each do |gempath|
-        @specs = Dir.glob("#{gempath}/specifications/*.gemspec").collect { |specfile| eval(File.read(specfile)) }.sort!
+        @specs = Dir.glob("#{gempath}/specifications/*.gemspec").collect { |specfile|
+	  eval(File.read(specfile))
+	}.sort
         @specs.each do |spec|
           spec.require_paths.each {|path| @paths << "#{gempath}/gems/#{spec.full_name}/#{path}"}
         end
       end
     end
     
+    # True if the file can be resolved with the existing load path.
     def self.search_loadpath(file)
-      result = Dir.glob("{#{($LOAD_PATH).join(',')}}/#{file}{,.rb,.so,.bundle,.dll,.sl}").delete_if {|f| File.directory?(f)}.size > 0
-      result
+      glob_over($LOAD_PATH, file).size > 0
     end
     
     def self.search_gempath(file)
       build_paths unless @paths
-      fullname = Dir.glob("{#{(@paths).join(',')}}/#{file}{,.rb,.so,.bundle,.dll,.sl}").delete_if {|f| File.directory?(f)}.first
+      fullname = glob_over(@paths, file).first
       return false unless fullname
       @specs.each do |spec|
         if fullname.include?("/#{spec.full_name}/")
@@ -76,6 +91,16 @@ module Gem
       end
       false
     end
+
+    private
+
+    SUFFIX_PATTERN = "{,.rb,.so,.bundle,.dll,.sl}"
+
+    def self.glob_over(list, file)
+      files = Dir.glob("{#{(list).join(',')}}/#{file}#{SUFFIX_PATTERN}")
+      files.delete_if { |f| File.directory?(f) }
+    end
+    
   end
 end
 

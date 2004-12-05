9c70b40f5ee92d7a88f18041d0647489939576e5
Jim Weirich <jim.weirich@gmail.com>
2004-12-05 02:25:31 +0000

    Renamed TestCache to TestSourceIndex Moved and simplified the test setup.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@664 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/test_cache.rb
+++ rubygems/test/test_cache.rb
@@ -2,35 +2,52 @@ require 'test/unit'
 require 'rubygems'
 Gem::manage_gems
 
-class TestCache < Test::Unit::TestCase
+class TestSourceIndex < Test::Unit::TestCase
+
+  SAMPLE_SPEC = Gem::Specification.new do |s|
+    s.name = 'foo'
+    s.version = "1.2.3"
+    s.platform = Gem::Platform::RUBY
+    s.summary = "This is a cool package"
+    s.files = [] 
+  end 
+  SAMPLE_SOURCE_HASH = { 'foo-1.2.3' => SAMPLE_SPEC }
+  
+  def setup
+    @source_index = Gem::SourceIndex.new(SAMPLE_SOURCE_HASH)
+  end
+
+  def test_create_from_directory
+  end
 
   def test_search_for_missing_gem_returns_nothing
-    gems = Gem::Cache.new(@sample_cache).search("bogusstring")
+    gems = @source_index.search("bogusstring")
     assert_equal(gems.size, 0)
   end
 
   def test_search_empty_cache_returns_nothing
-    gems = Gem::Cache.new({}).search("foo")
+    empty_source_index = Gem::SourceIndex.new({})
+    gems = empty_source_index.search("foo")
     assert_equal(gems.size, 0)
   end
 
   def test_search_with_full_gem_name_returns_gem
-    gems = Gem::Cache.new(@sample_cache).search("foo")
+    gems = @source_index.search("foo")
     assert_equal(gems.size, 1)
   end
 
   def test_search_with_full_gem_name_and_version_returns_gem
-    gems = Gem::Cache.new(@sample_cache).search("foo", "= 1.2.3")
+    gems = @source_index.search("foo", "= 1.2.3")
     assert_equal(gems.size, 1)
   end
 
   def test_search_with_full_gem_name_and_wrong_version_returns_nothing
-    gems = Gem::Cache.new(@sample_cache).search("foo", "= 3.2.1")
+    gems = @source_index.search("foo", "= 3.2.1")
     assert_equal(gems.size, 0)
   end
 
   def test_search_with_full_gem_name_in_mixed_case_returns_gem
-    gems = Gem::Cache.new(@sample_cache).search("FOo")
+    gems = @source_index.search("FOo")
     assert_equal(gems.size, 1,
       %{This is failing because we have duplication between remote_installer and } + 
       %{source_index.rb.  We should factor remote_installer's search logic out } +
@@ -38,21 +55,4 @@ class TestCache < Test::Unit::TestCase
       %{source_index.rb})
   end
 
-  def setup
-    @sample_spec = Gem::Specification.new do |s|
-      s.name = 'foo'
-      s.version = "1.2.3"
-      s.platform = Gem::Platform::RUBY
-      s.summary = "This is a cool package"
-      s.files = [] 
-    end 
-    @second_sample_spec = Gem::Specification.new do |s|
-      s.name = 'anothergem'
-      s.version = "0.0.1"
-      s.platform = Gem::Platform::RUBY
-      s.summary = "This is a cool package"
-      s.files = [] 
-    end 
-    @sample_cache = { 'foo-1.2.3' => @sample_spec }
-  end
 end

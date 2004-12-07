9e0ba6d98c9bb8a4625744ebc40f9260d093f452
Jim Weirich <jim.weirich@gmail.com>
2004-12-07 02:19:50 +0000

    Fixed default value of cache to be a hash.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@679 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/test_local_cache.rb
+++ rubygems/test/test_local_cache.rb
@@ -35,7 +35,7 @@ class TestLocalCache < Test::Unit::TestCase
   def test_use_system_by_default
     lc = Gem::LocalSourceInfoCache.new
     prep_cache_files(lc)
-    assert_equal "sys", lc.cache_data[0]
+    assert_equal "sys", lc.cache_data['key']
   end
 
   def test_use_user_cache_when_sys_no_writable
@@ -44,40 +44,40 @@ class TestLocalCache < Test::Unit::TestCase
     FileUtils.chmod 0544, lc.system_cache_file
 
     lc = Gem::LocalSourceInfoCache.new
-    assert_equal "usr", lc.cache_data[0]
+    assert_equal "usr", lc.cache_data['key']
   end
 
   def test_write_system_cache
     lc = Gem::LocalSourceInfoCache.new
     prep_cache_files(lc)
 
-    lc.cache_data[0] = 'new'
+    lc.cache_data['key'] = 'new'
     lc.write_cache
 
-    assert_equal ['new'], read_cache(lc.system_cache_file)
-    assert_equal ['usr'], read_cache(lc.user_cache_file)
+    assert_equal [['key', 'new']].sort, read_cache(lc.system_cache_file).to_a.sort
+    assert_equal [['key', 'usr']].sort, read_cache(lc.user_cache_file).to_a.sort
   end
 
   def test_flush
     lc = Gem::LocalSourceInfoCache.new
     prep_cache_files(lc)
 
-    lc.cache_data[0] = 'new'
+    lc.cache_data['key'] = 'new'
     lc.update
     lc.flush
 
-    assert_equal ['new'], read_cache(lc.system_cache_file)
+    assert_equal [['key','new']].sort, read_cache(lc.system_cache_file).to_a.sort
   end
 
   def test_write_user_cache
     lc = Gem::LocalSourceInfoCache.new
     prep_cache_files(lc)
     FileUtils.chmod 0544, lc.system_cache_file
-    lc.cache_data[0] = 'new'
+    lc.cache_data['key'] = 'new'
     lc.write_cache
 
-    assert_equal ['sys'], read_cache(lc.system_cache_file)
-    assert_equal ['new'], read_cache(lc.user_cache_file)
+    assert_equal [['key', 'sys']].sort, read_cache(lc.system_cache_file).to_a.sort
+    assert_equal [['key', 'new']].sort, read_cache(lc.user_cache_file).to_a.sort
   end
 
   def test_write_user_cache_from_scratch
@@ -86,11 +86,11 @@ class TestLocalCache < Test::Unit::TestCase
     FileUtils.rm_rf lc.user_cache_file
     FileUtils.chmod 0544, lc.system_cache_file
 
-    lc.cache_data[0] = 'new'
+    lc.cache_data['key'] = 'new'
     lc.write_cache
 
-    assert_equal ['sys'], read_cache(lc.system_cache_file)
-    assert_equal ['new'], read_cache(lc.user_cache_file)
+    assert_equal [['key', 'sys']].sort, read_cache(lc.system_cache_file).to_a.sort
+    assert_equal [['key', 'new']].sort, read_cache(lc.user_cache_file).to_a.sort
   end
 
   def test_write_user_directory_and_cache_from_scratch
@@ -99,18 +99,18 @@ class TestLocalCache < Test::Unit::TestCase
     FileUtils.rm_rf File.dirname(lc.user_cache_file)
     FileUtils.chmod 0544, lc.system_cache_file
 
-    lc.cache_data[0] = 'new'
+    lc.cache_data['key'] = 'new'
     lc.write_cache
 
-    assert_equal ['sys'], read_cache(lc.system_cache_file)
-    assert_equal ['new'], read_cache(lc.user_cache_file)
+    assert_equal [['key','sys']].sort, read_cache(lc.system_cache_file).to_a.sort
+    assert_equal [['key','new']].sort, read_cache(lc.user_cache_file).to_a.sort
   end
 
   def test_read_system_cache
     lc = Gem::LocalSourceInfoCache.new
     prep_cache_files(lc)
 
-    assert_equal ['sys'], lc.cache_data
+    assert_equal [['key','sys']].sort, lc.cache_data.to_a.sort
   end
 
   def test_read_user_cache
@@ -118,7 +118,7 @@ class TestLocalCache < Test::Unit::TestCase
     prep_cache_files(lc)
     FileUtils.chmod 0544, lc.system_cache_file
 
-    assert_equal ['usr'], lc.cache_data
+    assert_equal [['key','usr']].sort, lc.cache_data.to_a.sort
   end
 
   def test_no_writable_cache
@@ -138,7 +138,7 @@ class TestLocalCache < Test::Unit::TestCase
       [lc.user_cache_file, 'usr'],
     ].each do |fn, data|
       FileUtils.mkdir_p File.dirname(fn)
-      open(fn, "w") { |f| f.puts [data].to_yaml }
+      open(fn, "w") { |f| f.puts({'key' => data}.to_yaml) }
     end
   end
 

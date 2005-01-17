095ac4a1e216baaa99f336cbfa903de7367640bd
Jim Weirich <jim.weirich@gmail.com>
2005-01-17 18:15:55 +0000

    removed delibratly broken test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@733 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/test_builder.rb
+++ rubygems/test/test_builder.rb
@@ -4,7 +4,6 @@ Gem::manage_gems
 
 class TestBuilder < Test::Unit::TestCase
   def test_invalid_spec_does_not_build
-    assert_equal 1, 0
     spec = Gem::Specification.new 
     builder = Gem::Builder.new(spec)
     assert_raises(Gem::InvalidSpecificationException) {

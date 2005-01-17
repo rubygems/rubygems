aa7c31be1ddcc8b7c472e765fbee2ad376f9b6e1
Jim Weirich <jim.weirich@gmail.com>
2005-01-17 18:30:47 +0000

    broken build for demo
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@734 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/test_builder.rb
+++ rubygems/test/test_builder.rb
@@ -4,6 +4,7 @@ Gem::manage_gems
 
 class TestBuilder < Test::Unit::TestCase
   def test_invalid_spec_does_not_build
+    assert_equal 1, 0
     spec = Gem::Specification.new 
     builder = Gem::Builder.new(spec)
     assert_raises(Gem::InvalidSpecificationException) {

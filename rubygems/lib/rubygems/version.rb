5324e337a05b0c3e3f29d2d38272c14c08dbe887
Jim Weirich <jim.weirich@gmail.com>
2005-03-15 00:11:04 +0000

    Moved the master definition of class Requirement back under version. Kept the body of Requirement under Gem.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@831 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/lib/rubygems/version.rb
+++ rubygems/lib/rubygems/version.rb
@@ -50,6 +50,119 @@ module Gem
     end
   end
   
+  ####################################################################
+  # The Version class processes string versions into comparable values
+  #
+  class Version
+    include Comparable
+
+    # The originating definition of Requirement is left nested in
+    # Version for compatibility.  The full definition is given in
+    # Gem::Requirement.
+    class Requirement
+    end
+    
+    attr_accessor :version
+    
+    NUM_RE = /\s*(\d+(\.\d+)*)*\s*/
+    
+    ##
+    # Checks if version string is valid format
+    #
+    # str:: [String] the version string
+    # return:: [Boolean] true if the string format is correct, otherwise false
+    #
+    def self.correct?(str)
+      /^#{NUM_RE}$/.match(str)
+    end
+
+    ##
+    # Factory method to create a Version object.  Input may be a Version or a
+    # String.  Intended to simplify client code.
+    #
+    #   ver1 = Version.create('1.3.17')   # -> (Version object)
+    #   ver2 = Version.create(ver1)       # -> (ver1)
+    #   ver3 = Version.create(nil)        # -> nil
+    #
+    def self.create(input)
+      if input.respond_to? :version
+        return input
+      elsif input.nil?
+        return nil
+      else
+        return Version.new(input)
+      end
+    end
+
+    ##
+    # Constructs a version from the supplied string
+    #
+    # version:: [String] The version string.  Format is digit.digit...
+    #
+    def initialize(version)
+      raise ArgumentError, 
+        "Malformed version number string #{version}" unless Version.correct?(version)
+      @version = version
+    end
+    
+    ##
+    # Returns the text representation of the version
+    #
+    # return:: [String] version as string
+    #
+    def to_s
+      @version
+    end
+    
+    ##
+    # Convert version to integer array
+    #
+    # return:: [Array] list of integers
+    #
+    def to_ints
+      @version.scan(/\d+/).map {|s| s.to_i}
+    end
+    
+    ##
+    # Compares two versions
+    #
+    # other:: [Version or .to_ints] other version to compare to
+    # return:: [Fixnum] -1, 0, 1
+    #
+    def <=>(other)
+      return 1 unless other
+      rnums, vnums = to_ints, other.to_ints
+      [rnums.size, vnums.size].max.times {|i|
+        rnums[i] ||= 0
+        vnums[i] ||= 0
+      }
+      
+      begin
+        r,v = rnums.shift, vnums.shift
+      end until (r != v || rnums.empty?)
+
+      return r <=> v
+    end
+
+    # Return a new version object where the next to the last revision
+    # number is one greater. (e.g.  5.3.1 => 5.4)
+    def bump
+      ints = to_ints
+      ints.pop if ints.size > 1
+      ints[-1] += 1
+      self.class.new(ints.join("."))
+    end
+    
+  end
+
+  # Class Requirement's original definition is nested in Version.
+  # Although an probably inappropriate place, current gems specs
+  # reference the nested class name explicitly.  To remain compatible
+  # with old software loading gemspecs, we leave the original
+  # definition in Version, but define an alias Gem::Requirement for
+  # use everywhere else.
+  Requirement = ::Gem::Version::Requirement
+
   ##################################################################
   # Requirement version includes a prefaced comparator in addition
   # to a version number.
@@ -184,106 +297,4 @@ module Gem
     
   end
   
-  ####################################################################
-  # The Version class processes string versions into comparable values
-  #
-  class Version
-    include Comparable
-
-    # Nested requirement constant kept for compatibility with old gem
-    # specs.
-    Requirement = ::Gem::Requirement
-    
-    attr_accessor :version
-    
-    NUM_RE = /\s*(\d+(\.\d+)*)*\s*/
-    
-    ##
-    # Checks if version string is valid format
-    #
-    # str:: [String] the version string
-    # return:: [Boolean] true if the string format is correct, otherwise false
-    #
-    def self.correct?(str)
-      /^#{NUM_RE}$/.match(str)
-    end
-
-    ##
-    # Factory method to create a Version object.  Input may be a Version or a
-    # String.  Intended to simplify client code.
-    #
-    #   ver1 = Version.create('1.3.17')   # -> (Version object)
-    #   ver2 = Version.create(ver1)       # -> (ver1)
-    #   ver3 = Version.create(nil)        # -> nil
-    #
-    def self.create(input)
-      if input.respond_to? :version
-        return input
-      elsif input.nil?
-        return nil
-      else
-        return Version.new(input)
-      end
-    end
-
-    ##
-    # Constructs a version from the supplied string
-    #
-    # version:: [String] The version string.  Format is digit.digit...
-    #
-    def initialize(version)
-      raise ArgumentError, 
-        "Malformed version number string #{version}" unless Version.correct?(version)
-      @version = version
-    end
-    
-    ##
-    # Returns the text representation of the version
-    #
-    # return:: [String] version as string
-    #
-    def to_s
-      @version
-    end
-    
-    ##
-    # Convert version to integer array
-    #
-    # return:: [Array] list of integers
-    #
-    def to_ints
-      @version.scan(/\d+/).map {|s| s.to_i}
-    end
-    
-    ##
-    # Compares two versions
-    #
-    # other:: [Version or .to_ints] other version to compare to
-    # return:: [Fixnum] -1, 0, 1
-    #
-    def <=>(other)
-      return 1 unless other
-      rnums, vnums = to_ints, other.to_ints
-      [rnums.size, vnums.size].max.times {|i|
-        rnums[i] ||= 0
-        vnums[i] ||= 0
-      }
-      
-      begin
-        r,v = rnums.shift, vnums.shift
-      end until (r != v || rnums.empty?)
-
-      return r <=> v
-    end
-
-    # Return a new version object where the next to the last revision
-    # number is one greater. (e.g.  5.3.1 => 5.4)
-    def bump
-      ints = to_ints
-      ints.pop if ints.size > 1
-      ints[-1] += 1
-      self.class.new(ints.join("."))
-    end
-    
-  end
 end

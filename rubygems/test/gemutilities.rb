ce71d1801cd09467b0adc1a68f47f27dfc751eac
Ryan Davis <ryand-ruby@zenspider.com>
2005-05-29 20:30:53 +0000

    Added a new abstract TestCase class with temporary data setup and utilities. Pulled up a fair amount from TestLocalCache. Added utility methods to create gems on the fly.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@866 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/test/gemutilities.rb
+++ rubygems/test/gemutilities.rb
@@ -1,18 +1,82 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'test/unit/testcase'
+require 'tmpdir'
 require 'test/yaml_data'
 
 module Utilities
-  def make_gemhome(path)
-    FileUtils.mkdir_p path
-  end
-
   def make_cache_area(path, *uris)
-    make_gemhome(path)
     fn = File.join(path, 'source_cache')
     open(fn, 'w') do |f| f.write Marshal.dump(cache_hash(*uris)) end
   end
 
   extend self
+end
+
+class RubyGemTestCase < Test::Unit::TestCase
+  def setup
+    @tempdir = File.join Dir.tmpdir, "test_rubygems_#{$$}"
+    @gemhome = File.join @tempdir, "gemhome"
+    @gemcache = File.join(@gemhome, "source_cache")
+    @usrcache = File.join(@gemhome, ".gem", "user_cache")
+
+    FileUtils.mkdir_p @gemhome
+
+    ENV['GEMCACHE'] = @usrcache
+    Gem.use_paths(@gemhome)
+  end
+
+  def teardown
+    FileUtils.rm_r @tempdir
+    ENV['GEMCACHE'] = nil
+    Gem.clear_paths
+  end
+
+  def prep_cache_files(lc)
+    [ [lc.system_cache_file, 'sys'],
+      [lc.user_cache_file, 'usr'],
+    ].each do |fn, data|
+      FileUtils.mkdir_p File.dirname(fn)
+      open(fn, "w") { |f| f.puts(Marshal.dump({'key' => data})) }
+    end
+  end
+
+  def read_cache(fn)
+    open(fn) { |f| Marshal.load(f) }
+  end
+
+  def write_file(path)
+    path = File.join(@gemhome, path)
+    dir = File.dirname path
+    FileUtils.mkdir_p dir
+    File.open(path, "w") { |io|
+      yield(io)
+    }
+  end
+
+  def quick_gem(gemname, version='0.0.2')
+    spec = Gem::Specification.new do |s|
+      s.platform = Gem::Platform::RUBY
+      s.name = gemname
+      s.version = version
+      s.author = 'A User'
+      s.email = 'example@example.com'
+      s.homepage = 'http://example.com'
+      s.has_rdoc = true
+      s.summary = "this is a summary"
+      s.description = "This is a test description"
+      yield(s)
+    end
+
+    write_file(File.join("specifications", spec.full_name + ".gemspec")) do |io|
+      io.write(spec.to_ruby)
+    end
+
+    return spec
+  end
+
+  def test_stupid
+    # shuts up test/unit
+  end
 end

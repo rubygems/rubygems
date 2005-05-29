ed152bdfdc56d14da2bd8f2bf1c1be4063c6f78a
Ryan Davis <ryand-ruby@zenspider.com>
2005-05-29 22:16:17 +0000

    Added.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@876 3d4018f9-ac1a-0410-99e9-8a154d859a19

---
+++ rubygems/test/test_file_list.rb
@@ -0,0 +1,96 @@
+require 'fileutils'
+require 'tmpdir'
+require 'test/unit'
+require 'rubygems'
+require 'test/gemutilities'
+Gem::manage_gems
+
+class TestFileList < RubyGemTestCase
+
+    def import_spec(name)
+        file = File.join(@gem_install_path, "specifications", name)
+        eval File.read(file)
+    end
+
+    def setup
+        @spec = Gem::Specification.new do |s|
+            s.files = ['lib/code.rb','lib/apple.rb','lib/brown.rb']
+            s.name = "a"
+            s.version = "0.0.1"
+            s.summary = "summary"
+            s.description = "desc"
+            s.require_path = 'lib'
+        end
+
+        @cm = Gem::CommandManager.new
+        @inspect = @cm['inspect']
+
+        current_path = Dir.getwd
+        @gem_install_path =  File.join(current_path, "test/mock/gems/")
+        @gem_root_dir = File.join(@gem_install_path, "gems", @spec.name + "-" + @spec.version.to_s)
+
+        @gemspec_filename = @spec.name + '-' + @spec.version.to_s + '.gemspec'
+        @spec_destination_path = File.join(@gem_install_path, "specifications", @gemspec_filename)
+
+        begin
+            File.open(@spec_destination_path, 'w') do |fp| 
+                fp.write @spec.to_ruby
+            end
+        rescue Exception => e 
+
+        end
+
+    end
+
+    def teardown
+        #    FileUtils.rm_rf @dir unless $DEBUG
+    end
+
+    def test_inspect_list
+        args = ["-s", @gem_install_path, "a"]
+        Gem::Command.instance_eval "public :handle_options"
+        @inspect.handle_options(args)
+        sio = StringIO.new
+        @inspect.execute(sio)
+        files = sio.string.split("\n")
+        code = File.join(@gem_root_dir,"lib/code.rb")
+        assert_match(code, files[0])
+    end
+
+    def test_inspect_list_unknown
+        args = ["-s", @gem_install_path, "not_there"]
+        Gem::Command.instance_eval "public :handle_options"
+        @inspect.handle_options(args)
+        sio = StringIO.new
+        @inspect.execute(sio)
+        assert_match(/Unable to find/, sio.string)
+    end
+
+    def disable_test_specification
+
+
+
+        puts "  dest path: " + @spec_destination_path
+        assert(FileTest.exists?(@spec_destination_path))
+        assert(FileTest.size(@spec_destination_path)>0)
+        spec = import_spec(@gemspec_filename)
+
+        p spec.require_paths
+        p spec.full_gem_path
+
+        files = spec.files.map do |f|
+        end
+
+        root = @gem_install_path + "a-0.0.1";
+
+        puts "  files[0] is: " + files[0]
+
+        check1 = root + "/lib/code.rb"
+        puts "  check1 is " + check1
+        assert(files[0] == check1)
+        assert(files[1] == root + "/lib/apple.rb");
+        assert(files[2] == root + "/lib/brown.rb");
+    end
+
+end
+
---
+++ rubygems/test/test_specific_extras.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+require 'rubygems/cmd_manager'
+require 'rubygems/user_interaction'
+require 'test/mockgemui'
+
+class TestSpecificExtras < Test::Unit::TestCase
+  include Gem::DefaultUserInteraction
+
+  def setup
+    @cm = Gem::CommandManager.new
+    @cmd = @cm['rdoc']
+  end
+
+  
+  def test_add_extra_args
+    added_args = ["--all"]
+    command = "rdoc"
+    Gem::Command.add_specific_extra_args command, added_args
+
+    assert_equal(added_args, Gem::Command.specific_extra_args(command))
+
+    Gem::Command.instance_eval "public :add_extra_args"
+    h = @cmd.add_extra_args([])
+    assert_equal(added_args,h)
+  end
+  
+  def test_add_extra_args_unknown
+    added_args = ["--definitely_not_there"]
+    command = "rdoc"
+    Gem::Command.add_specific_extra_args command, added_args
+
+    assert_equal(added_args, Gem::Command.specific_extra_args(command))
+
+    Gem::Command.instance_eval "public :add_extra_args"
+    h = @cmd.add_extra_args([])
+    assert_equal([],h)
+  end
+end

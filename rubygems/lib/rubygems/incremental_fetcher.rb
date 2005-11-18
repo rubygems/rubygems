c748d424e5254af0268eb0004f7895b52fda3d52
Jim Weirich <jim.weirich@gmail.com>
2005-11-18 16:30:25 +0000

    Added comments to the incremental fetcher. Added a fetch_path method. Added a get_entry to handle the conversion of old cache entries into modern SourceInfoCacheEntry versions. Removed fetch (since we needed fetch_path anyways).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@968 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/lib/rubygems/incremental_fetcher.rb
+++ rubygems/lib/rubygems/incremental_fetcher.rb
@@ -1,18 +1,22 @@
 module Gem
 
+  ####################################################################
   class IncrementalFetcher
+    # Initialize an incremental source fetcher.
     def initialize(source_uri, fetcher, cache_manager)
       @source_uri = source_uri
       @fetcher = fetcher
       @manager = cache_manager
     end
 
+    # Return the size of the source index for the gem source.
     def size
       @fetcher.size
     end
 
+    # Return the source index for the gem source.
     def source_index
-      entry = @manager.cache_data[@source_uri]
+      entry = get_entry
       if entry.nil?
 	entry =
 	  @manager.cache_data[@source_uri] =
@@ -22,7 +26,27 @@ module Gem
       entry.source_index
     end
 
+    # Fetch the given path from the gem source.
+    def fetch_path(path)
+      @fetcher.fetch_path(path)
+    end
+
     private
+
+    def get_entry
+      result = @manager.cache_data[@source_uri]
+      case result
+      when SourceInfoCacheEntry, nil
+	# do nothing ... everything is fine
+      when Hash
+	result = SourceInfoCacheEntry.new(result['source_index'], result['size'])
+	@manager.cache_data[@source_uri] = result	
+      else
+	puts "DBG: #{result.class}"
+	fail "Ooops"
+      end
+      result	
+    end
 
     # Return the size of the remote source index.  Cache the value for later use.
     def remote_size
@@ -40,6 +64,7 @@ module Gem
       entry.replace_source_index(si, remote_size)
     end
 
+    # Remove extra entries from the cached source index.
     def remove_extra(source_index, spec_names)
       dictionary = spec_names.inject({}) { |h, k| h[k] = true; h }
       source_index.each do |name, spec|
@@ -50,11 +75,12 @@ module Gem
       end
     end
 
+    # Update the cached source index with new entries.
     def update_with_missing(source_index, spec_names)
       spec_names.each do |spec_name|
 	spec = source_index.specification(spec_name)
 	if spec.nil?
-	  zipped_yaml = fetch("/quick/" + spec_name + ".gemspec.rz")
+	  zipped_yaml = fetch_path("/quick/" + spec_name + ".gemspec.rz")
 	  gemspec = YAML.load(unzip(zipped_yaml))
 	  source_index.add_spec(gemspec)
 	  @manager.update
@@ -62,17 +88,15 @@ module Gem
       end
     end
 
+    # Get the quick index needed for incremental updates.
     def get_quick_index
-      zipped_index = fetch("/quick/index.rz")
+      zipped_index = fetch_path("/quick/index.rz")
       unzip(zipped_index).split("\n")
     rescue ::Exception => ex
       fail OperationNotSupportedError.new("No quick index found: " + ex.message)
     end
 
-    def fetch(uri)
-      @fetcher.fetch_path(uri)
-    end
-
+    # Unzip the given string.
     def unzip(string)
       require 'zlib'
       Zlib::Inflate.inflate(string)

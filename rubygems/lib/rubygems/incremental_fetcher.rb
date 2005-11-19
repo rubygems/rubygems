063f1d864f87529326b3e9d502659fa0354772df
Jim Weirich <jim.weirich@gmail.com>
2005-11-19 13:54:33 +0000

    Added progress reporting to the incremental fetcher. Refactored incremental fetcher to determine the complete list of missing gems first. Added code to handle missing gemspecs in incremental download.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@971 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- rubygems/lib/rubygems/incremental_fetcher.rb
+++ rubygems/lib/rubygems/incremental_fetcher.rb
@@ -2,6 +2,8 @@ module Gem
 
   ####################################################################
   class IncrementalFetcher
+    include Gem::DefaultUserInteraction
+
     # Initialize an incremental source fetcher.
     def initialize(source_uri, fetcher, cache_manager)
       @source_uri = source_uri
@@ -57,7 +59,8 @@ module Gem
     def update_cache(entry)
       index_list = get_quick_index
       remove_extra(entry.source_index, index_list)
-      update_with_missing(entry.source_index, index_list)
+      missing_list = find_missing(entry.source_index, index_list)
+      update_with_missing(entry.source_index, missing_list)
       @manager.flush
     rescue OperationNotSupportedError => ex
       si = @fetcher.source_index
@@ -75,17 +78,30 @@ module Gem
       end
     end
 
-    # Update the cached source index with new entries.
-    def update_with_missing(source_index, spec_names)
-      spec_names.each do |spec_name|
-	spec = source_index.specification(spec_name)
-	if spec.nil?
+    # Make a list of full names for all the missing gemspecs.
+    def find_missing(source_index, spec_names)
+      spec_names.find_all { |full_name|
+	source_index.specification(full_name).nil?
+      }
+    end
+
+    # Update the cached source index with the missing names.
+    def update_with_missing(source_index, missing_names)
+      progress = ui.progress_reporter(missing_names.size,
+	"Need to update #{missing_names.size} gems from #{@source_uri}")
+      missing_names.each do |spec_name|
+	begin
 	  zipped_yaml = fetch_path("/quick/" + spec_name + ".gemspec.rz")
 	  gemspec = YAML.load(unzip(zipped_yaml))
 	  source_index.add_spec(gemspec)
 	  @manager.update
+	  progress.updated(spec_name)
+	rescue RuntimeError => ex
+	  ui.say "Failed to download spec for #{spec_name} from #{@source_uri}"
 	end
       end
+      progress.done
+      progress.count
     end
 
     # Get the quick index needed for incremental updates.

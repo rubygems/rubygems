1e27bb8a45cedacd290bb02faaf0169d1d8a44ff
Chad Fowler <chad@chadfowler.com>
2007-02-04 21:00:17 +0000

    Fixed failing tests
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@1209 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- test/test_gem_ext_ext_conf_builder.rb
+++ test/test_gem_ext_ext_conf_builder.rb
@@ -34,7 +34,7 @@ class TestGemExtExtConfBuilder < RubyGemTestCase
       "make: Nothing to be done for `install'.\n"
     ]
 
-    assert_equal "ruby extconf.rb", output[0]
+    assert_match /^ruby extconf.rb/, output[0]
     assert_equal "creating Makefile\n", output[1]
     case RUBY_PLATFORM
     when /mswin/ then
@@ -63,10 +63,10 @@ class TestGemExtExtConfBuilder < RubyGemTestCase
 
     assert_match(/\Aextconf failed:
 
-ruby extconf.rb
-checking for main\(\) in .*?nonexistent/, error.message)
+ruby extconf.rb.*
+checking for main\(\) in .*?nonexistent/m, error.message)
 
-    assert_equal 'ruby extconf.rb', output[0]
+    assert_match /^ruby extconf.rb/, output[0]
   end
 
   def test_class_make

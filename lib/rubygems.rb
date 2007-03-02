fa434421e8791f3eea23c5974de5efd1a6afcf2c
Eric Hodel <drbrain@segment7.net>
2007-03-02 02:25:55 +0000

    Add dirs from activate after -I and ENV['RUBYLIB'] dirs.  Otherwise gems won't allow -I to work when #gem is called.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@1216 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -253,28 +253,33 @@ module Gem
       if spec.loaded?
         return false unless autorequire
         result = spec.autorequire ? require(spec.autorequire) : false
-        return result || false 
+        return result || false
       end
 
       spec.loaded = true
       @loaded_specs[spec.name] = spec
-      
+
       # Load dependent gems first
       spec.dependencies.each do |dep_gem|
         activate(dep_gem, autorequire)
       end
-      
+
       # add bin dir to require_path
       if(spec.bindir) then
         spec.require_paths << spec.bindir
       end
-      
+
       # Now add the require_paths to the LOAD_PATH
-      spec.require_paths.each do |path|
-        $:.unshift File.join(spec.full_gem_path, path)
+      require_paths = spec.require_paths.map do |path|
+        File.join spec.full_gem_path, path
       end
-      
-      if autorequire && spec.autorequire then
+
+      sitelibdir = Config::CONFIG['sitelibdir']
+
+      $:.insert($:.index(sitelibdir), *require_paths)
+
+      # Now autorequire
+      if autorequire && spec.autorequire then # DEPRECATED
         Array(spec.autorequire).each do |a_lib|
           require a_lib
         end
@@ -282,7 +287,7 @@ module Gem
 
       return true
     end
-    
+
     # Report a load error during activation.  The message of load
     # error depends on whether it was a version mismatch or if there
     # are not gems of any version by the requested name.

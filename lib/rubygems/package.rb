ab342f141cbd8019fdbbe16fb84c0f7ed569adca
Jim Weirich <jim.weirich@gmail.com>
2007-06-20 05:12:07 +0000

    Added Jamis Bucks ZLib patch for evaluation
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@1259 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/rubygems/package.rb
+++ lib/rubygems/package.rb
@@ -618,9 +618,14 @@ module Gem::Package
     # this method would use the String IO approach on all platforms at all
     # times.  And that's the way it is.
     def zipped_stream(entry)
-      zis = Zlib::GzipReader.new entry
-      dis = zis.read
-      is = StringIO.new(dis)
+      # This is Jamis Buck's ZLib workaround.  The original code is
+      # commented out while we evaluate this patch.
+      entry.read(10) # skip the gzip header
+      zis = Zlib::Inflate.new(-Zlib::MAX_WBITS)
+      is = StringIO.new(zis.inflate(entry.read))
+      # zis = Zlib::GzipReader.new entry
+      # dis = zis.read
+      # is = StringIO.new(dis)
     ensure
       zis.finish if zis
     end

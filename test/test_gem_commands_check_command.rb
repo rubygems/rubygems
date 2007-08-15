05bde300f2cf7ba9710ffcc49a9adcd6d1306547
Eric Hodel <drbrain@segment7.net>
2007-08-15 21:45:36 +0000

    Make autotest compliant, remove useless test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@1337 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- test/test_gem_commands_check_command.rb
+++ test/test_gem_commands_check_command.rb
@@ -1,16 +1,14 @@
-#!/usr/bin/env ruby
 #--
 # Copyright 2006 by Chad Fowler, Rich Kilmer, Jim Weirich and others.
 # All rights reserved.
 # See LICENSE.txt for permissions.
 #++
 
-
 require 'test/unit'
 require 'test/gemutilities'
 require 'rubygems/commands/check_command'
 
-class TestCheckCommand < RubyGemTestCase
+class TestGemCommandsCheckCommand < RubyGemTestCase
 
   def setup
     super
@@ -18,16 +16,10 @@ class TestCheckCommand < RubyGemTestCase
     @cmd = Gem::Commands::CheckCommand.new
   end
 
-  def test_create
+  def test_initialize
     assert_equal "check", @cmd.command
     assert_equal "gem check", @cmd.program_name
     assert_match(/Check/, @cmd.summary)
   end
 
-  def test_invoke_help
-    use_ui @ui do 
-      assert ! @cmd.invoke('--help')
-      assert_match(/Usage:/, ui.output)
-    end
-  end
 end

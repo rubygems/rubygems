a3b76c64c5ce4eab31d020560eea89ffab54b469
Eric Hodel <drbrain@segment7.net>
2008-10-31 23:24:26 +0000

    Clarify and update generate_index help
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@1917 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/rubygems/commands/generate_index_command.rb
+++ lib/rubygems/commands/generate_index_command.rb
@@ -22,23 +22,36 @@ class Gem::Commands::GenerateIndexCommand < Gem::Command
     <<-EOF
 The generate_index command creates a set of indexes for serving gems
 statically.  The command expects a 'gems' directory under the path given to
-the --directory option.  When done, it will generate a set of files like this:
+the --directory option.  The given diretory will be the directory you serve
+as the gem repository.
 
-  gems/                                        # .gem files you want to index
+For `gem generate_index --directory /path/to/repo`, expose /path/to/repo via
+your HTTP server configuration (not /path/to/repo/gems).
+
+When done, it will generate a set of files like this:
+
+  gems/*.gem                                   # .gem files you want to
+                                               # index
+
+  latest_specs.<version>.gz                    # latest specs index
+  specs.<version>.gz                           # specs index
+  quick/Marshal.<version>/<gemname>.gemspec.rz # Marshal quick index file
+
+  # these files support legacy RubyGems
   quick/index
   quick/index.rz                               # quick index manifest
-  quick/<gemname>.gemspec.rz                   # legacy YAML quick index file
-  quick/Marshal.<version>/<gemname>.gemspec.rz # Marshal quick index file
+  quick/<gemname>.gemspec.rz                   # legacy YAML quick index
+                                               # file
   Marshal.<version>
-  Marshal.<version>.Z # Marshal full index
+  Marshal.<version>.Z                          # Marshal full index
   yaml
-  yaml.Z # legacy YAML full index
+  yaml.Z                                       # legacy YAML full index
 
-The .Z and .rz extension files are compressed with the inflate algorithm.  The
-Marshal version number comes from ruby's Marshal::MAJOR_VERSION and
-Marshal::MINOR_VERSION constants.  It is used to ensure compatibility.  The
-yaml indexes exist for legacy RubyGems clients and fallback in case of Marshal
-version changes.
+The .Z and .rz extension files are compressed with the inflate algorithm.
+The Marshal version number comes from ruby's Marshal::MAJOR_VERSION and
+Marshal::MINOR_VERSION constants.  It is used to ensure compatibility.
+The yaml indexes exist for legacy RubyGems clients and fallback in case of
+Marshal version changes.
     EOF
   end
 

6be0fb40fc1f6dc04cca41ff7b7aefd296b511e9
Daniel Berger <djberg96@gmail.com>
2009-01-03 11:39:12 +0000

    Updated the specification_version
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@1980 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- test/test_gem_specification.rb
+++ test/test_gem_specification.rb
@@ -715,7 +715,7 @@ Gem::Specification.new do |s|
 
   if s.respond_to? :specification_version then
     current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
-    s.specification_version = 2
+    s.specification_version = 3
 
     if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
       s.add_runtime_dependency(%q<rake>, [\"> 0.4\"])

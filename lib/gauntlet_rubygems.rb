654b8a2328d6bb2cd23e55006b49188a738aa60e
Eric Hodel <drbrain@segment7.net>
2009-04-01 04:02:36 +0000

    Add gauntlet code for RubyGems spec validator
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@2118 3d4018f9-ac1a-0410-99e9-8a154d859a19

---
+++ lib/gauntlet_rubygems.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'gauntlet'
+
+class Gem::Gauntlet < Gauntlet
+  
+  def run(name)
+    warn name
+    self.dirty = true
+
+    spec = Gem::Specification.load 'gemspec'
+    spec.validate
+
+    self.data[name] = false
+
+  rescue Gem::InvalidSpecificationException => e
+    self.data[name] = e.message
+  end
+
+  def should_skip?(name)
+    self.data[name] == false
+  end
+
+end
+
+gantlet = Gem::Gauntlet.new
+gauntlet.run_the_gauntlet ARGV.shift
+

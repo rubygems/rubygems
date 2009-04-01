954c9e048aa8ad551223cd28e1d0ac894b58dbb0
Ryan Davis <ryand-ruby@zenspider.com>
2009-04-01 04:21:49 +0000

    Added reporting and extra error handling
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@2123 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/gauntlet_rubygems.rb
+++ lib/gauntlet_rubygems.rb
@@ -1,31 +1,35 @@
 require 'rubygems'
 require 'gauntlet'
 
-class Gem::Gauntlet < Gauntlet
-  
+class GemGauntlet < Gauntlet
   def run(name)
     warn name
-    self.dirty = true
 
     spec = begin
              Gem::Specification.load 'gemspec'
            rescue SyntaxError
-             Gem::Specification.from_yaml 'gemspec'
+             Gem::Specification.from_yaml File.read('gemspec')
            end
     spec.validate
 
     self.data[name] = false
-
-  rescue Gem::InvalidSpecificationException => e
+    self.dirty = true
+  rescue SystemCallError, Gem::InvalidSpecificationException => e
     self.data[name] = e.message
+    self.dirty = true
   end
 
   def should_skip?(name)
     self.data[name] == false
   end
 
+  def report
+    self.data.sort.reject { |k,v| !v }.each do |k,v|
+      puts "%-21s: %s" % [k, v]
+    end
+  end
 end
 
-gauntlet = Gem::Gauntlet.new
+gauntlet = GemGauntlet.new
 gauntlet.run_the_gauntlet ARGV.shift
-
+gauntlet.report

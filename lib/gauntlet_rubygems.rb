237f16d2383a6032a61f96b65ed9236eeb1023c9
Ryan Davis <ryand-ruby@zenspider.com>
2009-04-01 04:23:06 +0000

    Added current status
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@2124 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/gauntlet_rubygems.rb
+++ lib/gauntlet_rubygems.rb
@@ -1,6 +1,21 @@
 require 'rubygems'
 require 'gauntlet'
 
+##
+# GemGauntlet validates all current gems. Currently these packages are
+# borked:
+#
+# Asami-0.04           : No such file or directory - bin/Asami.rb
+# ObjectGraph-1.0.1    : No such file or directory - bin/objectgraph
+# evil-ruby-0.1.0      : authors must be Array of Strings
+# fresh_cookies-1.0.0  : authors must be Array of Strings
+# plugems_deploy-0.2.0 : authors must be Array of Strings
+# pmsrb-0.2.0          : authors must be Array of Strings
+# pqa-1.6              : authors must be Array of Strings
+# rant-0.5.7           : authors must be Array of Strings
+# rvsh-0.4.5           : No such file or directory - bin/rvsh
+# xen-0.1.2.1          : authors must be Array of Strings
+
 class GemGauntlet < Gauntlet
   def run(name)
     warn name

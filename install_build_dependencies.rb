a1462006e5c0998ba97d189cc3c82616f6d57b6a
Eric Hodel <drbrain@segment7.net>
2009-06-19 22:21:35 +0000

    Revert 2236, version gets set at release time.
    Revert 2235, rubygems/rubygems_version.rb is not necessary.
    
    Revert 2234 and 2233, replace with built-in rake task from Hoe.
    
    
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@2237 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- install_build_dependencies.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-# Installs dependencies required to test and build RubyGems itself.
-
-require 'rubygems'
-puts 'Installing/upgrading minitest, hoe, and hoe-seattlerb gems if necessary...'
-puts `gem install minitest` unless Gem.available?(/^minitest$/,'>=1.3.1')
-puts `gem install hoe` unless Gem.available?(/^hoe$/,'>=2.1.0')
-puts `gem install hoe-seattlerb` unless Gem.available?(/^hoe-seattlerb$/,'>=1.0.0')
--- lib/rubygems/rubygems_version.rb
+++
@@ -1,3 +0,0 @@
-module Gem
-  RubyGemsVersion = VERSION = '1.3.4.1'
-end

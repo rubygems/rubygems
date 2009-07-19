041ee988acac417a2ea0fcec49f8d37e18b90208
Chad Woolley <thewoolleyman@gmail.com>
2009-07-19 08:35:35 +0000

    only send ci emails to dev list from official servers
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@2274 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,9 +1,10 @@
 # Project-specific configuration for CruiseControl.rb
+require 'socket'
 
 Project.configure do |project|
   
   # Send email notifications about broken and fixed builds to email1@your.site, email2@your.site (default: send to nobody)
-  project.email_notifier.emails = ['rubygems-developers@rubyforge.org']
+  project.email_notifier.emails = ['rubygems-developers@rubyforge.org'] if Socket.gethostname =~ /ci.pivotallabs.com/
 
   # Set email 'from' field to john@doe.com:
   project.email_notifier.from = 'devnull+rubygems-ci@pivotallabs.com'

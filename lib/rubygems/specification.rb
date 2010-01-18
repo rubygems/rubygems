9bdb856da32e560abe6389f5db1e37e5a4b255c8
Eric Hodel <drbrain@segment7.net>
2010-01-18 22:49:36 +0000

    RDoc clarification for various specy things you should/shouldn't do
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@2332 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/rubygems/specification.rb
+++ lib/rubygems/specification.rb
@@ -1048,14 +1048,18 @@ class Gem::Specification
   ##
   # :attr_accessor: rubygems_version
   #
-  # The version of RubyGems used to create this gem
+  # The version of RubyGems used to create this gem.
+  #
+  # Do not set this, it is set automatically when the gem is packaged.
 
   required_attribute :rubygems_version, Gem::RubyGemsVersion
 
   ##
   # :attr_accessor: specification_version
   #
-  # The Gem::Specification version of this gemspec
+  # The Gem::Specification version of this gemspec.
+  #
+  # Do not set this, it is set automatically when the gem is packaged.
 
   required_attribute :specification_version, CURRENT_SPECIFICATION_VERSION
 
@@ -1077,6 +1081,8 @@ class Gem::Specification
   # :attr_accessor: date
   #
   # The date this gem was created
+  #
+  # Do not set this, it is set automatically when the gem is packaged.
 
   required_attribute :date, TODAY
 
@@ -1084,13 +1090,20 @@ class Gem::Specification
   # :attr_accessor: summary
   #
   # A short summary of this gem's description.  Displayed in `gem list -d`.
+  #
+  # The description should be more detailed than the summary.  For example,
+  # you might wish to copy the entire README into the description.
+  #
+  # As of RubyGems 1.3.2 newlines are no longer stripped.
 
   required_attribute :summary
 
   ##
   # :attr_accessor: require_paths
   #
-  # Paths in the gem to add to $LOAD_PATH when this gem is activated
+  # Paths in the gem to add to $LOAD_PATH when this gem is activated.
+  #
+  # The default 'lib' is typically sufficient.
 
   required_attribute :require_paths, ['lib']
 
@@ -1100,6 +1113,13 @@ class Gem::Specification
   # :attr_accessor: email
   #
   # A contact email for this gem
+  #
+  # If you are providing multiple authors and multiple emails they should be
+  # in the same order such that:
+  #
+  #   Hash[*spec.authors.zip(spec.emails).flatten]
+  #
+  # Gives a hash of author name to email address.
 
   attribute :email
 
@@ -1137,6 +1157,8 @@ class Gem::Specification
   # :attr_accessor: default_executable
   #
   # The default executable for this gem.
+  #
+  # This is not used.
 
   attribute :default_executable
 
@@ -1164,7 +1186,7 @@ class Gem::Specification
   ##
   # :attr_accessor: required_ruby_version
   #
-  # The ruby of version required by this gem
+  # The version of ruby required by this gem
 
   attribute :required_ruby_version, Gem::Requirement.default
 
@@ -1179,6 +1201,9 @@ class Gem::Specification
   # :attr_accessor: platform
   #
   # The platform this gem runs on.  See Gem::Platform for details.
+  #
+  # Setting this to any value other than Gem::Platform::RUBY or
+  # Gem::Platform::CURRENT is probably wrong.
 
   attribute :platform, Gem::Platform::RUBY
 
@@ -1207,7 +1232,14 @@ class Gem::Specification
   ##
   # :attr_accessor: authors
   #
-  # The list of authors who wrote this gem
+  # The list of author names who wrote this gem.
+  #
+  # If you are providing multiple authors and multiple emails they should be
+  # in the same order such that:
+  #
+  #   Hash[*spec.authors.zip(spec.emails).flatten]
+  #
+  # Gives a hash of author name to email address.
 
   array_attribute :authors
 
@@ -1243,21 +1275,21 @@ class Gem::Specification
   ##
   # :attr_accessor: rdoc_options
   #
-  # An ARGV-style array of options to RDoc
+  # An ARGV style array of options to RDoc
 
   array_attribute :rdoc_options
 
   ##
   # :attr_accessor: extra_rdoc_files
   #
-  # Extra files to add to RDoc
+  # Extra files to add to RDoc such as README or doc/examples.txt
 
   array_attribute :extra_rdoc_files
 
   ##
   # :attr_accessor: executables
   #
-  # Executables included in the gem
+  # Executables included in the gem.
 
   array_attribute :executables
 
@@ -1281,6 +1313,9 @@ class Gem::Specification
   # :attr_reader: dependencies
   #
   # A list of Gem::Dependency objects this gem depends on.
+  #
+  # Use #add_dependency or #add_development_dependency to add dependencies to
+  # a gem.
 
   array_attribute :dependencies
 

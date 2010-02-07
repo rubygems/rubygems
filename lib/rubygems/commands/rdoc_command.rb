0bd4802d1e575518dfb1a364df3c5faad43452f0
Eric Hodel <drbrain@segment7.net>
2010-02-07 04:29:59 +0000

    Patch #25982
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@2352 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/rubygems/commands/rdoc_command.rb
+++ lib/rubygems/commands/rdoc_command.rb
@@ -8,7 +8,7 @@ class Gem::Commands::RdocCommand < Gem::Command
   def initialize
     super 'rdoc', 'Generates RDoc for pre-installed gems',
           :version => Gem::Requirement.default,
-          :include_rdoc => true, :include_ri => true
+          :include_rdoc => true, :include_ri => true, :overwrite => false
 
     add_option('--all',
                'Generate RDoc/RI documentation for all',
@@ -17,13 +17,18 @@ class Gem::Commands::RdocCommand < Gem::Command
     end
 
     add_option('--[no-]rdoc',
-               'Include RDoc generated documents') do |value, options|
+               'Generate RDoc HTML') do |value, options|
       options[:include_rdoc] = value
     end
 
     add_option('--[no-]ri',
-               'Include RI generated documents') do |value, options|
+               'Generate RI data') do |value, options|
       options[:include_ri] = value
+    end
+
+    add_option('--[no-]overwrite',
+               'Overwrite installed documents') do |value, options|
+      options[:overwrite] = value
     end
 
     add_version_option
@@ -34,7 +39,14 @@ class Gem::Commands::RdocCommand < Gem::Command
   end
 
   def defaults_str # :nodoc:
-    "--version '#{Gem::Requirement.default}' --rdoc --ri"
+    "--version '#{Gem::Requirement.default}' --rdoc --ri --no-overwrite"
+  end
+
+  def description # :nodoc:
+    <<-DESC
+The rdoc command builds RDoc and RI documentation for installed gems.  Use
+--overwrite to force rebuilding of documentation.
+    DESC
   end
 
   def usage # :nodoc:
@@ -58,7 +70,8 @@ class Gem::Commands::RdocCommand < Gem::Command
 
     if options[:include_ri]
       specs.sort.each do |spec|
-        Gem::DocManager.new(spec).generate_ri
+        doc = Gem::DocManager.new(spec)
+        doc.generate_ri if options[:overwrite] || !doc.ri_installed?
       end
 
       Gem::DocManager.update_ri_cache
@@ -66,11 +79,13 @@ class Gem::Commands::RdocCommand < Gem::Command
 
     if options[:include_rdoc]
       specs.sort.each do |spec|
-        Gem::DocManager.new(spec).generate_rdoc
+        doc = Gem::DocManager.new(spec)
+        doc.generate_rdoc if options[:overwrite] || !doc.rdoc_installed?
       end
     end
 
     true
   end
+
 end
 

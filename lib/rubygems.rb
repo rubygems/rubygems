90398750b468ad145e82599e9d6b7a0736fdeb60
Eric Hodel <drbrain@segment7.net>
2010-02-09 06:54:21 +0000

    merge 2390
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/branches/1_3@2391 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -98,7 +98,7 @@ require 'etc'
 # -The RubyGems Team
 
 module Gem
-  RubyGemsVersion = VERSION = '1.3.6'
+  RubyGemsVersion = VERSION = '1.3.6.pre.1'
 
   ##
   # Raised when RubyGems is unable to load or activate a gem.  Contains the

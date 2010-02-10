6ac7be2360fa6fd148f1c04d6383400dbc6cb4e9
Eric Hodel <drbrain@segment7.net>
2010-02-10 19:59:57 +0000

    2408
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/branches/1_3@2409 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/rubygems/config_file.rb
+++ lib/rubygems/config_file.rb
@@ -205,6 +205,8 @@ class Gem::ConfigFile
     dirname = File.dirname(credentials_path)
     Dir.mkdir(dirname) unless File.exists?(dirname)
 
+    require 'yaml'
+
     File.open(credentials_path, 'w') do |f|
       f.write config.to_yaml
     end
@@ -310,6 +312,7 @@ class Gem::ConfigFile
 
   # Writes out this config file, replacing its source.
   def write
+    require 'yaml'
     open config_file_name, 'w' do |io|
       io.write to_yaml
     end

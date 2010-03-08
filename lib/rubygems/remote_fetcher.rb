3cbfe0aa7a43674a068dcce1765bab52932b8b15
James Tucker <jftucker@gmail.com>
2010-03-08 19:33:31 +0000

    Retry on a read timeout when fetching gems
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubygems/trunk@2462 3d4018f9-ac1a-0410-99e9-8a154d859a19

--- lib/rubygems/remote_fetcher.rb
+++ lib/rubygems/remote_fetcher.rb
@@ -351,7 +351,9 @@ class Gem::RemoteFetcher
     # HACK work around EOFError bug in Net::HTTP
     # NOTE Errno::ECONNABORTED raised a lot on Windows, and make impossible
     # to install gems.
-    rescue EOFError, Errno::ECONNABORTED, Errno::ECONNRESET, Errno::EPIPE
+    rescue EOFError, Timeout::Error,
+           Errno::ECONNABORTED, Errno::ECONNRESET, Errno::EPIPE
+
       requests = @requests[connection.object_id]
       say "connection reset after #{requests} requests, retrying" if
         Gem.configuration.really_verbose

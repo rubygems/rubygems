29a10eed18de8fb4ced534bd1fc70be4de6820f6
raggi <jftucker@gmail.com>
2010-07-05 03:27:11 +0100

    Temporarily disable rubygems plugins autoloading, in order to reduce load times, look at source_index / cache file optimisation instead.

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -1159,5 +1159,6 @@ require 'rubygems/custom_require' if gem_disabled or RUBY_VERSION < '1.9'
 
 Gem.clear_paths
 
-Gem.load_plugins
+# XXX  performance outlier, this forces a full load of the source_index
+# Gem.load_plugins
 

efd5cc7de7f8feafc0cf427f548e227c829d4aae
raggi <jftucker@gmail.com>
2010-11-11 07:16:28 -0800

    Revert "Implement rubygems/fast which skips plugin loads, plugins can be loaded by requiring rubygems/plugins" - cleanup pre merge with evans branch
    This reverts commit 3720575bd1d6655d6825af7dea121bf4f22c5fa0.

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -1051,10 +1051,6 @@ module Gem
 
     alias cache source_index # an alias for the old name
 
-    # Internal helper to inform us if we were fastloaded, so plugins.rb should
-    # or should not load plugins.
-    attr_reader :fast
-
     # :startdoc:
 
   end
@@ -1169,5 +1165,6 @@ require 'rubygems/custom_require' if gem_disabled or RUBY_VERSION < '1.9'
 
 Gem.clear_paths
 
-require 'rubygems/plugins' unless Gem.fast
+# XXX  performance outlier, this forces a full load of the source_index
+# Gem.load_plugins
 
--- lib/rubygems/plugins.rb
+++
@@ -1,5 +0,0 @@
-=begin
-This file exists so that $" can effectively be used to load plugins once and
-only once.
-=end
-Gem.load_plugins

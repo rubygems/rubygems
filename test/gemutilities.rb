1f628c7315c95f9dab062bcde697db3124492587
John Barnette <jbarnette@gmail.com>
2010-11-15 13:52:42 -0600

    Fix shadowed local var.

--- test/gemutilities.rb
+++ test/gemutilities.rb
@@ -300,8 +300,8 @@ class RubyGemTestCase < MiniTest::Unit::TestCase
   def util_gem(name, version, deps = nil, &block)
     if deps then # fuck you eric
       block = proc do |s|
-        deps.each do |name, req|
-          s.add_dependency name, (req || '>= 0')
+        deps.each do |n, req|
+          s.add_dependency n, (req || '>= 0')
         end
       end
     end

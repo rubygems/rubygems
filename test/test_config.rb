55db7a400f504d87403898736920d443ef751366
John Barnette <jbarnette@gmail.com>
2010-11-15 13:56:10 -0600

    Silence and check for rbconfig/datadir deprecation warning in tests.

--- test/test_config.rb
+++ test/test_config.rb
@@ -4,8 +4,12 @@ require 'rubygems'
 class TestConfig < RubyGemTestCase
 
   def test_datadir
-    datadir = RbConfig::CONFIG['datadir']
-    assert_equal "#{datadir}/xyz", RbConfig.datadir('xyz')
+    _, err = capture_io do
+      datadir = RbConfig::CONFIG['datadir']
+      assert_equal "#{datadir}/xyz", RbConfig.datadir('xyz')
+    end
+
+    assert_match(/deprecate/, err)
   end
 
 end

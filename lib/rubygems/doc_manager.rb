586a04c9e28a1bb4ecbf146a813a8aa937b8033b
Marc-Andre Lafortune <github@marc-andre.ca>
2010-11-24 19:36:50 -0500

    Avoid unused variable warnings. See [redmine:3514]

--- lib/rubygems/doc_manager.rb
+++ lib/rubygems/doc_manager.rb
@@ -76,7 +76,7 @@ class Gem::DocManager
       :formatter => RDoc::RI::Formatter,
     }
 
-    driver = RDoc::RI::Driver.new(options).class_cache
+    RDoc::RI::Driver.new(options).class_cache
   end
 
   ##

d028ac207e70072fa494ee4f9abc11d40e8ed600
Eric Hodel <drbrain@segment7.net>
2010-12-03 18:04:44 -0800

    Test Gem::Security::build_self_signed_cert, ::sign_cert

--- test/test_gem_security.rb
+++ test/test_gem_security.rb
@@ -12,7 +12,6 @@ class TestGemSecurity < RubyGemTestCase
 
     assert_kind_of OpenSSL::X509::Certificate, cert
 
-
     assert_equal    2,                     cert.version
     assert_equal    0,                     cert.serial
     assert_equal    key.public_key.to_pem, cert.public_key.to_pem
@@ -34,6 +33,41 @@ class TestGemSecurity < RubyGemTestCase
 
     assert_equal name.to_s, cert.issuer.to_s
     assert_equal name.to_s, cert.subject.to_s
+  end
+
+  def test_class_build_self_signed_cert
+    email = 'nobody@example'
+    opt = {
+      :cert_age  => 60,
+      :key_size  => 512,
+      :save_cert => false,
+      :save_key  => false,
+    }
+
+    result = Gem::Security.build_self_signed_cert email, opt
+
+    key = result[:key]
+
+    assert_kind_of OpenSSL::PKey::RSA, key
+    # assert_equal 512, key.something_here
+
+    cert = result[:cert]
+
+    assert_equal '/CN=nobody/DC=example', cert.issuer.to_s
+  end
+
+  def test_class_sign_cert
+    name = OpenSSL::X509::Name.parse "CN=nobody/DC=example"
+    key  = OpenSSL::PKey::RSA.new 512
+    cert = OpenSSL::X509::Certificate.new
+
+    cert.subject    = name
+    cert.public_key = key.public_key
+
+    signed = Gem::Security.sign_cert cert, key, cert
+
+    assert cert.verify key
+    assert_equal name.to_s, signed.subject.to_s
   end
 
 end

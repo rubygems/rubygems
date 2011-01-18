b388ed417150bb259dbb6f46111c4dde1e86db0f
Chad Woolley <thewoolleyman@gmail.com>
2011-01-17 21:21:20 -0700

    revert to explicit sourcing of rvm environment file, due to issues with 'rvm use' not overriding rvm default interpreter

--- ci_build.sh
+++ ci_build.sh
@@ -2,7 +2,7 @@
 export rvm_install_on_use_flag=1
 export rvm_gemset_create_on_use_flag=1
 rvm use $1
-. "$HOME/.rvm/environments/ruby-$(rvm-prompt)"
+. "$HOME/.rvm/environments/ruby-$1" # There seems to be a bug where 'rvm use' from within a script doesn't override default interpreter, so we have to manually source the rvm environment file for the proper interpreter
 
 # TODO: How can you make Hoe's check_extra_deps not use sudo?
 #rake check_extra_deps default

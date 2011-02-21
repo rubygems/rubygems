79468a29d34fda633276c49af2d355d78d60b1fd
Eric Hodel <drbrain@segment7.net>
2011-02-21 13:28:31 -0800

    Fix new 1.9.3 warnings

--- lib/rubygems/spec_fetcher.rb
+++ lib/rubygems/spec_fetcher.rb
@@ -70,7 +70,7 @@ class Gem::SpecFetcher
   # Returns the local directory to write +uri+ to.
 
   def cache_dir(uri)
-    escaped_path = uri.path.sub(%r[^/([a-zA-z]):/], '/\\1-/') # Correct for windows paths
+    escaped_path = uri.path.sub(%r[^/([a-z]):/]i, '/\\1-/') # Correct for windows paths
     File.join @dir, "#{uri.host}%#{uri.port}", File.dirname(escaped_path)
   end
 

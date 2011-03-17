2815c4c5af81f5bf6e2290d073e605a3f6154905
Erik Hollensbe <erik@hollensbe.org>
2011-03-17 03:14:54 -0400

    Broke Gem::Path, Gem::FileSystem, and Gem::PathSupport into individual files.

--- lib/rubygems/path_support.rb
+++ lib/rubygems/path_support.rb
@@ -1,4 +1,4 @@
-require 'rubygems/path'
+require 'rubygems/file_system'
 
 module Gem
   class PathSupport
@@ -68,51 +68,6 @@ module Gem
       end
 
       @path = gem_path.map { |this_path| Gem::FS.new(this_path) }.uniq
-    end
-  end
-
-  class FS < Gem::Path
-    ##
-    # Default directories in a gem repository
-
-    DIRECTORIES = %w[cache doc gems specifications] unless defined?(DIRECTORIES)
-
-    ##
-    # Quietly ensure the named Gem directory contains all the proper
-    # subdirectories.  If we can't create a directory due to a permission
-    # problem, then we will silently continue.
-
-    def ensure_gem_subdirectories
-      require 'fileutils'
-
-      DIRECTORIES.each do |name|
-        fn = send(name)
-        FileUtils.mkdir_p fn rescue nil unless File.exist? fn
-      end
-    end
-
-    def bin
-      path.add 'bin'
-    end
-
-    def cache
-      path.add 'cache'
-    end
-
-    def specifications
-      path.add 'specifications'
-    end
-
-    def gems
-      path.add 'gems'
-    end
-
-    def doc
-      path.add 'doc'
-    end
-
-    def source_cache
-      path.add 'source_cache'
     end
   end
 end

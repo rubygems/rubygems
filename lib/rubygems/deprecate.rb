face2b9fc0857d70068d451a6fe308c796e37f0b
Ryan Davis <ryand-ruby@zenspider.com>
2011-03-30 17:09:29 -0700

    Added stupid simple deprecation module

---
+++ lib/rubygems/deprecate.rb
@@ -0,0 +1,48 @@
+##
+# Provides a single method +deprecate+ to be used to declare when
+# something is going away.
+#
+#     class Legacy
+#       def self.klass_method
+#         # ...
+#       end
+#
+#       def instance_method
+#         # ...
+#       end
+#
+#       extend Deprecate
+#       deprecate :instance_method, "X.z", 2011, 4
+#
+#       class << self
+#         extend Deprecate
+#         deprecate :klass_method, :none, 2011, 4
+#       end
+#     end
+
+module Deprecate
+
+  ##
+  # Simple deprecation method that deprecates +name+ by wrapping it up
+  # in a dummy method. It warns on each call to the dummy method
+  # telling the user of +repl+ (unless +repl+ is :none) and the
+  # year/month that it is planned to go away.
+
+  def deprecate name, repl, year, month
+    class_eval {
+      old = "_deprecated_#{name}"
+      alias_method old, name
+      define_method name do |*args|
+        target = Class == self.class ? "#{self}." : "#{self.class}#"
+        msg = [ "NOTE: #{target}#{name} is deprecated",
+                repl == :none ? " with no replacement" : ", use #{repl}",
+                ". It will be removed on or after %4d-%02d-01" % [year, month]
+              ]
+        warn "#{msg.join}."
+        send old, *args
+      end
+    }
+  end
+
+  module_function :deprecate
+end

2c6eadd5477cb49eaadae80b5ab7fc73389ab64c
Ryan Davis <ryand-ruby@zenspider.com>
2011-04-04 18:08:31 -0700

    formatting

--- lib/rubygems/dependency_installer.rb
+++ lib/rubygems/dependency_installer.rb
@@ -15,14 +15,14 @@ class Gem::DependencyInstaller
   attr_reader :installed_gems
 
   DEFAULT_OPTIONS = {
-    :env_shebang => false,
-    :domain => :both, # HACK dup
-    :force => false,
-    :format_executable => false, # HACK dup
+    :env_shebang         => false,
+    :domain              => :both, # HACK dup
+    :force               => false,
+    :format_executable   => false, # HACK dup
     :ignore_dependencies => false,
-    :prerelease => false,
-    :security_policy => nil, # HACK NoSecurity requires OpenSSL.  AlmostNo? Low?
-    :wrappers => true,
+    :prerelease          => false,
+    :security_policy     => nil, # HACK NoSecurity requires OpenSSL. AlmostNo? Low?
+    :wrappers            => true,
   }
 
   ##
@@ -53,17 +53,17 @@ class Gem::DependencyInstaller
 
     options = DEFAULT_OPTIONS.merge options
 
-    @bin_dir = options[:bin_dir]
-    @development = options[:development]
-    @domain = options[:domain]
-    @env_shebang = options[:env_shebang]
-    @force = options[:force]
-    @format_executable = options[:format_executable]
+    @bin_dir             = options[:bin_dir]
+    @development         = options[:development]
+    @domain              = options[:domain]
+    @env_shebang         = options[:env_shebang]
+    @force               = options[:force]
+    @format_executable   = options[:format_executable]
     @ignore_dependencies = options[:ignore_dependencies]
-    @prerelease = options[:prerelease]
-    @security_policy = options[:security_policy]
-    @user_install = options[:user_install]
-    @wrappers = options[:wrappers]
+    @prerelease          = options[:prerelease]
+    @security_policy     = options[:security_policy]
+    @user_install        = options[:user_install]
+    @wrappers            = options[:wrappers]
 
     @installed_gems = []
 

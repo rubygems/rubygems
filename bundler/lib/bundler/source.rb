rubygems/bundler

Provided more helpful feedback when a git command fails.

lib/bundler/source.rb
@@ -560,7 +560,9 @@ module Bundler
           out = %x{git #{command}}
 
           if $?.exitstatus != 0
-            raise GitError, "An error has occurred in git when running `git #{command}`. Cannot complete bundling."
+            error_message = "An error has occurred in git when running `git #{command}`. Cannot complete bundling."
+            error_message << "\nIf this error persists you could try removing the cache directory '#{cache_path}'" if cached?
+            raise GitError, error_message
           end
           out
         else

rubygems/rubygems

bundler/lib/bundler/source.rb
@@ -560,9 +560,8 @@ module Bundler
           out = %x{git #{command}}
 
           if $?.exitstatus != 0
-            msg = "Git error: " +
-              "command `git #{command}` in directory #{Dir.pwd} has failed.\n" +
-              "Cannot complete bundling."
+            msg = "Git error: command `git #{command}` in directory #{Dir.pwd} has failed."
+            msg << "\nIf this error persists you could try removing the cache directory '#{cache_path}'" if cached?
             raise GitError, msg
           end
           out
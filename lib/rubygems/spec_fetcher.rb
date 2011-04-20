24514a1157caf834c9b30fc514c82fa209bbc472
Ryan Davis <ryand-ruby@zenspider.com>
2011-04-20 13:57:06 -0700

    minor cleanup

--- lib/rubygems/spec_fetcher.rb
+++ lib/rubygems/spec_fetcher.rb
@@ -85,6 +85,7 @@ class Gem::SpecFetcher
                         all               = false,
                         matching_platform = true,
                         prerelease        = false)
+
     specs_and_sources, errors = find_matching_with_errors(dependency,
                                                           all,
                                                           matching_platform,
@@ -138,7 +139,10 @@ class Gem::SpecFetcher
   # matching released versions are returned.  If +matching_platform+
   # is false, gems for all platforms are returned.
 
-  def find_matching_with_errors(dependency, all = false, matching_platform = true, prerelease = false)
+  def find_matching_with_errors(dependency,
+                                all               = false,
+                                matching_platform = true,
+                                prerelease        = false)
     found = {}
 
     rejected_specs = {}

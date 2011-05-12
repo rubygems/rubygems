ff140d5bbea2e8c9f95eda8c4d70a0ed6b2ad6d4
Ryan Davis <ryand-ruby@zenspider.com>
2011-05-11 17:27:41 -0700

    prepping for release

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -120,7 +120,7 @@ require "rubygems/deprecate"
 # -The RubyGems Team
 
 module Gem
-  VERSION = '1.8.1'
+  VERSION = '1.8.2'
 
   ##
   # Raised when RubyGems is unable to load or activate a gem.  Contains the

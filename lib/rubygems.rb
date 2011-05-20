4b5f37dee5eddfa378dd668bd284848f646fe117
Eric Hodel <drbrain@segment7.net>
2011-05-19 17:22:04 -0700

    Prep for release of 1.8.3

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -120,7 +120,7 @@ require "rubygems/deprecate"
 # -The RubyGems Team
 
 module Gem
-  VERSION = '1.8.2'
+  VERSION = '1.8.3'
 
   ##
   # Raised when RubyGems is unable to load or activate a gem.  Contains the

820924c02f6067d827b7f41a6bf3a90e218d64f7
Ryan Davis <ryand-ruby@zenspider.com>
2011-05-25 19:34:08 -0700

    prepping for 1.8.4 release
    Conflicts:
    
    	History.txt

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -121,7 +121,7 @@ require "rubygems/deprecate"
 # -The RubyGems Team
 
 module Gem
-  VERSION = '1.8.3'
+  VERSION = '1.8.4'
 
   ##
   # Raised when RubyGems is unable to load or activate a gem.  Contains the

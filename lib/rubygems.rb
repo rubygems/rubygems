6be69fdeeca9199a526ac7ec3b8f59d8d432a6c8
Evan Phoenix <evan@fallingsnow.net>
2011-05-31 23:52:01 -0700

    Minor perf improvement

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -963,9 +963,10 @@ module Gem
 
   def self.loaded_path? path
     # TODO: ruby needs a feature to let us query what's loaded in 1.8 and 1.9
-    $LOADED_FEATURES.find { |s|
-      s =~ /(^|\/)#{Regexp.escape path.to_s}#{Regexp.union(*Gem.suffixes)}$/
-    }
+
+    re = /(^|\/)#{Regexp.escape path.to_s}#{Regexp.union(*Gem.suffixes)}$/
+
+    $LOADED_FEATURES.find { |s| s =~ re }
   end
 
   ##

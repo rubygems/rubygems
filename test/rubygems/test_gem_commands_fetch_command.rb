040d8d490e461eb710738225b0cc555be347efd2
Evan Phoenix <evan@fallingsnow.net>
2011-06-01 00:02:59 -0700

    Fix fetching a specific prerelease version

--- test/rubygems/test_gem_commands_fetch_command.rb
+++ test/rubygems/test_gem_commands_fetch_command.rb
@@ -53,6 +53,30 @@ class TestGemCommandsFetchCommand < Gem::TestCase
            "#{@a2_pre.full_name} not fetched"
   end
 
+  def test_execute_specific_prerelease
+    util_setup_fake_fetcher true
+    util_clear_gems
+    util_setup_spec_fetcher @a2, @a2_pre
+
+    @fetcher.data["#{@gem_repo}gems/#{@a2.file_name}"] =
+      File.read(@a2.cache_file)
+    @fetcher.data["#{@gem_repo}gems/#{@a2_pre.file_name}"] =
+      File.read(@a2_pre.cache_file)
+
+    @cmd.options[:args] = [@a2.name]
+    @cmd.options[:prerelease] = true
+    @cmd.options[:version] = "2.a"
+
+    use_ui @ui do
+      Dir.chdir @tempdir do
+        @cmd.execute
+      end
+    end
+
+    assert File.exist?(File.join(@tempdir, @a2_pre.file_name)),
+           "#{@a2_pre.full_name} not fetched"
+  end
+
   def test_execute_version
     util_setup_fake_fetcher
     util_setup_spec_fetcher @a1, @a2

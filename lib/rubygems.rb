f828aa83d6dff3c82d70baef85d7a3c22448c6cc
Aaron Patterson <aaron.patterson@gmail.com>
2011-06-08 11:47:29 -0700

    move .rb to the beginning of the suffixes list.  require without rb extension is more common

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -995,8 +995,8 @@ module Gem
   # Suffixes for require-able paths.
 
   def self.suffixes
-    @suffixes ||= ['',
-                   '.rb',
+    @suffixes ||= ['.rb',
+                   '',
                    *%w(DLEXT DLEXT2).map { |key|
                      val = RbConfig::CONFIG[key]
                      next unless val and not val.empty?

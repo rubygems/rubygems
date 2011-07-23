76d1ccce1126485d4ec0099e84cccae8e6b7fa50
Eric Hodel <drbrain@segment7.net>
2011-07-23 10:30:20 -0700

    ! Restore behavior of Gem::Specification#loaded?  Ruby Bug #5032

--- lib/rubygems/specification.rb
+++ lib/rubygems/specification.rb
@@ -213,9 +213,9 @@ class Gem::Specification
   ##
   # True when this gemspec has been activated. This attribute is not persisted.
 
-  attr_accessor :loaded
+  attr_accessor :loaded # :nodoc:
 
-  alias :loaded? :loaded
+  alias :loaded? :loaded # :nodoc:
 
   ##
   # True when this gemspec has been activated. This attribute is not persisted.
@@ -683,6 +683,7 @@ class Gem::Specification
     spec.instance_variable_set :@platform,                  array[16].to_s
     spec.instance_variable_set :@license,                   array[17]
     spec.instance_variable_set :@loaded,                    false
+    spec.instance_variable_set :@activated,                 false
 
     spec
   end
@@ -742,7 +743,8 @@ class Gem::Specification
     add_self_to_load_path
 
     Gem.loaded_specs[self.name] = self
-    self.activated = true
+    @activated = true
+    @loaded = true
 
     return true
   end
@@ -1318,6 +1320,7 @@ class Gem::Specification
 
   def initialize name = nil, version = nil
     @loaded = false
+    @activated = false
     @loaded_from = nil
     @original_platform = nil
 

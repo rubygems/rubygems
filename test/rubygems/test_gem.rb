7e0771d3cc7401784a6bb762c0ac6641f94d08db
Eric Hodel <drbrain@segment7.net>
2011-07-23 10:18:18 -0700

    Gem::Deprecate is still Deprecate in 1.8

--- test/rubygems/test_gem.rb
+++ test/rubygems/test_gem.rb
@@ -1086,7 +1086,7 @@ class TestGem < Gem::TestCase
     # @abin_path = File.join spec.full_gem_path, spec.bindir, 'abin'
     # FileUtils.mkdir_p File.join(stem, "gems", "test-3")
 
-    Gem::Deprecate.skip_during do
+    Deprecate.skip_during do
       expected = [File.join(@gemhome, "gems", "a-4", "lib")]
       assert_equal expected, Gem.latest_load_paths
     end

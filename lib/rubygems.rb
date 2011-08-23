dd8e55359c595b18c23c07e324ceddf44cace5e5
Eric Hodel <drbrain@segment7.net>
2011-08-23 15:17:07 -0700

    Update history for 1.8.9 release

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -118,7 +118,7 @@ require "rubygems/deprecate"
 # -The RubyGems Team
 
 module Gem
-  VERSION = '1.8.8'
+  VERSION = '1.8.9'
 
   ##
   # Raised when RubyGems is unable to load or activate a gem.  Contains the

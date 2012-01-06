0034e05324c5c62fd37e55464dac9b371b9210e3
Evan Phoenix <evan@phx.io>
2012-01-06 11:32:09 -0800

    Bump version and History

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -118,7 +118,7 @@ require "rubygems/deprecate"
 # -The RubyGems Team
 
 module Gem
-  VERSION = '1.8.14'
+  VERSION = '1.8.15'
 
   ##
   # Raised when RubyGems is unable to load or activate a gem.  Contains the

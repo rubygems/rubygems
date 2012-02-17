83804c03cfa484bbbaa160afb5b42cacb56b8a69
Evan Phoenix <evan@phx.io>
2012-02-17 10:01:26 -0800

    Define SUCKAGE better, ie only MRI 1.9.2. Fixes #276

--- lib/rubygems/compatibility.rb
+++ lib/rubygems/compatibility.rb
@@ -6,7 +6,9 @@
 # support them, so we define some constants to use later.
 module Gem
   QUICKLOADER_SUCKAGE = RUBY_VERSION =~ /^1\.9\.1/
-  GEM_PRELUDE_SUCKAGE = RUBY_VERSION =~ /^1\.9\.2/
+
+  # Only MRI 1.9.2 has the custom prelude.
+  GEM_PRELUDE_SUCKAGE = RUBY_VERSION =~ /^1\.9\.2/ and RUBY_ENGINE == "ruby"
 end
 
 # Gem::QuickLoader exists in the gem prelude code in ruby 1.9.2 itself.

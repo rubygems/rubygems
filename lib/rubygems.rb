3316bdd507c18bf7b4202a9bd547936537003a3d
Evan Phoenix <evan@fallingsnow.net>
2012-03-11 21:30:59 -0700

    Bump version to 1.8.18

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -120,7 +120,7 @@ require "rubygems/deprecate"
 # -The RubyGems Team
 
 module Gem
-  VERSION = '1.8.17'
+  VERSION = '1.8.18'
 
   ##
   # Raised when RubyGems is unable to load or activate a gem.  Contains the

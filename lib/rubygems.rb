2637b4b752949bb7ecc93c649c89ac9240c5f889
Evan Phoenix <evan@phx.io>
2012-03-22 15:28:41 -0700

    Bump to 1.8.21

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -120,7 +120,7 @@ require "rubygems/deprecate"
 # -The RubyGems Team
 
 module Gem
-  VERSION = '1.8.20'
+  VERSION = '1.8.21'
 
   ##
   # Raised when RubyGems is unable to load or activate a gem.  Contains the

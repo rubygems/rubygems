c7c9deba169c7972a92ac855d19879125ebf7936
Kouhei Sutou <kou@clear-code.com>
2012-09-16 10:15:40 +0900

    Remove deprecated Gem.activate_spec
    It is described that "will be removed on or after 2011-06".

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -188,10 +188,6 @@ module Gem
     Gem::Specification.find_by_name(dep, *requirements).activate
   end
 
-  def self.activate_spec spec # :nodoc:
-    spec.activate
-  end
-
   # DOC: This needs to be documented or nodoc'd.
 
   def self.unresolved_deps
@@ -1310,7 +1306,6 @@ require 'rubygems/custom_require'
 module Gem
   class << self
     extend Gem::Deprecate
-    deprecate :activate_spec,         "Specification#activate", 2011,  6
     deprecate :activate,              "Specification#activate", 2011, 10
     deprecate :all_load_paths,        :none,                    2011, 10
     deprecate :all_partials,          :none,                    2011, 10

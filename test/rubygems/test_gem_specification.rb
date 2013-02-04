1e03ec44cd414e67ee63a5590e52f1fccf2915fb
Eric Hodel <drbrain@segment7.net>
2013-02-04 15:24:52 -0800

    Use #send, remove_instance_variable is private

--- test/rubygems/test_gem_specification.rb
+++ test/rubygems/test_gem_specification.rb
@@ -119,7 +119,7 @@ end
   end
 
   def test_self_from_yaml
-    @a1.remove_instance_variable :@specification_version
+    @a1.send :remove_instance_variable, :@specification_version
 
     spec = Gem::Specification.from_yaml @a1.to_yaml
 

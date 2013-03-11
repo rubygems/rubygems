6ac38c850df2c1c4acc2464c55968ea89c3479f0
Eric Hodel <drbrain@segment7.net>
2013-03-11 10:58:56 -0700

    Fixup for @75a793e

--- test/rubygems/test_gem_spec_fetcher.rb
+++ test/rubygems/test_gem_spec_fetcher.rb
@@ -245,6 +245,7 @@ class TestGemSpecFetcher < Gem::TestCase
 
     source       = Gem::Source.new URI 'http://example'
     same_source  = nil
+    https_source = nil
 
     use_ui @ui do
       same_source = @sf.upgrade_http_source source

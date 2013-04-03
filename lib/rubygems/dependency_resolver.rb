6c752d58bfb9a52c2f58de22e6f6d8d5dde41f2a
Eric Hodel <drbrain@segment7.net>
2013-04-03 15:54:47 -0700

    Moved IndexSet to its own file

--- lib/rubygems/dependency_resolver.rb
+++ lib/rubygems/dependency_resolver.rb
@@ -71,58 +71,6 @@ module Gem
   #
   class DependencyResolver
 
-    # The global rubygems pool represented via the traditional
-    # source index.
-    #
-    class IndexSet
-      def initialize
-        @f = Gem::SpecFetcher.fetcher
-
-        @all = Hash.new { |h,k| h[k] = [] }
-
-        list, _ = @f.available_specs(:released)
-        list.each do |uri, specs|
-          specs.each do |n|
-            @all[n.name] << [uri, n]
-          end
-        end
-
-        @specs = {}
-      end
-
-      # Return an array of IndexSpecification objects matching
-      # DependencyRequest +req+.
-      #
-      def find_all(req)
-        res = []
-
-        name = req.dependency.name
-
-        @all[name].each do |uri, n|
-          if req.dependency.match? n
-            res << IndexSpecification.new(self, n.name, n.version,
-                                          uri, n.platform)
-          end
-        end
-
-        res
-      end
-
-      # No prefetching needed since we load the whole index in
-      # initially.
-      #
-      def prefetch(gems)
-      end
-
-      # Called from IndexSpecification to get a true Specification
-      # object.
-      #
-      def load_spec(name, ver, source)
-        key = "#{name}-#{ver}"
-        @specs[key] ||= source.fetch_spec(Gem::NameTuple.new(name, ver))
-      end
-    end
-
     # A set which represents the installed gems. Respects
     # all the normal settings that control where to look
     # for installed gems.
@@ -358,6 +306,7 @@ require 'rubygems/dependency_resolver/api_specification'
 require 'rubygems/dependency_resolver/activation_request'
 require 'rubygems/dependency_resolver/dependency_conflict'
 require 'rubygems/dependency_resolver/dependency_request'
+require 'rubygems/dependency_resolver/index_set'
 require 'rubygems/dependency_resolver/index_specification'
 require 'rubygems/dependency_resolver/installed_specification'
 require 'rubygems/dependency_resolver/installer_set'

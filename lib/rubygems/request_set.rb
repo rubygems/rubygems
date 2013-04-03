0044cc4b6a0a416dbe44593cc99ef4501365bc0b
Eric Hodel <drbrain@segment7.net>
2013-04-03 16:36:59 -0700

    Alphabetize RequestSet

--- lib/rubygems/request_set.rb
+++ lib/rubygems/request_set.rb
@@ -14,14 +14,16 @@ class Gem::RequestSet
 
   attr_reader :always_install
 
-  # Treat missing dependencies as silent errors
-  attr_accessor :soft_missing
-
   attr_reader :dependencies
 
   attr_accessor :development
 
-  def initialize(*deps)
+  ##
+  # Treat missing dependencies as silent errors
+
+  attr_accessor :soft_missing
+
+  def initialize *deps
     @dependencies = deps
 
     @always_install = []
@@ -31,79 +33,46 @@ class Gem::RequestSet
     yield self if block_given?
   end
 
-  # Declare that a gem of name +name+ with +reqs+ requirements
-  # is needed.
-  #
-  def gem(name, *reqs)
+  ##
+  # Declare that a gem of name +name+ with +reqs+ requirements is needed.
+
+  def gem name, *reqs
     @dependencies << Gem::Dependency.new(name, reqs)
   end
 
-  # Add +deps+ Gem::Depedency objects to the set.
-  #
-  def import(deps)
+  ##
+  # Add +deps+ Gem::Dependency objects to the set.
+
+  def import deps
     @dependencies += deps
   end
 
-  # Resolve the requested dependencies and return an Array of
-  # Specification objects to be activated.
-  #
-  def resolve set = nil
-    resolver = Gem::DependencyResolver.new @dependencies, set
-    resolver.development  = @development
-    resolver.soft_missing = @soft_missing
+  def install options, &block
+    if dir = options[:install_dir]
+      return install_into dir, false, options, &block
+    end
 
-    @requests = resolver.resolve
-  end
+    cache_dir = options[:cache_dir] || Gem.dir
 
-  # Resolve the requested dependencies against the gems
-  # available via Gem.path and return an Array of Specification
-  # objects to be activated.
-  #
-  def resolve_current
-    resolve Gem::DependencyResolver::CurrentSet.new
-  end
+    specs = []
 
-  # Load a dependency management file.
-  #
-  def load_gemdeps(path)
-    gf = Gem::RequestSet::GemDepedencyAPI.new(self, path)
-    gf.load
-  end
-
-  def specs
-    @specs ||= @requests.map { |r| r.full_spec }
-  end
-
-  def tsort_each_node(&block)
-    @requests.each(&block)
-  end
-
-  def tsort_each_child(node)
-    node.spec.dependencies.each do |dep|
-      next if dep.type == :development and not @development
-
-      match = @requests.find { |r| dep.match? r.spec.name, r.spec.version }
-      if match
-        begin
-          yield match
-        rescue TSort::Cyclic
-        end
-      else
-        unless @soft_missing
-          raise Gem::DependencyError, "Unresolved depedency found during sorting - #{dep}"
-        end
+    sorted_requests.each do |req|
+      if req.installed? and
+         @always_install.none? { |spec| spec == req.spec.spec } then
+        yield req, nil if block_given?
+        next
       end
-    end
-  end
 
-  def sorted_requests
-    @sorted ||= strongly_connected_components.flatten
-  end
+      path = req.download cache_dir
 
-  def specs_in(dir)
-    Dir["#{dir}/specifications/*.gemspec"].map do |g|
-      Gem::Specification.load g
+      inst = Gem::Installer.new path, options
+
+      yield req, inst if block_given?
+
+      specs << inst.install
     end
+
+    specs
   end
 
   def install_into dir, force = true, options = {}
@@ -142,33 +111,70 @@ class Gem::RequestSet
     installed
   end
 
-  def install options, &block
-    if dir = options[:install_dir]
-      return install_into dir, false, options, &block
-    end
+  ##
+  # Load a dependency management file.
 
-    cache_dir = options[:cache_dir] || Gem.dir
-
-    specs = []
-
-    sorted_requests.each do |req|
-      if req.installed? and
-         @always_install.none? { |spec| spec == req.spec.spec } then
-        yield req, nil if block_given?
-        next
-      end
-
-      path = req.download cache_dir
-
-      inst = Gem::Installer.new path, options
-
-      yield req, inst if block_given?
-
-      specs << inst.install
-    end
-
-    specs
+  def load_gemdeps path
+    gf = Gem::RequestSet::GemDepedencyAPI.new self, path
+    gf.load
   end
+
+  ##
+  # Resolve the requested dependencies and return an Array of Specification
+  # objects to be activated.
+
+  def resolve set = nil
+    resolver = Gem::DependencyResolver.new @dependencies, set
+    resolver.development  = @development
+    resolver.soft_missing = @soft_missing
+
+    @requests = resolver.resolve
+  end
+
+  ##
+  # Resolve the requested dependencies against the gems available via Gem.path
+  # and return an Array of Specification objects to be activated.
+
+  def resolve_current
+    resolve Gem::DependencyResolver::CurrentSet.new
+  end
+
+  def sorted_requests
+    @sorted ||= strongly_connected_components.flatten
+  end
+
+  def specs
+    @specs ||= @requests.map { |r| r.full_spec }
+  end
+
+  def specs_in dir
+    Dir["#{dir}/specifications/*.gemspec"].map do |g|
+      Gem::Specification.load g
+    end
+  end
+
+  def tsort_each_node &block # :nodoc:
+    @requests.each(&block)
+  end
+
+  def tsort_each_child node # :nodoc:
+    node.spec.dependencies.each do |dep|
+      next if dep.type == :development and not @development
+
+      match = @requests.find { |r| dep.match? r.spec.name, r.spec.version }
+      if match
+        begin
+          yield match
+        rescue TSort::Cyclic
+        end
+      else
+        unless @soft_missing
+          raise Gem::DependencyError, "Unresolved depedency found during sorting - #{dep}"
+        end
+      end
+    end
+  end
+
 end
 
 require 'rubygems/request_set/gem_dependency_api'

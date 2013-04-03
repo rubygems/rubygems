2ba9ec9a43651b47a5aa0cb0852b7e99b8bea9d9
Eric Hodel <drbrain@segment7.net>
2013-04-03 15:33:41 -0700

    Moved APISpecification to its own file

--- lib/rubygems/dependency_resolver.rb
+++ lib/rubygems/dependency_resolver.rb
@@ -71,38 +71,6 @@ module Gem
   #
   class DependencyResolver
 
-    # Represents a specification retrieved via the rubygems.org
-    # API. This is used to avoid having to load the full
-    # Specification object when all we need is the name, version,
-    # and dependencies.
-    #
-    class APISpecification
-      attr_reader :set # :nodoc:
-
-      def initialize(set, api_data)
-        @set = set
-        @name = api_data[:name]
-        @version = Gem::Version.new api_data[:number]
-        @dependencies = api_data[:dependencies].map do |name, ver|
-          Gem::Dependency.new name, ver.split(/\s*,\s*/)
-        end
-      end
-
-      attr_reader :name, :version, :dependencies
-
-      def == other # :nodoc:
-        self.class === other and
-          @set          == other.set and
-          @name         == other.name and
-          @version      == other.version and
-          @dependencies == other.dependencies
-      end
-
-      def full_name
-        "#{@name}-#{@version}"
-      end
-    end
-
     # The global rubygems pool, available via the rubygems.org API.
     # Returns instances of APISpecification.
     #
@@ -630,6 +598,7 @@ module Gem
   end
 end
 
+require 'rubygems/dependency_resolver/api_specification'
 require 'rubygems/dependency_resolver/installed_specification'
 require 'rubygems/dependency_resolver/installer_set'
 

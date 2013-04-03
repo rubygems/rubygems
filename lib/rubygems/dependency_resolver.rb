a63250cea0aafa2c03131b1199788a7813f868ae
Eric Hodel <drbrain@segment7.net>
2013-04-03 15:45:22 -0700

    Moved APISet to its own file

--- lib/rubygems/dependency_resolver.rb
+++ lib/rubygems/dependency_resolver.rb
@@ -71,65 +71,6 @@ module Gem
   #
   class DependencyResolver
 
-    # The global rubygems pool, available via the rubygems.org API.
-    # Returns instances of APISpecification.
-    #
-    class APISet
-      def initialize
-        @data = Hash.new { |h,k| h[k] = [] }
-        @dep_uri = URI 'https://rubygems.org/api/v1/dependencies'
-      end
-
-      # Return data for all versions of the gem +name+.
-      #
-      def versions(name)
-        if @data.key?(name)
-          return @data[name]
-        end
-
-        uri = @dep_uri + "?gems=#{name}"
-        str = Gem::RemoteFetcher.fetcher.fetch_path uri
-
-        Marshal.load(str).each do |ver|
-          @data[ver[:name]] << ver
-        end
-
-        @data[name]
-      end
-
-      # Return an array of APISpecification objects matching
-      # DependencyRequest +req+.
-      #
-      def find_all(req)
-        res = []
-
-        versions(req.name).each do |ver|
-          if req.dependency.match? req.name, ver[:number]
-            res << APISpecification.new(self, ver)
-          end
-        end
-
-        res
-      end
-
-      # A hint run by the resolver to allow the Set to fetch
-      # data for DependencyRequests +reqs+.
-      #
-      def prefetch(reqs)
-        names = reqs.map { |r| r.dependency.name }
-        needed = names.find_all { |d| !@data.key?(d) }
-
-        return if needed.empty?
-
-        uri = @dep_uri + "?gems=#{needed.sort.join ','}"
-        str = Gem::RemoteFetcher.fetcher.fetch_path uri
-
-        Marshal.load(str).each do |ver|
-          @data[ver[:name]] << ver
-        end
-      end
-    end
-
     # Represents a possible Specification object returned
     # from IndexSet. Used to delay needed to download full
     # Specification objects when only the +name+ and +version+
@@ -447,6 +388,7 @@ module Gem
   end
 end
 
+require 'rubygems/dependency_resolver/api_set'
 require 'rubygems/dependency_resolver/api_specification'
 require 'rubygems/dependency_resolver/activation_request'
 require 'rubygems/dependency_resolver/dependency_conflict'

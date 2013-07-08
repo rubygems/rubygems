ba0bbec3d2a74c2e55312a54adc442f0818648be
Eric Hodel <drbrain@segment7.net>
2013-07-08 15:28:21 -0700

    Update error message assertion for ruby 2.1

--- test/rubygems/test_gem_package.rb
+++ test/rubygems/test_gem_package.rb
@@ -598,7 +598,8 @@ class TestGemPackage < Gem::Package::TarTestCase
       package.verify
     end
 
-    assert_equal 'No such file or directory - nonexistent.gem', e.message
+    assert_match %r%^No such file or directory%, e.message
+    assert_match %r%nonexistent.gem$%,           e.message
   end
 
   def test_verify_security_policy

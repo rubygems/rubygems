ca228b76b95f66f6dcbefd8e705eef5b7c7f120d
Eric Hodel <drbrain@segment7.net>
2013-07-25 15:02:48 -0700

    Encrypt test key with DES-CBC for JRuby
    AES-256-CBC does not work for encrypting a key on JRuby, see jruby/jruby#919
    
    Part of #606

---
+++ util/create_encrypted_key.rb
@@ -0,0 +1,16 @@
+require 'openssl'
+
+private_key_path = '../../test/rubygems/private_key.pem'
+private_key_path = File.expand_path private_key_path, __FILE__
+
+key = OpenSSL::PKey::RSA.new File.read private_key_path
+
+cipher = OpenSSL::Cipher.new 'DES-CBC'
+
+encrypted_key_path = '../../test/rubygems/encrypted_private_key.pem'
+encrypted_key_path = File.expand_path encrypted_key_path, __FILE__
+
+open encrypted_key_path, 'w' do |io|
+  io.write key.to_pem cipher, 'Foo bar'
+end
+

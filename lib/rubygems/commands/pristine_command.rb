0a20d5e6107064da215787dffea551aac5802bb3
Eric Hodel <drbrain@segment7.net>
2013-08-12 17:44:04 -0700

    The pristine command downloads missing .gem files now

--- lib/rubygems/commands/pristine_command.rb
+++ lib/rubygems/commands/pristine_command.rb
@@ -58,8 +58,7 @@ If you have made modifications to an installed gem, the pristine command
 will revert them.  After all the gem's files have been checked extensions are
 rebuilt and all bin stubs for the gem are regenerated.
 
-If the cached gem cannot be found, you will need to use `gem install` to
-revert the gem.
+If the cached gem cannot be found it will be downloaded.
 
 If --no-extensions is provided pristine will not attempt to restore gems
 with extensions.

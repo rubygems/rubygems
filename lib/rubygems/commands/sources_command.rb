rubygems/rubygems

commit d58c5b2abda70c6819950e0174d61f05e1a3d3d1

Extract list? from SourcesCommand#execute

lib/rubygems/commands/sources_command.rb
@@ -106,12 +106,15 @@ Do you want to add this insecure source?
     end
   end
 
-  def execute
-    options[:list] = !(options[:add] ||
-                       options[:clear_all] ||
-                       options[:remove] ||
-                       options[:update])
+  def list? # :nodoc:
+    !(options[:list] ||
+      options[:add] ||
+      options[:clear_all] ||
+      options[:remove] ||
+      options[:update])
+  end
 
+  def execute
     clear_all if options[:clear_all]
 
     source_uri = options[:add]
@@ -122,7 +125,7 @@ Do you want to add this insecure source?
 
     update if options[:update]
 
-    list if options[:list]
+    list if list?
   end
 
   def remove_source source_uri # :nodoc:

commit f77671b8495bdea083eb08a8e43924094a1169ff

Fix bug introduced by d58c5b2 for gem sources list
When "--list" or "-l" is passed to "gem sources", the command is
currently ignoring it.

lib/rubygems/commands/sources_command.rb
@@ -154,8 +154,7 @@ To remove a source use the --remove argument:
   end
 
   def list? # :nodoc:
-    !(options[:list] ||
-      options[:add] ||
+    !(options[:add] ||
       options[:clear_all] ||
       options[:remove] ||
       options[:update])
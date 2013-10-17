d5a9dc997d0487c27a45e004338a0685f951c5ba
Eric Hodel <drbrain@segment7.net>
2013-10-16 17:53:37 -0700

    Document GemDependencyAPI

--- lib/rubygems/request_set/gem_dependency_api.rb
+++ lib/rubygems/request_set/gem_dependency_api.rb
@@ -1,5 +1,5 @@
 ##
-# A semi-compatible DSL for Bundler's Gemfile format
+# A semi-compatible DSL for the Bundler Gemfile and Isolate formats.
 
 class Gem::RequestSet::GemDependencyAPI
 
@@ -7,6 +7,10 @@ class Gem::RequestSet::GemDependencyAPI
   # The dependency groups created by #group in the dependency API file.
 
   attr_reader :dependency_groups
+
+  ##
+  # Creates a new GemDependencyAPI that will add dependencies to the
+  # Gem::RequestSet +set+ based on the dependency API description in +path+.
 
   def initialize set, path
     @set = set
@@ -16,15 +20,25 @@ class Gem::RequestSet::GemDependencyAPI
     @dependency_groups = Hash.new { |h, group| h[group] = [] }
   end
 
+  ##
+  # Loads the gem dependency file
+
   def load
     instance_eval File.read(@path).untaint, @path, 1
   end
 
-  # :category: Bundler Gemfile DSL
+  ##
+  # :category: Gem Dependencies DSL
+  # :call-seq:
+  #   gem(name)
+  #   gem(name, *requirements)
+  #   gem(name, *requirements, options)
+  #
+  # Specifies a gem dependency with the given +name+ and +requirements+.  You
+  # may also supply +options+ following the +requirements+
 
-  def gem name, *reqs
-    # Ignore the opts for now.
-    options = reqs.pop if reqs.last.kind_of?(Hash)
+  def gem name, *requirements
+    options = requirements.pop if requirements.last.kind_of?(Hash)
     options ||= {}
 
     groups =
@@ -34,12 +48,12 @@ class Gem::RequestSet::GemDependencyAPI
 
     if groups then
       groups.each do |group|
-        gem_arguments = [name, *reqs]
+        gem_arguments = [name, *requirements]
         gem_arguments << options unless options.empty?
         @dependency_groups[group] << gem_arguments
       end
     else
-      @set.gem name, *reqs
+      @set.gem name, *requirements
     end
   end
 
@@ -51,6 +65,7 @@ class Gem::RequestSet::GemDependencyAPI
   end
 
   ##
+  # :category: Gem Dependencies DSL
   # Block form for placing a dependency in the given +groups+.
 
   def group *groups
@@ -62,15 +77,22 @@ class Gem::RequestSet::GemDependencyAPI
     @current_groups = nil
   end
 
+  ##
+  # :category: Gem Dependencies DSL
+
   def platform what
     if what == :ruby
       yield
     end
   end
 
+  ##
+  # :category: Gem Dependencies DSL
+
   alias :platforms :platform
 
   ##
+  # :category: Gem Dependencies DSL
   # Restricts this gem dependencies file to the given ruby +version+.  The
   # +:engine+ options from Bundler are currently ignored.
 
@@ -82,6 +104,9 @@ class Gem::RequestSet::GemDependencyAPI
 
     raise Gem::RubyVersionMismatch, message
   end
+
+  ##
+  # :category: Gem Dependencies DSL
 
   def source url
   end

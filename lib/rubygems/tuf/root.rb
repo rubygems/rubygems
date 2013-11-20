91407cd77f6cd0f45e1d2731a03a706141aed4fe
Tony Arcieri <tonyarcieri@squareup.com>
2013-11-20 12:08:17 -0800

    root.txt fixture and TUF-specific certs
    Keys that can be reused across TUF tests and a root.txt that contains
    them

--- lib/rubygems/tuf/root.rb
+++ lib/rubygems/tuf/root.rb
@@ -1,8 +1,18 @@
+# TODO: get rid of this somehow
+require 'json'
+
 ##
 # Determines if TUF targets are authentic
 
 class Gem::TUF::Root
-  def initialize root_txt, root_keys
+  def initialize root_txt, root_keys = nil
+    root_txt = JSON.parse(root_txt) if root_txt.is_a? String
+
+    # Extract root keys if not otherwise specified
+    root_keys ||= root_txt['signed']['keys'].map do |_, data|
+      OpenSSL::PKey::RSA.new(data['keyval']['public'])
+    end
+
     # TODO: threshholds for root keys
     verifier = Gem::TUF::Verifier.new(root_keys, 1)
     @root = verifier.verify(root_txt)
--- test/rubygems/test_gem_tuf_root.rb
+++ test/rubygems/test_gem_tuf_root.rb
@@ -6,20 +6,11 @@ unless defined?(OpenSSL::SSL) then
 end
 
 class TestGemTUFRoot < Gem::TestCase
-  def setup
-    super
-    @root_keys  = [PRIVATE_KEY.public_key]
-    @threshhold = 1
-
-    signer = Gem::TUF::Signer.new PRIVATE_KEY
-
-    # TODO: lol this is all wrong but it's a start
-    unsigned_root_txt = {"signed"=>{"_type"=>"Root", "expires"=>"2013-11-19T13:58:12-08:00", "keys"=>{"abc123"=>{"keytype"=>"md5lol", "keyval"=>{"private"=>"", "public"=>"asdfasdfsadfsadlkfjsad"}}}, "roles"=>{"release"=>{"keyids"=>["abc123"], "threshold"=>1}, "root"=>{"keyids"=>["abc123"], "threshold"=>1}, "targets"=>{"keyids"=>["abc123"], "threshold"=>1}, "timestamp"=>{"keyids"=>["abc123"], "threshold"=>1}}}}
-    @root_txt = signer.sign(unsigned_root_txt)
-  end
+  ROOT_PUBLIC_KEY = tuf_cert("root-public")
+  ROOT_FILE       = File.read(tuf_file("root.txt"))
 
   def test_initialize
-    root = Gem::TUF::Root.new(@root_txt, @root_keys)
+    root = Gem::TUF::Root.new ROOT_FILE
   end
 end if defined?(OpenSSL::SSL)
 

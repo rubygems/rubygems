bb6a8b3121240823b428d24a94e1d25b9a8f6a0d
Tony Arcieri <tonyarcieri@squareup.com>
2013-11-20 14:11:36 -0800

    Include all role keys in root.txt
    Adds an API to Gem::TUF::Root to obtain them

--- lib/rubygems/tuf/root.rb
+++ lib/rubygems/tuf/root.rb
@@ -5,22 +5,22 @@ require 'json'
 # Determines if TUF targets are authentic
 
 class Gem::TUF::Root
-  attr_reader :keys, :expires
+  attr_reader :keys, :role_keys, :role_thresholds, :expires
 
   def initialize root_txt, root_keys = nil
+    @keys, @role_keys, @role_thresholds = {}, {}, {}
     root_txt = JSON.parse(root_txt) if root_txt.is_a? String
 
-    @keys = {}
-
     root_txt['signed']['keys'].each do |keyid, data|
-      @keys[keyid] = OpenSSL::PKey::RSA.new(data['keyval']['public'])
+      keys[keyid] = OpenSSL::PKey::RSA.new(data['keyval']['public'])
     end
 
-    # TODO: threshholds for root keys
-    root_key_ids = root_txt['signed']['roles']['root']['keyids']
-    root_keys = root_key_ids.map { |keyid| @keys[keyid] }
+    root_txt['signed']['roles'].each do |name, role|
+      role_keys[name.to_sym]        = role['keyids'].map { |keyid| @keys[keyid] }
+      role_thresholds[name.to_sym] = role['threshold']
+    end
 
-    verifier = Gem::TUF::Verifier.new(root_keys, 1)
+    verifier = Gem::TUF::Verifier.new(role_keys[:root], role_thresholds[:root])
     @root = verifier.verify(root_txt)
 
     @expires = Time.parse(@root['expires'])

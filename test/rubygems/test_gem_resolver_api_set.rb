d55a5a64a5cd0de5d5e89d21f4ee8d3dd8cce9b0
Eric Hodel <drbrain@segment7.net>
2013-11-21 13:31:59 -0800

    Test Resolver::APISet#prefetch basics

--- test/rubygems/test_gem_resolver_api_set.rb
+++ test/rubygems/test_gem_resolver_api_set.rb
@@ -6,6 +6,7 @@ class TestGemResolverAPISet < Gem::TestCase
     super
 
     @DR = Gem::Resolver
+    @dep_uri = URI "#{@gem_repo}api/v1/dependencies"
   end
 
   def test_initialize
@@ -17,10 +18,36 @@ class TestGemResolverAPISet < Gem::TestCase
   end
 
   def test_initialize_uri
-    set = @DR::APISet.new URI "#{@gem_repo}/api/v1/dependencies"
+    set = @DR::APISet.new @dep_uri
 
-    assert_equal URI("#{@gem_repo}/api/v1/dependencies"), set.dep_uri
+    assert_equal URI("#{@gem_repo}api/v1/dependencies"), set.dep_uri
     assert_equal URI("#{@gem_repo}"),                     set.uri
+  end
+
+  def test_prefetch
+    spec_fetcher do |fetcher|
+      fetcher.spec 'a', 1
+    end
+
+    data = [
+      { :name         => 'a',
+        :number       => '1',
+        :platform     => 'ruby',
+        :dependencies => [], },
+    ]
+
+    @fetcher.data["#{@dep_uri}?gems=a,b"] = Marshal.dump data
+    @fetcher.data["#{@dep_uri}?gems=b"]   = Marshal.dump []
+
+    set = @DR::APISet.new @dep_uri
+
+    a_dep = @DR::DependencyRequest.new dep('a'), nil
+    b_dep = @DR::DependencyRequest.new dep('b'), nil
+
+    set.prefetch [a_dep, b_dep]
+
+    assert_equal %w[a-1], set.find_all(a_dep).map { |s| s.full_name }
+    assert_empty          set.find_all(b_dep)
   end
 
 end

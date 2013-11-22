7a6fedc60089b4548338e598931f88c9ee14fbde
Eric Hodel <drbrain@segment7.net>
2013-11-22 15:22:38 -0800

    Refactor creation of the set to setup

--- test/rubygems/test_gem_resolver_installed_specification.rb
+++ test/rubygems/test_gem_resolver_installed_specification.rb
@@ -2,12 +2,16 @@ require 'rubygems/test_case'
 
 class TestGemResolverInstalledSpecification < Gem::TestCase
 
-  def test_initialize
-    set     = Gem::Resolver::CurrentSet.new
+  def setup
+    super
 
+    @set = Gem::Resolver::CurrentSet.new
+  end
+
+  def test_initialize
     source_spec = util_spec 'a'
 
-    spec = Gem::Resolver::InstalledSpecification.new set, source_spec
+    spec = Gem::Resolver::InstalledSpecification.new @set, source_spec
 
     assert_equal 'a',                 spec.name
     assert_equal Gem::Version.new(2), spec.version
@@ -15,15 +19,13 @@ class TestGemResolverInstalledSpecification < Gem::TestCase
   end
 
   def test_installable_platform_eh
-    set     = Gem::Resolver::CurrentSet.new
-
     b, b_gem = util_gem 'a', 1 do |s|
       s.platform = Gem::Platform.new %w[cpu other_platform 1]
     end
 
     source = Gem::Source::SpecificFile.new b_gem
 
-    b_spec = Gem::Resolver::InstalledSpecification.new set, b, source
+    b_spec = Gem::Resolver::InstalledSpecification.new @set, b, source
 
     assert b_spec.installable_platform?
   end

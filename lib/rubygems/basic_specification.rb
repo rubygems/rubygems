4dd930f90679f96890352a532c604661271abf5a
Vít Ondruch <vondruch@redhat.com>
2013-12-10 13:32:03 +0100

    Fix the #extension_install_dir comment to reflect reality.

--- lib/rubygems/basic_specification.rb
+++ lib/rubygems/basic_specification.rb
@@ -69,13 +69,7 @@ class Gem::BasicSpecification
   end
 
   ##
-  # The directory the named +extension+ was installed into after being built.
-  #
-  # Usage:
-  #
-  #   spec.extensions.each do |ext|
-  #     puts spec.extension_dir ext
-  #   end
+  # Returns full path to the directory where gem's extensions are installed.
 
   def extension_dir
     @extension_dir ||=

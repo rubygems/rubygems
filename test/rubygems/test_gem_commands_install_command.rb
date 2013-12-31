rubygems/rubygems

commit d97fba14af575e388ebc325c7e5c53a30af69b7e

use Gem::Installer to skip resolver when ignoring dependencies

lib/rubygems/commands/install_command.rb
@@ -200,25 +200,31 @@ to write the specification by hand.  For example:
 
     req = Gem::Requirement.create(version)
 
-    inst = Gem::DependencyInstaller.new options
+    if options[:ignore_dependencies]
+      inst = Gem::Installer.new name, options
+      inst.install
+      @installed_specs.push(inst.spec)
+    else
+      inst = Gem::DependencyInstaller.new options
 
-    if options[:explain]
-      request_set = inst.resolve_dependencies name, req
+      if options[:explain]
+        request_set = inst.resolve_dependencies name, req
 
-      puts "Gems to install:"
+        puts "Gems to install:"
 
-      request_set.specs.map { |s| s.full_name }.sort.each do |s|
-        puts "  #{s}"
+        request_set.specs.map { |s| s.full_name }.sort.each do |s|
+          puts "  #{s}"
+        end
+
+        return
+      else
+        inst.install name, req
       end
 
-      return
-    else
-      inst.install name, req
+      @installed_specs.push(*inst.installed_gems)
+
+      show_install_errors inst.errors
     end
-
-    @installed_specs.push(*inst.installed_gems)
-
-    show_install_errors inst.errors
   end
 
   def install_gems # :nodoc:

commit f5bbf838c8b13369a61c6756355305388df5824f

Fix installing gem from file without dependencies.
Commit 9437ccc fixed the ability to install remote gems that was accidentally
broken by d97fba1, but in the process accidentally broke installing from local
files.

This also changes the order to check for local first, to avoid unnecessary
network requests in the case where the gem is local.

Closes #760.

lib/rubygems/commands/install_command.rb
@@ -228,19 +228,23 @@ to write the specification by hand.  For example:
   def install_gem_without_dependencies name, req # :nodoc:
     gem = nil
 
-    if remote? then
+    if local? then
+      if name =~ /\.gem$/ and File.file? name then
+        source = Gem::Source::SpecificFile.new name
+        spec = source.spec
+      else
+        source = Gem::Source::Local.new
+        spec = source.find_gem name, req
+      end
+      gem = source.download spec if spec
+    end
+
+    if remote? and not gem then
       dependency = Gem::Dependency.new name, req
       dependency.prerelease = options[:prerelease]
 
       fetcher = Gem::RemoteFetcher.fetcher
       gem = fetcher.download_to_cache dependency
-    end
-
-    if local? and not gem then
-      source = Gem::Source::Local.new
-      spec = source.find_gem name, req
-
-      gem = source.download spec
     end
 
     inst = Gem::Installer.new gem, options

test/rubygems/test_gem_commands_install_command.rb
@@ -560,6 +560,20 @@ ERROR:  Possible alternatives: non_existent_with_hint
     assert_equal %w[a-2], @cmd.installed_specs.map { |spec| spec.full_name }
   end
 
+  def test_install_gem_ignore_dependencies_specific_file
+    spec = quick_spec 'a', 2
+
+    util_build_gem spec
+
+    FileUtils.mv spec.cache_file, @tempdir
+
+    @cmd.options[:ignore_dependencies] = true
+
+    @cmd.install_gem File.join(@tempdir, spec.file_name), nil
+
+    assert_equal %w[a-2], @cmd.installed_specs.map { |spec| spec.full_name }
+  end
+
   def test_parses_requirement_from_gemname
     spec_fetcher do |fetcher|
       fetcher.gem 'a', 2
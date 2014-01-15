c016dc4b8c9d63ac4a2316b551c955e6c3edb8a6
Eric Hodel <drbrain@segment7.net>
2014-01-14 17:42:21 -0800

    Delay BestSet resolution until needed
    Now `gem install --local` does not make network requests.
    
    Fixes #776

--- test/rubygems/test_gem_resolver_best_set.rb
+++ test/rubygems/test_gem_resolver_best_set.rb
@@ -8,6 +8,12 @@ class TestGemResolverBestSet < Gem::TestCase
     @DR = Gem::Resolver
   end
 
+  def test_initialize
+    set = @DR::BestSet.new
+
+    assert_empty set.sets
+  end
+
   def test_find_all_index
     spec_fetcher do |fetcher|
       fetcher.spec 'a', 1
@@ -43,6 +49,31 @@ class TestGemResolverBestSet < Gem::TestCase
     found = set.find_all req
 
     assert_empty found
+  end
+
+  def test_prefetch
+    spec_fetcher do |fetcher|
+      fetcher.spec 'a', 1
+    end
+
+    set = @DR::BestSet.new
+
+    set.prefetch []
+
+    refute_empty set.sets
+  end
+
+  def test_prefetch_local
+    spec_fetcher do |fetcher|
+      fetcher.spec 'a', 1
+    end
+
+    set = @DR::BestSet.new
+    set.remote = false
+
+    set.prefetch []
+
+    assert_empty set.sets
   end
 
 end

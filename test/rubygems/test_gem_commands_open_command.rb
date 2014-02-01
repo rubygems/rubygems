0c9f9722340135c582ff87df836a067b4723d0b9
Vitali Fokin <vitali.fokin@gmail.com>
2014-02-01 21:59:20 +0300

    fix indentation

--- test/rubygems/test_gem_commands_open_command.rb
+++ test/rubygems/test_gem_commands_open_command.rb
@@ -29,17 +29,17 @@ class TestGemCommandsOpenCommand < Gem::TestCase
         assert_equal "", @ui.error
     end
 
- def test_execute_bad_gem
-    @cmd.options[:args] = %w[foo]
+    def test_execute_bad_gem
+        @cmd.options[:args] = %w[foo]
 
-    assert_raises Gem::MockGemUi::TermError do
-      use_ui @ui do
-        @cmd.execute
-      end
+        assert_raises Gem::MockGemUi::TermError do
+            use_ui @ui do
+                @cmd.execute
+            end
+        end
+
+        assert_match %r|Unable to find gem 'foo'|, @ui.output
+        assert_equal "", @ui.error
     end
-
-    assert_match %r|Unable to find gem 'foo'|, @ui.output
-    assert_equal "", @ui.error
-  end
 
 end

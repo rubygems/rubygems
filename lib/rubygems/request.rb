e52ec2d93711d6fbe8936b82bf3dbaf06292b4d4
HINOHARA Hiroshi <stl@nts.ricoh.co.jp>
2014-02-13 07:46:28 +0900

    Pass nil to net_http as proxy parameter not to use environment variables

--- lib/rubygems/request.rb
+++ lib/rubygems/request.rb
@@ -80,6 +80,8 @@ class Gem::Request
         Gem::UriFormatter.new(@proxy_uri.user).unescape,
         Gem::UriFormatter.new(@proxy_uri.password).unescape,
       ]
+    else
+      net_http_args += [nil, nil]
     end
 
     connection_id = [Thread.current.object_id, *net_http_args].join ':'

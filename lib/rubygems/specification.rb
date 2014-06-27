17eabc3747a35936cff5e8631b2f72514b02ed56
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-06-27 11:44:05 +0800

    change condition of YAML engine for Psych

--- lib/rubygems/specification.rb
+++ lib/rubygems/specification.rb
@@ -2386,7 +2386,7 @@ class Gem::Specification < Gem::BasicSpecification
 
   def to_yaml(opts = {}) # :nodoc:
     if (YAML.const_defined?(:ENGINE) && !YAML::ENGINE.syck?) ||
-        !defined?(Syck) && defined?(Psych) then
+        (defined?(Psych) && YAML == Psych) then
       # Because the user can switch the YAML engine behind our
       # back, we have to check again here to make sure that our
       # psych code was properly loaded, and load it if not.

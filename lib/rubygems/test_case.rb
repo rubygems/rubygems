6669d10da1c96e5ecad5e7b1ae5b5c7f62e98752
Aaron Patterson <aaron.patterson@gmail.com>
2015-01-24 16:22:42 -0800

    isloate rubygems after it's loaded test dependencies
    Fixes #914.  Let the test case file load test dependencies (like
    minitest), then switch GEM_HOME and GEM_PATH to a temporary directory.
    This ensures that things that are automatically loaded by RubyGems (like
    plugins) will not interfere with RubyGems' tests.

--- lib/rubygems/test_case.rb
+++ lib/rubygems/test_case.rb
@@ -1427,4 +1427,7 @@ Also, a list:
 end
 
 require 'rubygems/test_utilities'
-
+ENV['GEM_HOME'] = Dir.mktmpdir "home"
+ENV['GEM_PATH'] = Dir.mktmpdir "path"
+Gem::Specification.reset
+Gem.clear_paths

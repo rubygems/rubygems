rubygems/rubygems

commit e2935c85fc62594ccd567c8facd46ddbd01ad100

Pass the full path when calling the original Kernel.require if available.
calling the original Kernel.require with a full path is about 25% faster than with path-less paramter.
this reduces about 5% total time of rubygem's Kernel.require on my test environment.

lib/rubygems/core_ext/kernel_require.rb
@@ -66,7 +66,7 @@ module Kernel
 
     begin
       RUBYGEMS_ACTIVATION_MONITOR.exit
-      return gem_original_require(path)
+      return gem_original_require(spec.to_fullpath(path) || path)
     end if spec
 
     # Attempt to find +path+ in any unresolved gems...

lib/rubygems/basic_specification.rb
@@ -139,13 +139,34 @@ class Gem::BasicSpecification
   # activated.
 
   def full_require_paths
-    full_paths = raw_require_paths.map do |path|
-      File.join full_gem_path, path
+    @full_require_paths ||=
+    begin
+      full_paths = raw_require_paths.map do |path|
+        File.join full_gem_path, path
+      end
+
+      full_paths.unshift extension_dir unless @extensions.nil? || @extensions.empty?
+
+      full_paths
     end
+  end
 
-    full_paths.unshift extension_dir unless @extensions.nil? || @extensions.empty?
+  ##
+  # Full path of the target library file.
+  # If the file is not in this gem, return nil.
 
-    full_paths
+  def to_fullpath path
+    @paths_map ||= {}
+    @paths_map[path] ||=
+    begin
+      fullpath = nil
+      suffixes = Gem.suffixes
+      full_require_paths.find do |dir|
+        suffixes.find do |suf|
+          File.file? (fullpath = "#{dir}/#{path}#{suf}")
+        end
+      end ? fullpath : nil
+    end
   end
 
   ##

test/rubygems/test_gem_specification.rb
@@ -1863,6 +1863,30 @@ dependencies: []
     assert_equal expected, @ext.full_require_paths
   end
 
+  def test_to_fullpath
+    ext_spec
+
+    @ext.require_paths = 'lib'
+
+    dir = File.join(@gemhome, 'gems', @ext.original_name, 'lib')
+    expected = File.join(dir, 'code.rb')
+    FileUtils.mkdir_p dir
+    FileUtils.touch expected
+    assert_equal expected, @ext.to_fullpath('code')
+    assert_equal expected, @ext.to_fullpath('code.rb')
+    assert_nil @ext.to_fullpath('code.so')
+
+    dir = @ext.extension_dir
+    FileUtils.mkdir_p dir
+    expected = File.join(dir, 'ext-1.so')
+    FileUtils.touch expected
+    assert_equal expected, @ext.to_fullpath('ext-1')
+    assert_equal expected, @ext.to_fullpath('ext-1.so')
+    assert_nil @ext.to_fullpath('ext-1.rb')
+
+    assert_nil @ext.to_fullpath('notexist')
+  end
+
   def test_require_already_activated
     save_loaded_features do
       a1 = new_spec "a", "1", nil, "lib/d.rb"

commit 65ab980dbc13d1b8e95b7d1f7266406720718c24

-I should always win over gems
If there were gems in the "unresolved" list, then the files in that gem
would be preferred over files that included in -I.  For example, if you
had a depdedncy tree that looks like this:

```dot
digraph g {
  a -> b [label="version: = 1"];
  b -> c [label="version: > 0"];
}
```

and the installed gems were like this:

a = 1,
b = 1,
c = 1,
c = 2,

When the user requires 'a', then 'b' will be immediately activated
because there is only one decision to make.  However, 'c' will be added
to the `unresloved_deps` list.  Having anything in that list will cause
Rubygems [to skip checking if Ruby's `require` can handle the file](https://github.com/rubygems/rubygems/blob/07f1a83fe095e2b9b09e2f6b9661aed326b2de6a/lib/rubygems/core_ext/kernel_require.rb#L52-55)
If the user provides any paths to `-I`
that contain files that are also in an activated gem (like `b` in this
case), then Rubygems will prefer the file from the gem rather than the
file provided by -I.

This commit fixes it so that -I will win over gems.  e2935c85fc62 introduced
the incorrect behavior.

lib/rubygems/core_ext/kernel_require.rb
@@ -66,7 +66,7 @@ module Kernel
 
     begin
       RUBYGEMS_ACTIVATION_MONITOR.exit
-      return gem_original_require(spec.to_fullpath(path) || path)
+      return gem_original_require(path)
     end if spec
 
     # Attempt to find +path+ in any unresolved gems...

test/rubygems/test_require.rb
@@ -49,6 +49,36 @@ class TestGemRequire < Gem::TestCase
     end
   end
 
+  # Providing -I on the commandline should always beat gems
+  def test_dash_i_beats_gems
+    a1 = new_spec "a", "1", {"b" => "= 1"}, "lib/test_gem_require_a.rb"
+    b1 = new_spec "b", "1", {"c" => "> 0"}, "lib/b/c.rb"
+    c1 = new_spec "c", "1", nil, "lib/c/c.rb"
+    c2 = new_spec "c", "2", nil, "lib/c/c.rb"
+
+    install_specs c1, c2, b1, a1
+
+    dir = Dir.mktmpdir
+    dash_i_arg = File.join Dir.mktmpdir, 'lib'
+
+    c_rb = File.join dash_i_arg, 'b', 'c.rb'
+
+    FileUtils.mkdir_p File.dirname c_rb
+    File.open(c_rb, 'w') { |f| f.write "class Object; HELLO = 'world' end" }
+
+    lp = $LOAD_PATH.dup
+
+    # Pretend to provide a commandline argument that overrides a file in gem b
+    $LOAD_PATH.unshift dash_i_arg
+
+    assert_require 'test_gem_require_a'
+    assert_require 'b/c' # this should be required from -I
+    assert_equal "world", ::Object::HELLO
+  ensure
+    $LOAD_PATH.replace lp
+    Object.send :remove_const, :HELLO if Object.const_defined? :HELLO
+  end
+
   def test_concurrent_require
     Object.const_set :FILE_ENTERED_LATCH, Latch.new(2)
     Object.const_set :FILE_EXIT_LATCH, Latch.new(1)
5ee6a59784b1736553e16fda374c18491bb66abe
David Radcliffe <radcliffe.david@gmail.com>
2016-03-13 17:10:59 -0400

    update certs for RubyGems.org

--- util/update_bundled_ca_certificates.rb
+++ util/update_bundled_ca_certificates.rb
@@ -6,6 +6,7 @@ URIS = [
   URI('https://rubygems.org'),
   URI('https://staging.rubygems.org'),
   URI('https://s3.amazonaws.com'),
+  URI('https://fastly.rubygems.org'),
   URI('https://rubygems.global.ssl.fastly.net'),
 ]
 

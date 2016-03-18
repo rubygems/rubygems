rubygems/rubygems

commit a9c1aaffec0095081fd405ce78cfd689e937c1d7

consolidate `contains_requirable_file?` cache
This commit consolidates the `contains_requirable_file?` cache in to one
cache, which should speed up require failures and decrease memory usage.
This commit moves the `contains_requirable_file?` off  the spec and on
to the `Gem::Specification` class.  That way data about which spec has
which file can be centrally located, and we don't have to repeat data
through each cache.

Memory Usage Improvements
=========================

Here is what the memory usage looked like before this change:

```
[aaron@TC rubygems (master)]$ ruby --disable-gems -robjspace -Ilib -e"require 'rubygems';(ENV['N']||10).to_i.times {|x| begin;require(x.to_s);rescue LoadError;end}; GC.start; GC.start; p ObjectSpace.memsize_of_all"
2965374
[aaron@TC rubygems (master)]$ N=100 ruby --disable-gems -robjspace -Ilib -e"require 'rubygems';(ENV['N']||10).to_i.times {|x| begin;require(x.to_s);rescue LoadError;end}; GC.start; GC.start; p ObjectSpace.memsize_of_all"
6273878
[aaron@TC rubygems (master)]$ N=1000 ruby --disable-gems -robjspace -Ilib -e"require 'rubygems';(ENV['N']||10).to_i.times {|x| begin;require(x.to_s);rescue LoadError;end}; GC.start; GC.start; p ObjectSpace.memsize_of_all"
39738934
```

and here it is after this change:

```
[aaron@TC rubygems (file_req_cache)]$ ruby --disable-gems -robjspace -Ilib -e"require 'rubygems';(ENV['N']||10).to_i.times {|x| begin;require(x.to_s);rescue LoadError;end}; GC.start; GC.start; p ObjectSpace.memsize_of_all"
2444798
[aaron@TC rubygems (file_req_cache)]$ N=100 ruby --disable-gems -robjspace -Ilib -e"require 'rubygems';(ENV['N']||10).to_i.times {|x| begin;require(x.to_s);rescue LoadError;end}; GC.start; GC.start; p ObjectSpace.memsize_of_all"
2452886
[aaron@TC rubygems (file_req_cache)]$ N=1000 ruby --disable-gems -robjspace -Ilib -e"require 'rubygems';(ENV['N']||10).to_i.times {|x| begin;require(x.to_s);rescue LoadError;end}; GC.start; GC.start; p ObjectSpace.memsize_of_all"
2533878
```

lib/rubygems/basic_specification.rb
@@ -65,22 +65,17 @@ class Gem::BasicSpecification
   # Return true if this spec can require +file+.
 
   def contains_requirable_file? file
-    @contains_requirable_file ||= {}
-    @contains_requirable_file[file] ||=
-    begin
-      if @ignored then
-        return false
-      elsif missing_extensions? then
-        @ignored = true
+    if @ignored then
+      return false
+    elsif missing_extensions? then
+      @ignored = true
 
-        warn "Ignoring #{full_name} because its extensions are not built.  " +
-             "Try: gem pristine #{name} --version #{version}"
-        return false
-      end
+      warn "Ignoring #{full_name} because its extensions are not built.  " +
+        "Try: gem pristine #{name} --version #{version}"
+      return false
+    end
 
-      have_file? file, Gem.suffixes
-    end ? :yes : :no
-    @contains_requirable_file[file] == :yes
+    have_file? file, Gem.suffixes
   end
 
   def default_gem?

lib/rubygems/stub_specification.rb
@@ -88,6 +88,8 @@ class Gem::StubSpecification < Gem::BasicSpecification
     end
   end
 
+  def this; self; end
+
   def default_gem?
     @default_gem
   end

lib/rubygems/core_ext/kernel_require.rb
@@ -60,9 +60,7 @@ module Kernel
     #--
     # TODO request access to the C implementation of this to speed up RubyGems
 
-    spec = Gem::Specification.stubs.find { |s|
-      s.activated? and s.contains_requirable_file? path
-    }
+    spec = Gem::Specification.find_active_stub_by_path path
 
     begin
       RUBYGEMS_ACTIVATION_MONITOR.exit

lib/rubygems/specification.rb
@@ -175,6 +175,11 @@ class Gem::Specification < Gem::BasicSpecification
 
   @@stubs_by_name = {}
 
+  # Sentinel object to represent "not found" stubs
+  NOT_FOUND = Struct.new(:to_spec, :this).new # :nodoc:
+  @@spec_with_requirable_file          = {}
+  @@active_stub_with_requirable_file   = {}
+
   ######################################################################
   # :section: Required gemspec attributes
 
@@ -1027,10 +1032,10 @@ class Gem::Specification < Gem::BasicSpecification
 
   def self.find_by_path path
     path = path.dup.freeze
-    stub = stubs.find { |spec|
-      spec.contains_requirable_file? path
-    }
-    stub && stub.to_spec
+    spec = @@spec_with_requirable_file[path] ||= (stubs.find { |s|
+      s.contains_requirable_file? path
+    } || NOT_FOUND)
+    spec.to_spec
   end
 
   ##
@@ -1042,6 +1047,13 @@ class Gem::Specification < Gem::BasicSpecification
       s.contains_requirable_file? path unless s.activated?
     }
     stub && stub.to_spec
+  end
+
+  def self.find_active_stub_by_path path
+    stub = @@active_stub_with_requirable_file[path] ||= (stubs.find { |s|
+      s.activated? and s.contains_requirable_file? path
+    } || NOT_FOUND)
+    stub.this
   end
 
   ##
@@ -1261,6 +1273,8 @@ class Gem::Specification < Gem::BasicSpecification
     @@all = nil
     @@stubs = nil
     @@stubs_by_name = {}
+    @@spec_with_requirable_file          = {}
+    @@active_stub_with_requirable_file   = {}
     _clear_load_cache
     unresolved = unresolved_deps
     unless unresolved.empty? then

commit ff678ca1ef4cbab3a681e0c342b7a7d94a34318b

Handle cases when the @@stubs variable contains non-stubs.
This fixes the regression introduced in a9c1aaffec0095081fd405ce78cfd689e937c1d7 related to non-stubs being present in the list of stubs. This can happen e.g. when the `all` method is being used. We discovered this while using the `github_changelog_generator` in projects using Bundler (because rvm does some special tricks to "unload" bundler files when ruby gets run from a Bundler project without bundler... https://github.com/rvm/bundler-unload/blob/master/lib/bundler-unload.rb#L28)

Credits to @orenf for fixing this in https://github.com/orenf/rubygems/commit/4a4b4758bd454b4f7525635aca5e8a00e47cab42. This commit builds on top of that, based on discussions in https://github.com/rubygems/rubygems/pull/1556.

Since the `this` method is needed from different kind of `Specification` objects, moving it up one step in the class hierarchy (i.e. to `BasicSpecification`) seemed like the reasonable thing to do. An accompanying test has also been added, to ensure that we have properly fixed the issue.

lib/rubygems/basic_specification.rb
@@ -306,6 +306,8 @@ class Gem::BasicSpecification
     raise NotImplementedError
   end
 
+  def this; self; end
+
   private
 
   def have_extensions?; !extensions.empty?; end
@@ -323,5 +325,4 @@ class Gem::BasicSpecification
       false
     end
   end
-
 end

lib/rubygems/stub_specification.rb
@@ -89,8 +89,6 @@ class Gem::StubSpecification < Gem::BasicSpecification
     end
   end
 
-  def this; self; end
-
   def default_gem?
     @default_gem
   end
@@ -212,4 +210,3 @@ class Gem::StubSpecification < Gem::BasicSpecification
   end
 
 end
-

test/rubygems/test_gem_specification.rb
@@ -100,6 +100,17 @@ end
     load 'rubygems/syck_hack.rb'
   end
 
+  def test_self_find_active_stub_by_path
+    spec = new_spec('a', '1', nil, 'lib/foo.rb')
+    spec.activated = true
+
+    # There used to be a bug (introduced in a9c1aaf) when Gem::Specification
+    # objects are present in the @stubs collection. This test verifies that
+    # this scenario works correctly.
+    Gem::Specification.all = [spec]
+    Gem::Specification.find_active_stub_by_path('foo')
+  end
+
   def test_self_activate
     foo = util_spec 'foo', '1'
 
c74b001870d3f91365afafcc41f0b8e5a433ed32
Sam Jones <sam@samljones.com>
2016-06-08 18:08:49 -0400

    Creating Sign In Command
    Closes rubygems/rubygems#1634
    
    Sign in command signs the user into a gem's host prior to pushing a gem.
    Uses the same sign in mechanism as `PushCommand`.
    Can be used to sign into a gem's host prior to running automated tooling
    to push gem.

---
+++ lib/rubygems/commands/signin_command.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+require 'rubygems/command'
+require 'rubygems/gemcutter_utilities'
+require 'rubygems/package'
+
+class Gem::Commands::SigninCommand < Gem::Command
+  include Gem::GemcutterUtilities
+
+  def description # :nodoc:
+    <<-EOF
+        The signin command executes host sign in.
+    EOF
+  end
+
+  def arguments # :nodoc:
+    "GEM        gem that you are logging in to push"
+  end
+
+  def usage # :nodoc:
+    "#{program_name} GEM"
+  end
+
+  def initialize
+    super 'signin', 'Signin to a gem server'
+
+    add_option('--host HOST',
+               'Push to another gemcutter-compatible host') do |value, options|
+      options[:host] = value
+    end
+  end
+
+  def execute
+    sign_in sign_in_host
+  end
+
+  def sign_in_host
+    options[:host] || default_host
+  end
+
+  def default_host
+    gem_data = Gem::Package.new(get_one_gem_name)
+    gem_data.spec.metadata['default_gem_server']
+  end
+
+end

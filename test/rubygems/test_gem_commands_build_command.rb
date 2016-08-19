8011fd8f9a821417c2245a12f9c246623a2ebbf9
Sylvain Daubert <sylvain.daubert@laposte.net>
2016-08-19 18:22:55 +0200

    test_build_signed_gem: rename test_dir to my_dir.

--- test/rubygems/test_gem_commands_build_command.rb
+++ test/rubygems/test_gem_commands_build_command.rb
@@ -118,12 +118,12 @@ class TestGemCommandsBuildCommand < Gem::TestCase
   end
 
   def test_build_signed_gem
-    test_dir = File.expand_path(File.dirname(__FILE__))
-    cert_file = File.join(test_dir, 'public_cert3072.pem')
+    my_dir = File.expand_path(File.dirname(__FILE__))
+    cert_file = File.join(my_dir, 'public_cert3072.pem')
     trust_dir = Gem::Security.trust_dir
 
     spec = util_spec 'some_gem' do |s|
-      s.signing_key = File.join(test_dir, 'private_key3072.pem')
+      s.signing_key = File.join(my_dir, 'private_key3072.pem')
       s.cert_chain = [cert_file]
     end
 

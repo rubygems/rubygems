cb7f29d003f44440dd66878eb15161bd00fd3dfa
Samuel Giddins <segiddins@segiddins.me>
2016-08-25 12:46:28 -0500

    Add a helpful suggestion when `gem install` fails due to required_ruby{gems,}_version

--- lib/rubygems/installer.rb
+++ lib/rubygems/installer.rb
@@ -608,7 +608,8 @@ class Gem::Installer
   def ensure_required_ruby_version_met # :nodoc:
     if rrv = spec.required_ruby_version then
       unless rrv.satisfied_by? Gem.ruby_version then
-        raise Gem::InstallError, "#{spec.name} requires Ruby version #{rrv}."
+        raise Gem::RuntimeRequirementNotMetError,
+          "#{spec.name} requires Ruby version #{rrv}."
       end
     end
   end
@@ -616,7 +617,7 @@ class Gem::Installer
   def ensure_required_rubygems_version_met # :nodoc:
     if rrgv = spec.required_rubygems_version then
       unless rrgv.satisfied_by? Gem.rubygems_version then
-        raise Gem::InstallError,
+        raise Gem::RuntimeRequirementNotMetError,
           "#{spec.name} requires RubyGems version #{rrgv}. " +
           "Try 'gem update --system' to update RubyGems itself."
       end

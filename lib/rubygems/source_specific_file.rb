29ddda5d00897f6b9f1106c0ba0daaa8089a02d9
Ellen Marie Dash <me@duckie.co>
2017-06-03 16:46:13 -0400

    [rubygems/source_{local,specific_file}] warn about deprecated files.

--- lib/rubygems/source_specific_file.rb
+++ lib/rubygems/source_specific_file.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'rubygems/source/specific_file'
 
-# TODO warn upon require, this file is deprecated.
-
+unless Gem::Deprecate.skip
+  Kernel.warn "#{Gem.location_of_caller(3).join(':')}: Warning: Requiring rubygems/source_local is deprecated; please use rubygems/source/local instead."
+end

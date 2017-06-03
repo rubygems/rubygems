d1f199be86c1640e20fcf85033a28ee62ed3cec0
Ellen Marie Dash <me@duckie.co>
2017-06-03 17:27:39 -0400

    [rubygems/source_specific_file] correct typo: 'local' != 'specific_file'

--- lib/rubygems/source_specific_file.rb
+++ lib/rubygems/source_specific_file.rb
@@ -2,5 +2,5 @@
 require 'rubygems/source/specific_file'
 
 unless Gem::Deprecate.skip
-  Kernel.warn "#{Gem.location_of_caller(3).join(':')}: Warning: Requiring rubygems/source_local is deprecated; please use rubygems/source/local instead."
+  Kernel.warn "#{Gem.location_of_caller(3).join(':')}: Warning: Requiring rubygems/source_specific_file is deprecated; please use rubygems/source/specific_file instead."
 end

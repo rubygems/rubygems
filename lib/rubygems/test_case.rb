934c9e474069d87714629d0c4464b2c6223d25db
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2017-06-12 11:27:40 +0900

    Backport r58736 from ruby/ruby repository.
      * https://github.com/rubygems/rubygems/issues/1928
      * https://svn.ruby-lang.org/cgi-bin/viewvc.cgi?view=revision&revision=58736

--- lib/rubygems/test_case.rb
+++ lib/rubygems/test_case.rb
@@ -422,6 +422,7 @@ class Gem::TestCase < MiniTest::Unit::TestCase
 
     Gem::Specification._clear_load_cache
     Gem::Specification.unresolved_deps.clear
+    Gem::refresh
   end
 
   def common_installer_setup

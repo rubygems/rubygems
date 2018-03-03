8933115bff09402f6baaffe2f2bf6971f71081fb
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2018-03-03 10:59:04 +0900

    Deprecate ubygems.rb

--- lib/ubygems.rb
+++ lib/ubygems.rb
@@ -7,5 +7,8 @@
 # See LICENSE.txt for permissions.
 #++
 
+# TODO: Remove this file at RubyGems 4.0
+# Based by https://bugs.ruby-lang.org/issues/14322#note-4
+warn "`ubygems.rb' is deprecated, It will be removed on or after 2018-12-01. remove `-rubygems' from your command-line"
 
 require 'rubygems'

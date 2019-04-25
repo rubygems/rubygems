8d9806bdca4915450f344deb705a7bdc1fe7a3ed
David Rodríguez <deivid.rodriguez@riseup.net>
2019-04-25 17:46:08 +0200

    More unneded stuff
    rdoc is a default gem in all supported rubies.

--- lib/rubygems/test_case.rb
+++ lib/rubygems/test_case.rb
@@ -26,13 +26,6 @@ begin
 rescue LoadError
 end
 
-# We have to load these up front because otherwise we'll try to load
-# them while we're testing rubygems, and thus we can't actually load them.
-unless Gem::Dependency.new('rdoc', '>= 3.10').matching_specs.empty?
-  gem 'rdoc'
-  gem 'json'
-end
-
 require 'bundler'
 
 require 'minitest/autorun'

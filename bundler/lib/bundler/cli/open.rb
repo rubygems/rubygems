rubygems/bundler

Fix open default gem error

lib/bundler/cli/open.rb
@@ -13,12 +13,16 @@ module Bundler
     def run
       editor = [ENV["BUNDLER_EDITOR"], ENV["VISUAL"], ENV["EDITOR"]].find {|e| !e.nil? && !e.empty? }
       return Bundler.ui.info("To open a bundled gem, set $EDITOR or $BUNDLER_EDITOR") unless editor
-      path = Bundler::CLI::Common.select_spec(name, :regex_match).full_gem_path
-      Dir.chdir(path) do
-        command = Shellwords.split(editor) + [path]
-        Bundler.with_clean_env do
-          system(*command)
-        end || Bundler.ui.info("Could not run '#{command.join(" ")}'")
+      spec = Bundler::CLI::Common.select_spec(name, :regex_match)
+      begin
+        Dir.chdir(spec.full_gem_path) do |path|
+          command = Shellwords.split(editor) + [path]
+          Bundler.with_clean_env do
+            system(*command)
+          end || Bundler.ui.info("Could not run '#{command.join(" ")}'")
+        end
+      rescue Errno::ENOENT
+        raise InvalidOption, "Unable to open #{spec.to_s} because the directory it would normally be installed to does not exist. This could happen when you try to open a default gem."
       end
     end
   end

rubygems/rubygems

bundler/lib/bundler/cli/open.rb
@@ -15,11 +15,15 @@ module Bundler
       return Bundler.ui.info("To open a bundled gem, set $EDITOR or $BUNDLER_EDITOR") unless editor
       return unless spec = Bundler::CLI::Common.select_spec(name, :regex_match)
       path = spec.full_gem_path
-      Dir.chdir(path) do
-        command = Shellwords.split(editor) + [path]
-        Bundler.with_original_env do
-          system(*command)
-        end || Bundler.ui.info("Could not run '#{command.join(" ")}'")
+      if spec.default_gem?
+        Bundler.ui.info "Unable to open #{name} because it's a default gem, so the directory it would normally be installed to does not exist."
+      else
+        Dir.chdir(path) do
+          command = Shellwords.split(editor) + [path]
+          Bundler.with_original_env do
+            system(*command)
+          end || Bundler.ui.info("Could not run '#{command.join(" ")}'")
+        end
       end
     end
   end
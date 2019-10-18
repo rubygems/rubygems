cee56a0f2c9b6aa8252153b63513ad910526f6ea
Jeremy Evans <code@jeremyevans.net>
2019-10-18 13:08:18 -0700

    Remove taint usage on Ruby 2.7+
    Ruby 2.7 deprecates taint and it no longer has an effect.
    This attempts to leave the behavior the same on older
    Ruby versions, but avoid the use of deprecated methods
    related to taint on Ruby 2.7+.
    
    See https://bugs.ruby-lang.org/issues/16131 for details.

--- lib/rubygems/installer.rb
+++ lib/rubygems/installer.rb
@@ -392,7 +392,7 @@ class Gem::Installer
       specs = []
 
       Gem::Util.glob_files_in_dir("*.gemspec", File.join(gem_home, "specifications")).each do |path|
-        spec = Gem::Specification.load path.untaint
+        spec = Gem::Specification.load path.tap(&Gem::UNTAINT)
         specs << spec if spec
       end
 
@@ -500,7 +500,7 @@ class Gem::Installer
     raise Gem::FilePermissionError.new(@bin_dir) unless File.writable? @bin_dir
 
     spec.executables.each do |filename|
-      filename.untaint
+      filename.tap(&Gem::UNTAINT)
       bin_path = File.join gem_dir, spec.bindir, filename
 
       unless File.exist? bin_path
@@ -631,7 +631,7 @@ class Gem::Installer
 
   def ensure_loadable_spec
     ruby = spec.to_ruby_for_cache
-    ruby.untaint
+    ruby.tap(&Gem::UNTAINT)
 
     begin
       eval ruby

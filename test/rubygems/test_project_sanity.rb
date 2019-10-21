fb73d6da8bb6d77be33f707320356677f339ef6b
David Rodríguez <deivid.rodriguez@riseup.net>
2019-10-21 13:30:20 +0200

    Add a check to keep Manifest.txt in sync
    And sync it.

--- test/rubygems/test_project_sanity.rb
+++ test/rubygems/test_project_sanity.rb
@@ -23,6 +23,14 @@ class TestProjectSanity < Minitest::Test
     end
   end
 
+  def test_manifest_is_up_to_date
+    skip unless File.exist?(File.expand_path("../../../Rakefile", __FILE__))
+
+    _, status = Open3.capture2e("rake check_manifest")
+
+    assert status.success?, "Expected Manifest.txt to be up to date, but it's not. Run `rake update_manifest` to sync it."
+  end
+
   private
 
   def with_empty_pkg_folder

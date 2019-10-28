44f0db9857c9a5cd7b94b6e1811b25115fef152e
David Rodríguez <deivid.rodriguez@riseup.net>
2019-10-28 18:52:23 +0100

    Remove misleading comment
    When I read, I thought the assertion was incorrect. It's doing the right
    thing, though.

--- test/rubygems/test_gem.rb
+++ test/rubygems/test_gem.rb
@@ -455,7 +455,6 @@ class TestGem < Gem::TestCase
       s.executables = []
     end
     install_specs spec
-    # Should not find a-10's non-abin (bug)
     assert_equal @abin_path, Gem.bin_path('a', 'abin')
   end
 

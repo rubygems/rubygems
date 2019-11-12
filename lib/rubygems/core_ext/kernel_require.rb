5d50feb31bafb82571cbd08c826bf76ff72bd0fb
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-11-12 15:44:19 +0900

    Only enabled mon_owned condition with Ruby 2.5+

--- lib/rubygems/core_ext/kernel_require.rb
+++ lib/rubygems/core_ext/kernel_require.rb
@@ -32,7 +32,9 @@ module Kernel
   # that file has already been loaded is preserved.
 
   def require(path)
-    monitor_owned = RUBYGEMS_ACTIVATION_MONITOR.mon_owned?
+    if RUBY_VERSION >= "2.5"
+      monitor_owned = RUBYGEMS_ACTIVATION_MONITOR.mon_owned?
+    end
     RUBYGEMS_ACTIVATION_MONITOR.enter
 
     path = path.to_path if path.respond_to? :to_path
@@ -167,9 +169,12 @@ module Kernel
 
     raise load_error
   ensure
-    if monitor_owned != (ow = RUBYGEMS_ACTIVATION_MONITOR.mon_owned?)
-      STDERR.puts [$$, Thread.current, $!, $!.backtrace].inspect if $!
-      raise "CRITICAL: RUBYGEMS_ACTIVATION_MONITOR.owned?: before #{monitor_owned} -> after #{ow}"
+    if RUBY_VERSION >= "2.5"
+      ow = RUBYGEMS_ACTIVATION_MONITOR.mon_owned?
+      if monitor_owned != ow
+        STDERR.puts [$$, Thread.current, $!, $!.backtrace].inspect if $!
+        raise "CRITICAL: RUBYGEMS_ACTIVATION_MONITOR.owned?: before #{monitor_owned} -> after #{ow}"
+      end
     end
   end
 

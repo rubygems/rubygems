94819a79ede34491e29d1f1fefd84a8bf8e90a31
Zach Rabinovich <zach.rabinovich@sarekon.com>
2020-03-23 06:53:17 -0700

    Initial stab at adding semver operator

--- lib/rubygems/requirement.rb
+++ lib/rubygems/requirement.rb
@@ -3,20 +3,21 @@ require "rubygems/deprecate"
 
 ##
 # A Requirement is a set of one or more version restrictions. It supports a
-# few (<tt>=, !=, >, <, >=, <=, ~></tt>) different restriction operators.
+# few (<tt>=, !=, >, <, >=, <=, ~>, ^</tt>) different restriction operators.
 #
 # See Gem::Version for a description on how versions and requirements work
 # together in RubyGems.
 
 class Gem::Requirement
   OPS = { #:nodoc:
-    "="  =>  lambda {|v, r| v == r },
-    "!=" =>  lambda {|v, r| v != r },
-    ">"  =>  lambda {|v, r| v >  r },
-    "<"  =>  lambda {|v, r| v <  r },
-    ">=" =>  lambda {|v, r| v >= r },
-    "<=" =>  lambda {|v, r| v <= r },
-    "~>" =>  lambda {|v, r| v >= r && v.release < r.bump },
+    "="  =>  lambda { |v, r| v == r },
+    "!=" =>  lambda { |v, r| v != r },
+    ">"  =>  lambda { |v, r| v >  r },
+    "<"  =>  lambda { |v, r| v <  r },
+    ">=" =>  lambda { |v, r| v >= r },
+    "<=" =>  lambda { |v, r| v <= r },
+    "~>" =>  lambda { |v, r| v >= r && v.release < r.bump },
+    "^" =>  lambda { |v, r| v >= r && v.release < r.major_bump }
   }.freeze
 
   SOURCE_SET_REQUIREMENT = Struct.new(:for_lockfile).new "!" # :nodoc:

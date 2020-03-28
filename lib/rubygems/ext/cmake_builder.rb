ce55903df3b2ba0b5414706ca733e47d99414c92
Tsuyoshi Morita <golirev@gmail.com>
2020-03-28 10:51:36 +0900

    Fix `can't modify frozen String` error

--- lib/rubygems/ext/cmake_builder.rb
+++ lib/rubygems/ext/cmake_builder.rb
@@ -5,7 +5,7 @@ class Gem::Ext::CmakeBuilder < Gem::Ext::Builder
 
   def self.build(extension, dest_path, results, args=[], lib_dir=nil)
     unless File.exist?('Makefile')
-      cmd = "cmake . -DCMAKE_INSTALL_PREFIX=#{dest_path}"
+      cmd = +"cmake . -DCMAKE_INSTALL_PREFIX=#{dest_path}"
       cmd << " #{Gem::Command.build_args.join ' '}" unless Gem::Command.build_args.empty?
 
       run cmd, results

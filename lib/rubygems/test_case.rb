5f20647ec17b3de78eebe3c11e023050b9990af1
David Rodríguez <deivid.rodriguez@riseup.net>
2020-04-08 21:10:33 +0200

    Remove unnecessary `@@ruby` variable

--- lib/rubygems/test_case.rb
+++ lib/rubygems/test_case.rb
@@ -1201,7 +1201,7 @@ Also, a list:
 
   def build_rake_in(good=true)
     gem_ruby = Gem.ruby
-    Gem.ruby = @@ruby
+    Gem.ruby = self.class.rubybin
     env_rake = ENV["rake"]
     rake = (good ? @@good_rake : @@bad_rake)
     ENV["rake"] = rake
@@ -1270,7 +1270,6 @@ Also, a list:
 
   end
 
-  @@ruby = rubybin
   @@good_rake = "#{rubybin} #{escape_path(TEST_PATH, 'good_rake.rb')}"
   @@bad_rake = "#{rubybin} #{escape_path(TEST_PATH, 'bad_rake.rb')}"
 

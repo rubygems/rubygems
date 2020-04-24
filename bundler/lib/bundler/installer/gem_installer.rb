2a8b742a79cea5d71f145984159806ac45c1c028
David Rodríguez <deivid.rodriguez@riseup.net>
2020-04-24 21:48:50 +0200

    Consistently use qualified class names

--- bundler/lib/bundler/installer/gem_installer.rb
+++ bundler/lib/bundler/installer/gem_installer.rb
@@ -19,7 +19,7 @@ module Bundler
       Bundler.ui.debug "#{worker}:  #{spec.name} (#{spec.version}) from #{spec.loaded_from}"
       generate_executable_stubs
       return true, post_install_message
-    rescue Bundler::InstallHookError, Bundler::SecurityError, APIResponseMismatchError
+    rescue Bundler::InstallHookError, Bundler::SecurityError, Bundler::APIResponseMismatchError
       raise
     rescue Errno::ENOSPC
       return false, out_of_space_message

e53d457232ed1f2e98a526a5baa9f47f9bee5e24
Tomasz Pajor <tomek@polishgeeks.com>
2020-07-14 21:12:04 +0200

    add plugin hooks for bundle check

--- bundler/lib/bundler/cli/check.rb
+++ bundler/lib/bundler/cli/check.rb
@@ -14,10 +14,15 @@ module Bundler
       begin
         definition = Bundler.definition
         definition.validate_runtime!
+
+        Plugin.hook(Plugin::Events::GEM_BEFORE_CHECK_ALL, definition.dependencies)
+
         not_installed = definition.missing_specs
       rescue GemNotFound, VersionConflict
         Bundler.ui.error "Bundler can't satisfy your Gemfile's dependencies."
         Bundler.ui.warn "Install missing gems with `bundle install`."
+
+        Plugin.hook(Plugin::Events::GEM_AFTER_CHECK_ALL, definition.dependencies)
         exit 1
       end
 
@@ -25,13 +30,19 @@ module Bundler
         Bundler.ui.error "The following gems are missing"
         not_installed.each {|s| Bundler.ui.error " * #{s.name} (#{s.version})" }
         Bundler.ui.warn "Install missing gems with `bundle install`"
+
+        Plugin.hook(Plugin::Events::GEM_AFTER_CHECK_ALL, definition.dependencies)
         exit 1
       elsif !Bundler.default_lockfile.file? && Bundler.frozen_bundle?
         Bundler.ui.error "This bundle has been frozen, but there is no #{Bundler.default_lockfile.relative_path_from(SharedHelpers.pwd)} present"
+
+        Plugin.hook(Plugin::Events::GEM_AFTER_CHECK_ALL, definition.dependencies)
         exit 1
       else
         Bundler.load.lock(:preserve_unknown_sections => true) unless options[:"dry-run"]
         Bundler.ui.info "The Gemfile's dependencies are satisfied"
+
+        Plugin.hook(Plugin::Events::GEM_AFTER_CHECK_ALL, definition.dependencies)
       end
     end
   end

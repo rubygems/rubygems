c3995fb9f5b520f05936ea6ec99d8d4fad18f024
David Rodríguez <deivid.rodriguez@riseup.net>
2020-09-28 11:46:15 +0200

    Add tests for build_root ui messages

--- test/rubygems/test_gem_installer.rb
+++ test/rubygems/test_gem_installer.rb
@@ -1791,13 +1791,22 @@ gem 'other', version
 
   def test_process_options_build_root
     build_root = File.join @tempdir, 'build_root'
+    bin_dir = File.join(build_root, @gemhome.gsub(/^[a-zA-Z]:/, ''), 'bin')
+    gem_home = File.join(build_root, @gemhome.gsub(/^[a-zA-Z]:/, ''))
 
     @gem = setup_base_gem
-    installer = Gem::Installer.at @gem, :build_root => build_root
+    installer = use_ui(@ui) { Gem::Installer.at @gem, :build_root => build_root }
 
     assert_equal build_root, installer.build_root
-    assert_equal File.join(build_root, @gemhome.gsub(/^[a-zA-Z]:/, ''), 'bin'), installer.bin_dir
-    assert_equal File.join(build_root, @gemhome.gsub(/^[a-zA-Z]:/, '')), installer.gem_home
+    assert_equal bin_dir, installer.bin_dir
+    assert_equal gem_home, installer.gem_home
+
+    errors = @ui.error.split("\n")
+
+    assert_equal "WARNING:  You build with buildroot.", errors.shift
+    assert_equal "  Build root: #{build_root}", errors.shift
+    assert_equal "  Bin dir: #{bin_dir}", errors.shift
+    assert_equal "  Gem home: #{gem_home}", errors.shift
   end
 
   def test_shebang_arguments

b2bd23d62f7d6a293d254b56b5ce12567d15cf31
David Rodríguez <deivid.rodriguez@riseup.net>
2020-09-30 11:52:57 +0200

    Merge pull request #3975 from rubygems/buildroot_windows
    Fix `--build-root` flag under Windows
    
    (cherry picked from commit 9b25442ea8e031dd57466bbf5c123fe182dfbd78)

--- lib/rubygems/installer.rb
+++ lib/rubygems/installer.rb
@@ -693,10 +693,8 @@ class Gem::Installer
     @build_args = options[:build_args] || Gem::Command.build_args
 
     unless @build_root.nil?
-      require 'pathname'
-      @build_root = Pathname.new(@build_root).expand_path
-      @bin_dir = File.join(@build_root, options[:bin_dir] || Gem.bindir(@gem_home))
-      @gem_home = File.join(@build_root, @gem_home)
+      @bin_dir = File.join(@build_root, @bin_dir.gsub(/^[a-zA-Z]:/, ''))
+      @gem_home = File.join(@build_root, @gem_home.gsub(/^[a-zA-Z]:/, ''))
       alert_warning "You build with buildroot.\n  Build root: #{@build_root}\n  Bin dir: #{@bin_dir}\n  Gem home: #{@gem_home}"
     end
   end

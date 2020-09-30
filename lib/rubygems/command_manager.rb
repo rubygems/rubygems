90b6d8f383059a381e843559022b32b9e975ecc7
bronzdoc <lsagastume1990@gmail.com>
2020-09-30 14:49:00 -0600

    Show warning at the top for every deprecated command

--- lib/rubygems/command_manager.rb
+++ lib/rubygems/command_manager.rb
@@ -174,13 +174,8 @@ class Gem::CommandManager
     else
       cmd_name = args.shift.downcase
       cmd = find_command cmd_name
-      if cmd_name == "server"
-        cmd.deprecation_warning if cmd.deprecated?
-        cmd.invoke_with_build_args args, build_args
-      else
-        cmd.invoke_with_build_args args, build_args
-        cmd.deprecation_warning if cmd.deprecated?
-      end
+      cmd.deprecation_warning if cmd.deprecated?
+      cmd.invoke_with_build_args args, build_args
     end
   end
 

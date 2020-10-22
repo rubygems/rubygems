98498fd4b52a4ce23c7fa1d6d6056ad0634a64f9
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-10-23 08:15:46 +0900

    Merge pull request #4023 from landongrindheim/move-description-into-query-command
    Omit deprecated commands from command help output
    
    (cherry picked from commit a3427c7b56f3afb138477cd9382e0af57b495dc4)

--- lib/rubygems/commands/query_command.rb
+++ lib/rubygems/commands/query_command.rb
@@ -31,4 +31,13 @@ class Gem::Commands::QueryCommand < Gem::Command
 
     add_query_options
   end
+
+  def description # :nodoc:
+    <<-EOF
+The query command is the basis for the list and search commands.
+
+You should really use the list and search commands instead.  This command
+is too hard to use.
+    EOF
+  end
 end

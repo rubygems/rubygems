cf5d93ab5b53c6e7dc742dabde07deed54e96837
David Rodríguez <deivid.rodriguez@riseup.net>
2020-10-28 13:52:39 +0100

    Make spec more complete

--- bundler/spec/install/gemfile/specific_platform_spec.rb
+++ bundler/spec/install/gemfile/specific_platform_spec.rb
@@ -25,12 +25,15 @@ RSpec.describe "bundle install with specific_platform enabled" do
       ])
     end
 
-    it "caches both the universal-darwin and ruby gems when --all-platforms is passed" do
+    it "caches both the universal-darwin and ruby gems when --all-platforms is passed and properly picks them up on further bundler invocations" do
       setup_multiplatform_gem
       gemfile(google_protobuf)
       bundle "package --all-platforms"
       expect([cached_gem("google-protobuf-3.0.0.alpha.5.0.5.1"), cached_gem("google-protobuf-3.0.0.alpha.5.0.5.1-universal-darwin")]).
         to all(exist)
+
+      bundle "install --verbose"
+      expect(err).to be_empty
     end
 
     it "caches multiplatform git gems with a single gemspec when --all-platforms is passed" do

31dc763f05ad54bc081d87b18a5c8b223a9b9f83
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-12-10 15:15:56 +0900

    Guard for the old Ruby

--- bundler/spec/bundler/cli_spec.rb
+++ bundler/spec/bundler/cli_spec.rb
@@ -10,7 +10,7 @@ using Module.new {
       super.gsub(/.[\b]/, "")
     end
   end
-}
+} if RUBY_VERSION >= "2.4"
 
 RSpec.describe "bundle executable" do
   it "returns non-zero exit status when passed unrecognized options" do

bd921b7970cc56e952e14836628928b741177885
David Rodríguez <deivid.rodriguez@riseup.net>
2020-12-18 12:43:07 +0100

    Use `GemParser` from rubygems if present

--- bundler/lib/bundler/compact_index_client/gem_parser.rb
+++ bundler/lib/bundler/compact_index_client/gem_parser.rb
@@ -2,22 +2,26 @@
 
 module Bundler
   class CompactIndexClient
-    class GemParser
-      def parse(line)
-        version_and_platform, rest = line.split(" ", 2)
-        version, platform = version_and_platform.split("-", 2)
-        dependencies, requirements = rest.split("|", 2).map {|s| s.split(",") } if rest
-        dependencies = dependencies ? dependencies.map {|d| parse_dependency(d) } : []
-        requirements = requirements ? requirements.map {|d| parse_dependency(d) } : []
-        [version, platform, dependencies, requirements]
-      end
+    if defined?(Gem::Resolver::APISet::GemParser)
+      GemParser = Gem::Resolver::APISet::GemParser
+    else
+      class GemParser
+        def parse(line)
+          version_and_platform, rest = line.split(" ", 2)
+          version, platform = version_and_platform.split("-", 2)
+          dependencies, requirements = rest.split("|", 2).map {|s| s.split(",") } if rest
+          dependencies = dependencies ? dependencies.map {|d| parse_dependency(d) } : []
+          requirements = requirements ? requirements.map {|d| parse_dependency(d) } : []
+          [version, platform, dependencies, requirements]
+        end
 
-      private
+        private
 
-      def parse_dependency(string)
-        dependency = string.split(":")
-        dependency[-1] = dependency[-1].split("&") if dependency.size > 1
-        dependency
+        def parse_dependency(string)
+          dependency = string.split(":")
+          dependency[-1] = dependency[-1].split("&") if dependency.size > 1
+          dependency
+        end
       end
     end
   end

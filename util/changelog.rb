0df2be761ae61379dc625f1dd2581e3aee2dd6da
David Rodríguez <deivid.rodriguez@riseup.net>
2020-12-28 10:20:31 +0100

    Merge pull request #4198 from rubygems/fix_release_notes
    Fix release notes including not only the latest release
    
    (cherry picked from commit a3c8a5f4bd9b0b1cbdf47d78178fec3ad23c60a9)

--- util/changelog.rb
+++ util/changelog.rb
@@ -32,7 +32,6 @@ class Changelog
 
   def release_notes
     current_version_title = "#{release_section_token}#{version}"
-    current_minor_title = "#{release_section_token}#{version.segments[0, 2].join(".")}"
 
     current_version_index = lines.find_index {|line| line.strip =~ /^#{current_version_title}($|\b)/ }
     unless current_version_index
@@ -41,7 +40,7 @@ class Changelog
     current_version_index += 1
     previous_version_lines = lines[current_version_index.succ...-1]
     previous_version_index = current_version_index + (
-      previous_version_lines.find_index {|line| line.start_with?(release_section_token) && !line.start_with?(current_minor_title) } ||
+      previous_version_lines.find_index {|line| line.start_with?(release_section_token) } ||
       lines.count
     )
 

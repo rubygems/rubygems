dc1f4f5c9e7b4c20ba11d71dad419e418d909d00
David Rodríguez <deivid.rodriguez@riseup.net>
2021-01-06 15:29:59 +0100

    Add MacOS to bundler's CI
    * It can help us catch MacOS specific issues.
    * It aligns bundler specs with rubygems which already run on MacOS.
    * It allows bundler developers using MacOS to have a green suite.
    * It can reduce friction with ruby-core by catching CI issues that will be detected later by them otherwise.

--- bundler/spec/realworld/edgecases_spec.rb
+++ bundler/spec/realworld/edgecases_spec.rb
@@ -222,7 +222,7 @@ RSpec.describe "real world edgecases", :realworld => true do
   end
 
   it "doesn't hang on big gemfile" do
-    skip "Only for ruby 2.7.3" if RUBY_VERSION != "2.7.3"
+    skip "Only for ruby 2.7.3" if RUBY_VERSION != "2.7.3" || RUBY_PLATFORM =~ /darwin/
 
     gemfile <<~G
       # frozen_string_literal: true
@@ -334,7 +334,7 @@ RSpec.describe "real world edgecases", :realworld => true do
   end
 
   it "doesn't hang on tricky gemfile" do
-    skip "Only for ruby 2.7.3" if RUBY_VERSION != "2.7.3"
+    skip "Only for ruby 2.7.3" if RUBY_VERSION != "2.7.3" || RUBY_PLATFORM =~ /darwin/
 
     gemfile <<~G
       source 'https://rubygems.org'
@@ -360,7 +360,7 @@ RSpec.describe "real world edgecases", :realworld => true do
   end
 
   it "doesn't hang on nix gemfile" do
-    skip "Only for ruby 3.0.1" if RUBY_VERSION != "3.0.1"
+    skip "Only for ruby 3.0.1" if RUBY_VERSION != "3.0.1" || RUBY_PLATFORM =~ /darwin/
 
     gemfile <<~G
       source "https://rubygems.org" do

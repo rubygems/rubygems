rubygems/rubygems

commit ad632bb0800a07433b8d9a9dd8269dd3bcb2f068

Never spawn subshells when building extensions
As a result, we no longer need to manually quote `DESTDIR`, and all
commands printed as part of error messages are now properly escaped so
they can be copy-pasted directly into the shell.

Co-authored-by: Nobuyoshi Nakada <nobu@ruby-lang.org>

lib/rubygems/ext/builder.rb
@@ -29,7 +29,7 @@ class Gem::Ext::Builder
       make_program = (/mswin/ =~ RUBY_PLATFORM) ? 'nmake' : 'make'
     end
 
-    destdir = '"DESTDIR=%s"' % ENV['DESTDIR']
+    destdir = 'DESTDIR=%s' % ENV['DESTDIR']
 
     ['clean', '', 'install'].each do |target|
       # Pass DESTDIR via command line to override what's in MAKEFLAGS
@@ -37,7 +37,7 @@ class Gem::Ext::Builder
         make_program,
         destdir,
         target,
-      ].join(' ').rstrip
+      ].reject(&:empty?)
       begin
         run(cmd, results, "make #{target}".rstrip, make_dir)
       rescue Gem::InstallError
@@ -56,7 +56,7 @@ class Gem::Ext::Builder
         p(command)
       end
       results << "current directory: #{dir}"
-      results << (command.respond_to?(:shelljoin) ? command.shelljoin : command)
+      results << command.shelljoin
 
       require "open3"
       # Set $SOURCE_DATE_EPOCH for the subprocess.

test/rubygems/test_gem_ext_builder.rb
@@ -47,9 +47,9 @@ install:
 
     results = results.join("\n").b
 
-    assert_match %r{"DESTDIR=#{ENV['DESTDIR']}" clean$},   results
-    assert_match %r{"DESTDIR=#{ENV['DESTDIR']}"$},         results
-    assert_match %r{"DESTDIR=#{ENV['DESTDIR']}" install$}, results
+    assert_match %r{DESTDIR\\=#{ENV['DESTDIR']} clean$},   results
+    assert_match %r{DESTDIR\\=#{ENV['DESTDIR']}$},         results
+    assert_match %r{DESTDIR\\=#{ENV['DESTDIR']} install$}, results
 
     if /nmake/ !~ results
       assert_match %r{^clean: destination$},   results
@@ -76,9 +76,9 @@ install:
 
     results = results.join("\n").b
 
-    assert_match %r{"DESTDIR=#{ENV['DESTDIR']}" clean$},   results
-    assert_match %r{"DESTDIR=#{ENV['DESTDIR']}"$},         results
-    assert_match %r{"DESTDIR=#{ENV['DESTDIR']}" install$}, results
+    assert_match %r{DESTDIR\\=#{ENV['DESTDIR']} clean$},   results
+    assert_match %r{DESTDIR\\=#{ENV['DESTDIR']}$},         results
+    assert_match %r{DESTDIR\\=#{ENV['DESTDIR']} install$}, results
   end
 
   def test_build_extensions

lib/rubygems/ext/cmake_builder.rb
@@ -4,8 +4,7 @@ require_relative '../command'
 class Gem::Ext::CmakeBuilder < Gem::Ext::Builder
   def self.build(extension, dest_path, results, args=[], lib_dir=nil, cmake_dir=Dir.pwd)
     unless File.exist?(File.join(cmake_dir, 'Makefile'))
-      cmd = "cmake . -DCMAKE_INSTALL_PREFIX=#{dest_path}"
-      cmd << " #{Gem::Command.build_args.join ' '}" unless Gem::Command.build_args.empty?
+      cmd = ["cmake", ".", "-DCMAKE_INSTALL_PREFIX=#{dest_path}", *Gem::Command.build_args]
 
       run cmd, results, class_name, cmake_dir
     end

lib/rubygems/ext/configure_builder.rb
@@ -8,8 +8,7 @@
 class Gem::Ext::ConfigureBuilder < Gem::Ext::Builder
   def self.build(extension, dest_path, results, args=[], lib_dir=nil, configure_dir=Dir.pwd)
     unless File.exist?(File.join(configure_dir, 'Makefile'))
-      cmd = "sh ./configure --prefix=#{dest_path}"
-      cmd << " #{args.join ' '}" unless args.empty?
+      cmd = ["sh", "./configure", "--prefix=#{dest_path}", *args]
 
       run cmd, results, class_name, configure_dir
     end

test/rubygems/test_gem_ext_cmake_builder.rb
@@ -40,7 +40,7 @@ install (FILES test.txt DESTINATION bin)
 
     output = output.join "\n"
 
-    assert_match %r{^cmake \. -DCMAKE_INSTALL_PREFIX=#{Regexp.escape @dest_path}}, output
+    assert_match %r{^cmake \. -DCMAKE_INSTALL_PREFIX\\=#{Regexp.escape @dest_path}}, output
     assert_match %r{#{Regexp.escape @ext}}, output
     assert_contains_make_command '', output
     assert_contains_make_command 'install', output
@@ -60,7 +60,7 @@ install (FILES test.txt DESTINATION bin)
 
     assert_match 'cmake failed', error.message
 
-    assert_match %r{^cmake . -DCMAKE_INSTALL_PREFIX=#{Regexp.escape @dest_path}}, output
+    assert_match %r{^cmake . -DCMAKE_INSTALL_PREFIX\\=#{Regexp.escape @dest_path}}, output
     assert_match %r{#{shell_error_msg}}, output
   end
 

test/rubygems/test_gem_ext_configure_builder.rb
@@ -28,7 +28,7 @@ class TestGemExtConfigureBuilder < Gem::TestCase
     Gem::Ext::ConfigureBuilder.build nil, @dest_path, output, [], nil, @ext
 
     assert_match(/^current directory:/, output.shift)
-    assert_equal "sh ./configure --prefix=#{@dest_path}", output.shift
+    assert_equal "sh ./configure --prefix\\=#{@dest_path}", output.shift
     assert_equal "", output.shift
     assert_match(/^current directory:/, output.shift)
     assert_contains_make_command 'clean', output.shift
@@ -50,7 +50,7 @@ class TestGemExtConfigureBuilder < Gem::TestCase
     end
 
     shell_error_msg = %r{(\./configure: .*)|((?:[Cc]an't|cannot) open '?\./configure'?(?:: No such file or directory)?)}
-    sh_prefix_configure = "sh ./configure --prefix="
+    sh_prefix_configure = "sh ./configure --prefix\\="
 
     assert_match 'configure failed', error.message
 

commit 2c1c2001356afb92a4285e0bb33f47a00a9a2e61

Gem::Ext::Builder: accept custom make command with extra options
Debian's gem2deb passes a set of build variables to Rubygems extension
builders using ENV['make']. These options are used to produce hardened
binaries, as well as to help with reproducible builds.

This fixes a regression introduced by
ad632bb0800a07433b8d9a9dd8269dd3bcb2f068.

lib/rubygems/ext/builder.rb
@@ -28,13 +28,14 @@ class Gem::Ext::Builder
     unless make_program
       make_program = (/mswin/ =~ RUBY_PLATFORM) ? 'nmake' : 'make'
     end
+    make_program = Shellwords.split(make_program)
 
     destdir = 'DESTDIR=%s' % ENV['DESTDIR']
 
     ['clean', '', 'install'].each do |target|
       # Pass DESTDIR via command line to override what's in MAKEFLAGS
       cmd = [
-        make_program,
+        *make_program,
         destdir,
         target,
       ].reject(&:empty?)

test/rubygems/test_gem_ext_builder.rb
@@ -14,6 +14,7 @@ class TestGemExtBuilder < Gem::TestCase
     FileUtils.mkdir_p @dest_path
 
     @orig_DESTDIR = ENV['DESTDIR']
+    @orig_make = ENV['make']
 
     @spec = util_spec 'a'
 
@@ -22,6 +23,7 @@ class TestGemExtBuilder < Gem::TestCase
 
   def teardown
     ENV['DESTDIR'] = @orig_DESTDIR
+    ENV['make'] = @orig_make
 
     super
   end
@@ -79,6 +81,28 @@ install:
     assert_match %r{DESTDIR\\=#{ENV['DESTDIR']} clean$},   results
     assert_match %r{DESTDIR\\=#{ENV['DESTDIR']}$},         results
     assert_match %r{DESTDIR\\=#{ENV['DESTDIR']} install$}, results
+  end
+
+  def test_custom_make_with_options
+    ENV['make'] = 'make V=1'
+    results = []
+    File.open File.join(@ext, 'Makefile'), 'w' do |io|
+      io.puts <<-MAKEFILE
+all:
+\t@#{Gem.ruby} -e "puts 'all: OK'"
+
+clean:
+\t@#{Gem.ruby} -e "puts 'clean: OK'"
+
+install:
+\t@#{Gem.ruby} -e "puts 'install: OK'"
+      MAKEFILE
+    end
+    Gem::Ext::Builder.make @dest_path, results, @ext
+    results = results.join("\n").b
+    assert_match %r{clean: OK}, results
+    assert_match %r{all: OK}, results
+    assert_match %r{install: OK}, results
   end
 
   def test_build_extensions
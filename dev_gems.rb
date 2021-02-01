950c14804c77b352b2de881d22074340cf60d2d4
David Rodríguez <deivid.rodriguez@riseup.net>
2021-02-01 14:32:03 +0100

    Bump simplecov to 0.21.2

--- dev_gems.rb
+++ dev_gems.rb
@@ -3,7 +3,7 @@ source "https://rubygems.org"
 gem "rdoc", "6.2.0" # 6.2.1 is required > Ruby 2.3
 gem "minitest", "~> 5.14", ">= 5.14.3" # 5.14.3 is required to support Ruby 3.1
 gem "minitest-bisect", "~> 1.5"
-gem "simplecov", "~> 0.17.0"
+gem "simplecov", "~> 0.21.2"
 gem "rubocop", "~> 0.80.1"
 gem "rubocop-performance", "~> 1.5.2"
 gem "webrick", "~> 1.6"

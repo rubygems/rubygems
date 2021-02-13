f1d44093a9b5879c33634c2e766702ea87c494a8
Nobuyoshi Nakada <nobu@ruby-lang.org>
2021-02-13 09:53:57 +0900

    Run rake in the source directory
    As well as accessing the Rakefile relative to `__FILE__`.

--- test/rubygems/test_project_sanity.rb
+++ test/rubygems/test_project_sanity.rb
@@ -7,7 +7,7 @@ class TestProjectSanity < Gem::TestCase
   def test_manifest_is_up_to_date
     skip unless File.exist?(File.expand_path("../../../Rakefile", __FILE__))
 
-    _, status = Open3.capture2e("rake check_manifest")
+    _, status = Open3.capture2e("rake check_manifest", chdir: __dir__)
 
     assert status.success?, "Expected Manifest.txt to be up to date, but it's not. Run `rake update_manifest` to sync it."
   end

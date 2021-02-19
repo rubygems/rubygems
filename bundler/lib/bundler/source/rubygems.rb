d8a9905adaeb7f5202826469ea65cb5a3bf09ca3
David Rodríguez <deivid.rodriguez@riseup.net>
2021-02-19 11:22:49 +0100

    Don't invalidate the cache is `@remote` or `@cached` already set

--- bundler/lib/bundler/source/rubygems.rb
+++ bundler/lib/bundler/source/rubygems.rb
@@ -26,11 +26,15 @@ module Bundler
       end
 
       def remote!
+        return if @allow_remote
+
         @specs = nil
         @allow_remote = true
       end
 
       def cached!
+        return if @allow_cached
+
         @specs = nil
         @allow_cached = true
       end

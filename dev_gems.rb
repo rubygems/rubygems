777f9857b674c4f884c1fdfe8a44779740c4dcd5
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2021-05-11 18:05:03 +0900

    Downgrade faraday-1.0.1 for Ruby 2.3

--- dev_gems.rb
+++ dev_gems.rb
@@ -13,4 +13,5 @@ gem "rspec-expectations", "~> 3.8"
 gem "rspec-mocks", "~> 3.8"
 gem "netrc", "~> 0.11.0"
 gem "octokit", "~> 4.18"
+gem "faraday", "1.0.1" # 1.1+ is required > Ruby 2.3
 gem "aws-sdk-s3", "~> 1.87"

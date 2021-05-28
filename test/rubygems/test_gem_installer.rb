2b5e691f487422401f566355d1be789cb107df92
David Rodríguez <deivid.rodriguez@riseup.net>
2021-05-28 12:38:00 +0200

    Merge pull request #4629 from rubygems/mswin
    Run rubygems CI on mswin
    
    (cherry picked from commit bbc2f38e2412ded7117f498c3626674f35f2a193)

--- test/rubygems/test_gem_installer.rb
+++ test/rubygems/test_gem_installer.rb
@@ -1481,6 +1481,7 @@ gem 'other', version
 
   def test_install_extension_and_script
     pend "Makefile creation crashes on jruby" if Gem.java_platform?
+    pend if /mswin/ =~ RUBY_PLATFORM && ENV.key?('GITHUB_ACTIONS') # not working from the beginning
 
     @spec = setup_base_spec
     @spec.extensions << "extconf.rb"

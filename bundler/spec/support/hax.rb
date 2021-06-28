50e762a8761d441a0446006402ed6cf1153affca
David Rodríguez <deivid.rodriguez@riseup.net>
2021-06-28 17:12:39 +0200

    Move API request hacks to a separate file

--- bundler/spec/support/hax.rb
+++ bundler/spec/support/hax.rb
@@ -35,18 +35,3 @@ module Gem
     end
   end
 end
-
-if ENV["BUNDLER_SPEC_API_REQUEST_LIMIT"]
-  require_relative "path"
-  require "bundler/source"
-  require "bundler/source/rubygems"
-
-  module Bundler
-    class Source
-      class Rubygems < Source
-        remove_const :API_REQUEST_LIMIT
-        API_REQUEST_LIMIT = ENV["BUNDLER_SPEC_API_REQUEST_LIMIT"].to_i
-      end
-    end
-  end
-end

fadf5e03ea432515e8dd73404a34e06935a2d1df
David Rodríguez <deivid.rodriguez@riseup.net>
2021-08-21 17:39:42 +0200

    Move `Bundler.rubygems.clear_paths` to a better place
    Since it clears both home and path.

--- bundler/lib/bundler.rb
+++ bundler/lib/bundler.rb
@@ -657,6 +657,7 @@ EOF
     def configure_gem_home_and_path
       configure_gem_path
       configure_gem_home
+      Bundler.rubygems.clear_paths
     end
 
     def configure_gem_path
@@ -670,7 +671,6 @@ EOF
 
     def configure_gem_home
       Bundler::SharedHelpers.set_env "GEM_HOME", bundle_path.to_s
-      Bundler.rubygems.clear_paths
     end
 
     def tmp_home_path

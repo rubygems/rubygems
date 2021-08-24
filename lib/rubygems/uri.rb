b2009e226932e8c0945d748f304e97150e53f6ba
David Rodríguez <deivid.rodriguez@riseup.net>
2021-08-24 12:02:29 +0200

    Merge `Gem::UriParser` and `Gem::PrintableUri` into a `Gem::Uri` class
    The new class is a wrapper on top of an URI. And then, when you want
    credentials redacted, you call #redact that returns a copy of itself,
    but with credentials redacted.

---
+++ lib/rubygems/uri.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+##
+# The Uri handles rubygems source URIs.
+#
+
+class Gem::Uri
+  def initialize(source_uri)
+    @parsed_uri = parse(source_uri)
+  end
+
+  def redacted
+    return self unless valid_uri?
+
+    if token?
+      clone.tap(&:redact_user!)
+    elsif oauth_basic?
+      clone.tap(&:redact_user!)
+    elsif password?
+      clone.tap(&:redact_password!)
+    else
+      self
+    end
+  end
+
+  def to_s
+    @parsed_uri.to_s
+  end
+
+  def redact_credentials_from(text)
+    return text unless valid_uri? && password?
+
+    text.sub(password, 'REDACTED')
+  end
+
+  def method_missing(method_name, *args, &blk)
+    if @parsed_uri.respond_to?(method_name)
+      @parsed_uri.send(method_name, *args, &blk)
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    @parsed_uri.respond_to?(method_name, include_private) || super
+  end
+
+  private
+
+  ##
+  # Parses the #uri, raising if it's invalid
+
+  def parse!(uri)
+    require "uri"
+
+    raise URI::InvalidURIError unless uri
+
+    # Always escape URI's to deal with potential spaces and such
+    # It should also be considered that source_uri may already be
+    # a valid URI with escaped characters. e.g. "{DESede}" is encoded
+    # as "%7BDESede%7D". If this is escaped again the percentage
+    # symbols will be escaped.
+    begin
+      URI.parse(uri)
+    rescue URI::InvalidURIError
+      URI.parse(URI::DEFAULT_PARSER.escape(uri))
+    end
+  end
+
+  ##
+  # Parses the #uri, returning the original uri if it's invalid
+
+  def parse(uri)
+    return uri unless uri.is_a?(String)
+
+    parse!(uri)
+  rescue URI::InvalidURIError
+    uri
+  end
+
+  def redact_user!
+    self.user = 'REDACTED'
+  end
+
+  def redact_password!
+    self.password = 'REDACTED'
+  end
+
+  def valid_uri?
+    !@parsed_uri.is_a?(String)
+  end
+
+  def password?
+    !!password
+  end
+
+  def oauth_basic?
+    password == 'x-oauth-basic'
+  end
+
+  def token?
+    !user.nil? && password.nil?
+  end
+end

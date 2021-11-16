29c20f9e4bd197e41c5038dac1aad2026e891263
David Rodríguez <deivid.rodriguez@riseup.net>
2021-11-16 12:54:15 +0100

    Lock rubocop-ast on ruby 2.4

--- bundler/tool/bundler/standard24_gems.rb
+++ bundler/tool/bundler/standard24_gems.rb
@@ -3,6 +3,7 @@
 source "https://rubygems.org"
 
 gem "standard", "~> 1.0"
+gem "rubocop-ast", "1.4.1" # 1.4.2+ is required > Ruby 2.4
 gem "parallel", "1.19.2" # 1.20+ is required > Ruby 2.4
 
 gem "minitest"

6481c3ef5b5e8124c4b419ea704689ce72f53493
David Rodríguez <deivid.rodriguez@riseup.net>
2021-11-30 21:01:21 +0100

    Fix typo in skip message

--- bundler/spec/commands/exec_spec.rb
+++ bundler/spec/commands/exec_spec.rb
@@ -614,7 +614,7 @@ RSpec.describe "bundle exec" do
 
   it "loads the correct optparse when `auto_install` is set, and optparse is a dependency" do
     if Gem.ruby_version >= Gem::Version.new("3.0.0") && Gem.rubygems_version < Gem::Version.new("3.3.0.a")
-      skip "optparse is a default gem, and rubygems loads install during install"
+      skip "optparse is a default gem, and rubygems loads it during install"
     end
 
     build_repo4 do

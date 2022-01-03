6d24fd52b21b46e0921553f3c8f113cc4901a0c1
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-01-03 08:27:52 -0500

    Cleanup for readability

--- lib/rubygems/ext/cargo_builder.rb
+++ lib/rubygems/ext/cargo_builder.rb
@@ -20,9 +20,8 @@ class Gem::Ext::CargoBuilder < Gem::Ext::Builder
 
   def build(extension, dest_path, results, args=[], lib_dir=nil, cargo_dir=Dir.pwd)
     build_crate(extension, dest_path, results, args, lib_dir, cargo_dir)
-    dylib = validate_cargo_build!(dest_path)
-    FileUtils.cp(dylib, dylib.gsub(File.basename(dylib), "#{spec.name}.#{RbConfig::CONFIG['DLEXT']}"))
-
+    dylib_path = validate_cargo_build!(dest_path)
+    rename_cdylib_for_ruby_compatibility(dylib_path)
     finalize_directory(extension, dest_path, results, args, lib_dir, cargo_dir)
     results
   end
@@ -52,67 +51,14 @@ class Gem::Ext::CargoBuilder < Gem::Ext::Builder
     end
   end
 
-  def validate_cargo_build!(dir)
-    dylibs = Dir.glob(File.join(dir, 'release', "lib#{spec.name}.{so,dylib}"))
-
-    raise DylibNotFoundError.new(dir) if dylibs.empty?
-
-    dylibs.first
-  end
-
-  def dynamic_linker_flags
-    args = RbConfig::CONFIG['DLDFLAGS'].strip.split(" ")
-
-    args.flat_map {|a| ldflag_to_link_mofifier(a) }.compact
-  end
-
-  def ldflag_to_link_mofifier(arg)
-    flag = arg[0..1]
-    val = arg[2..-1]
-
-    case flag
-    when "-L"
-      ["-L", "native=#{val}"]
-    when "-l"
-      ["-l", "dylib=#{val}"]
-    when "-F"
-      ["-l", "framework=#{val}"]
-    when "-W"
-      ["-C", "link_arg=#{arg}"]
-    end
-  end
-
-  def libruby_flags_to_link_modifiers(str)
-    flags = []
-
-    str.scan(/-framework (\S+)/).flatten.each do |framework|
-      flags << "-l" << "framework=#{framework}"
-    end
-
-    str.scan(/-l\s*(\S+)/).flatten.each do |lib|
-      kind = lib.include?("static") ? "static" : "dylib"
-
-      # Do not actually link ruby since it is loaded at runtime
-      next if lib.start_with?("ruby")
-
-      flags << "-l" << "#{kind}=#{lib}"
-    end
-
-    flags
-  end
-
-  def libruby_arg
-    ruby_static? ? RbConfig::CONFIG['LIBRUBYARG_STATIC'] : RbConfig::CONFIG['LIBRUBYARG_SHARED']
-  end
-
   def cargo_rustc_args(dest_dir)
     [
       '--lib',
       '--release',
       '--frozen',
       '--',
-      *dynamic_linker_flags,
-      *libruby_flags_to_link_modifiers(libruby_arg),
+      *rustc_dynamic_linker_flags,
+      *rustc_libruby_flags,
     ]
   end
 
@@ -168,5 +114,65 @@ class Gem::Ext::CargoBuilder < Gem::Ext::Builder
   def get_relative_path(path, base)
     path[0..base.length - 1] = '.' if path.start_with?(base)
     path
+  end
+
+  # Ruby expects the dylib to follow a file name convention for loading
+  def rename_cdylib_for_ruby_compatibility(dylib_path)
+    new_name = dylib_path.gsub(File.basename(dylib_path), "#{spec.name}.#{RbConfig::CONFIG['DLEXT']}")
+    FileUtils.cp(dylib_path, new_name)
+  end
+
+  def validate_cargo_build!(dir)
+    dylib_path = File.join(dir, 'release', "lib#{spec.name}.#{RbConfig::CONFIG['SOEXT']}")
+
+    raise DylibNotFoundError.new(dir) unless File.exist?(dylib_path)
+
+    dylib_path
+  end
+
+  def rustc_dynamic_linker_flags
+    args = RbConfig::CONFIG['DLDFLAGS'].strip.split(" ")
+
+    args.flat_map {|a| ldflag_to_link_mofifier(a) }.compact
+  end
+
+  def ldflag_to_link_mofifier(arg)
+    flag = arg[0..1]
+    val = arg[2..-1]
+
+    case flag
+    when "-L"
+      ["-L", "native=#{val}"]
+    when "-l"
+      ["-l", "dylib=#{val}"]
+    when "-F"
+      ["-l", "framework=#{val}"]
+    when "-W"
+      ["-C", "link_arg=#{arg}"]
+    end
+  end
+
+  # Converts the linker args for libruby into the flags needed for rustc
+  def rustc_libruby_flags(str = libruby_arg)
+    flags = []
+
+    str.scan(/-framework (\S+)/).flatten.each do |framework|
+      flags << "-l" << "framework=#{framework}"
+    end
+
+    str.scan(/-l\s*(\S+)/).flatten.each do |lib|
+      kind = lib.include?("static") ? "static" : "dylib"
+
+      # Do not actually link ruby since it is loaded at runtime
+      next if lib.start_with?("ruby")
+
+      flags << "-l" << "#{kind}=#{lib}"
+    end
+
+    flags
+  end
+
+  def libruby_arg
+    ruby_static? ? RbConfig::CONFIG['LIBRUBYARG_STATIC'] : RbConfig::CONFIG['LIBRUBYARG_SHARED']
   end
 end

533c47a88a3ca94cb1811846729b6fe9199a1acd
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-01-04 21:48:29 -0500

    Ignore interpolated vars

--- lib/rubygems/ext/cargo_builder.rb
+++ lib/rubygems/ext/cargo_builder.rb
@@ -141,7 +141,7 @@ class Gem::Ext::CargoBuilder < Gem::Ext::Builder
     when "-L"         then ["-L", "native=#{val}"]
     when "-l"         then ["-l", val.to_s]
     when "-F"         then ["-l", "framework=#{val}"]
-    when "${"         then [] # ignore interpolated vars
+    when "$("         then [] # ignore interpolated vars
     else                   ["-C", "link_arg=#{arg}"]
     end
   end

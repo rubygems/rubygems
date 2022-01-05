e1905a4237086cb1cb2bfb2fe92dffaba914f59a
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-01-05 09:20:41 -0500

    Fix build.rb

--- test/rubygems/test_gem_ext_cargo_builder/rust_ruby_example/build.rb
+++ test/rubygems/test_gem_ext_cargo_builder/rust_ruby_example/build.rb
@@ -1,21 +1,20 @@
-if ENV['RUBYOPT'] or defined? Gem
-  ENV.delete 'RUBYOPT'
+# if ENV['RUBYOPT'] or defined? Gem
+#   ENV.delete 'RUBYOPT'
 
-  require 'rbconfig'
-  cmd = [RbConfig.ruby, '--disable-gems', 'BUILD.rb', *ARGV]
+#   require 'rbconfig'
+#   cmd = [RbConfig.ruby, '--disable-gems', 'BUILD.rb', *ARGV]
 
-  exec(*cmd)
-end
+#   exec(*cmd)
+# end
 
-$LOAD_PATH.unshift(File.expand_path("../../../../../lib", __FILE__))
+lp = File.expand_path("../../../../../lib", __FILE__)
 
 require 'tmpdir'
 
 Dir.mktmpdir("rust_ruby_example") do |dir|
   built_gem = File.expand_path(File.join(dir, "rust_ruby_example.gem"))
-  gem = File.expand_path("../../../../../bin/gem", __FILE__)
-  system gem, "build", "rust_ruby_example.gemspec", "--output", built_gem
-  system gem, "install", "--local", built_gem, *ARGV
+  gem = ["ruby", "-I#{lp}", File.expand_path("../../../../../bin/gem", __FILE__)]
+  system *gem, "build", "rust_ruby_example.gemspec", "--output", built_gem
+  system *gem, "install", "--verbose", "--local", built_gem, *ARGV
+  system %q(ruby -rrust_ruby_example -e "puts 'Result: ' + RustRubyExample.reverse('hello world')")
 end
-
-system %q(ruby -rrust_ruby_example -e "puts 'Result: ' + RustRubyExample.reverse('hello world')")

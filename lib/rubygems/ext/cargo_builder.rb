9b8be6d74f1c2f9279448c836accde9c828c0c82
Ian Ker-Seymer <i.kerseymer@gmail.com>
2022-01-04 19:47:25 -0500

    Do not specify linker

--- lib/rubygems/ext/cargo_builder.rb
+++ lib/rubygems/ext/cargo_builder.rb
@@ -57,7 +57,7 @@ class Gem::Ext::CargoBuilder < Gem::Ext::Builder
 
   def platform_specific_rustc_args
     flags = []
-    flags += ["-C", "linker=clang"] if Gem.win_platform?
+    # flags += ["-C", "linker=clang"] if Gem.win_platform?
     flags
   end
 

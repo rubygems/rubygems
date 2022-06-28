1d116229c578c417235f084b33c89c1d39a8c129
David Rodríguez <deivid.rodriguez@riseup.net>
2022-06-28 20:17:42 +0200

    Remove unnecessary `defined?` checks
    As far as I can see, RubyGems has never actually defined these. These
    checks were initially added at
    https://github.com/rubygems/bundler/commit/ab3bd7b1861243bf21c411f3ac2b53bf8a286832
    almost 10 years ago. I believe it was to fix some double load issue but
    I don't think they are needed anymore.

--- bundler/lib/bundler/rubygems_ext.rb
+++ bundler/lib/bundler/rubygems_ext.rb
@@ -216,11 +216,11 @@ module Gem
   require "rubygems/platform"
 
   class Platform
-    JAVA  = Gem::Platform.new("java") unless defined?(JAVA)
-    MSWIN = Gem::Platform.new("mswin32") unless defined?(MSWIN)
-    MSWIN64 = Gem::Platform.new("mswin64") unless defined?(MSWIN64)
-    MINGW = Gem::Platform.new("x86-mingw32") unless defined?(MINGW)
-    X64_MINGW = Gem::Platform.new("x64-mingw32") unless defined?(X64_MINGW)
+    JAVA  = Gem::Platform.new("java")
+    MSWIN = Gem::Platform.new("mswin32")
+    MSWIN64 = Gem::Platform.new("mswin64")
+    MINGW = Gem::Platform.new("x86-mingw32")
+    X64_MINGW = Gem::Platform.new("x64-mingw32")
   end
 
   Platform.singleton_class.module_eval do

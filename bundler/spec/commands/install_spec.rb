b6c63a4abada48029626450275b0ca6d872d7515
Thong Kuah <tkuah@gitlab.com>
2022-11-07 14:44:04 +1300

    Fix checksum spec - racc is included in std lib since Ruby 3.0
    We switch to a different gem name to avoid this conflict

--- bundler/spec/commands/install_spec.rb
+++ bundler/spec/commands/install_spec.rb
@@ -892,16 +892,16 @@ RSpec.describe "bundle install with gem sources" do
   context "with missing platform specific gems in lockfile" do
     before do
       build_repo4 do
-        build_gem "racc", "1.5.2"
+        build_gem "racca", "1.5.2"
 
         build_gem "nokogiri", "1.12.4" do |s|
           s.platform = "x86_64-darwin"
-          s.add_runtime_dependency "racc", "~> 1.4"
+          s.add_runtime_dependency "racca", "~> 1.4"
         end
 
         build_gem "nokogiri", "1.12.4" do |s|
           s.platform = "x86_64-linux"
-          s.add_runtime_dependency "racc", "~> 1.4"
+          s.add_runtime_dependency "racca", "~> 1.4"
         end
 
         build_gem "crass", "1.0.6"
@@ -924,7 +924,7 @@ RSpec.describe "bundle install with gem sources" do
         c.repo_gem gem_repo4, "crass", "1.0.6"
         c.repo_gem gem_repo4, "loofah", "2.12.0"
         c.repo_gem gem_repo4, "nokogiri", "1.12.4", "x86_64-darwin"
-        c.repo_gem gem_repo4, "racc", "1.5.2"
+        c.repo_gem gem_repo4, "racca", "1.5.2"
       end
 
       lockfile <<-L
@@ -936,8 +936,8 @@ RSpec.describe "bundle install with gem sources" do
               crass (~> 1.0.2)
               nokogiri (>= 1.5.9)
             nokogiri (1.12.4-x86_64-darwin)
-              racc (~> 1.4)
-            racc (1.5.2)
+              racca (~> 1.4)
+            racca (1.5.2)
 
         PLATFORMS
           x86_64-darwin-20
@@ -969,7 +969,7 @@ RSpec.describe "bundle install with gem sources" do
         c.repo_gem gem_repo4, "loofah", "2.12.0"
         c.repo_gem gem_repo4, "nokogiri", "1.12.4", "x86_64-darwin"
         c.repo_gem gem_repo4, "nokogiri", "1.12.4", "x86_64-linux"
-        c.repo_gem gem_repo4, "racc", "1.5.2"
+        c.repo_gem gem_repo4, "racca", "1.5.2"
       end
 
       expect(lockfile).to eq <<~L
@@ -981,10 +981,10 @@ RSpec.describe "bundle install with gem sources" do
               crass (~> 1.0.2)
               nokogiri (>= 1.5.9)
             nokogiri (1.12.4-x86_64-darwin)
-              racc (~> 1.4)
+              racca (~> 1.4)
             nokogiri (1.12.4-x86_64-linux)
-              racc (~> 1.4)
-            racc (1.5.2)
+              racca (~> 1.4)
+            racca (1.5.2)
 
         PLATFORMS
           x86_64-darwin-20

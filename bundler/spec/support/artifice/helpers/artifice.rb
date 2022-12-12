f0626c512e360ca8737fe224ff1b3922cdf05793
David Rodríguez <deivid.rodriguez@riseup.net>
2022-12-12 19:59:49 +0100

    Remove unnecessary constant

--- bundler/spec/support/artifice/helpers/artifice.rb
+++ bundler/spec/support/artifice/helpers/artifice.rb
@@ -2,8 +2,6 @@
 
 # This module was initially borrowed from https://github.com/wycats/artifice
 module Artifice
-  NET_HTTP = ::Net::HTTP
-
   # Activate Artifice with a particular Rack endpoint.
   #
   # Calling this method will replace the Net::HTTP system
@@ -31,7 +29,7 @@ module Artifice
 
   # Deactivate the Artifice replacement.
   def self.deactivate
-    replace_net_http(Net_HTTP)
+    replace_net_http(::Net::HTTP)
   end
 
   def self.replace_net_http(value)

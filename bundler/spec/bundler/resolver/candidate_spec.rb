903efed711192fd82a41412e82f4b79470745435
David Rodríguez <deivid.rodriguez@riseup.net>
2023-01-05 20:09:35 +0100

    Fix incorrect version ranges for causing conflicts sometimes
    Dependency ranges were sometimes matching only the specific platform
    variant of a given version. When transforming dependency requirements
    into pub_grub version ranges, we need to make sure both the generic
    version and the platform specific version can potentially match the
    ranges we create.

--- bundler/spec/bundler/resolver/candidate_spec.rb
+++ bundler/spec/bundler/resolver/candidate_spec.rb
@@ -3,8 +3,8 @@
 RSpec.describe Bundler::Resolver::Candidate do
   it "compares fine" do
     version1 = described_class.new("1.12.5", :specs => [Gem::Specification.new("foo", "1.12.5") {|s| s.platform = Gem::Platform::RUBY }])
-    version2 = described_class.new("1.12.5")
+    version2 = described_class.new("1.12.5", :specs => [Gem::Specification.new("foo", "1.12.5") {|s| s.platform = Gem::Platform::X64_LINUX }])
 
-    expect(version1 >= version2).to be true
+    expect(version2 >= version1).to be true
   end
 end

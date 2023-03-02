7e022cb5e34a4f362aa3ec1fef43f26fb46cbcef
David Rodríguez <deivid.rodriguez@riseup.net>
2023-03-02 18:23:46 +0100

    Fix warnings about unused variable
    ```
    /path/to/rubygems/test/rubygems/test_gem_commands_exec_command.rb:180: warning: assigned but unused variable - platforms
    ```

--- test/rubygems/test_gem_commands_exec_command.rb
+++ test/rubygems/test_gem_commands_exec_command.rb
@@ -177,8 +177,6 @@ class TestGemCommandsExecCommand < Gem::TestCase
   end
 
   def test_gem_with_platform_dependencies
-    platforms = Gem.platforms.dup
-
     spec_fetcher do |fetcher|
       fetcher.download "a", 2 do |s|
         s.executables = %w[a]
@@ -217,8 +215,6 @@ class TestGemCommandsExecCommand < Gem::TestCase
 
   def test_gem_with_platform_and_platform_dependencies
     pend "extensions don't quite work on jruby" if Gem.java_platform?
-
-    platforms = Gem.platforms.dup
 
     spec_fetcher do |fetcher|
       fetcher.download "a", 2 do |s|

0915942661694ae696844f4c5b9d2876d9e7b936
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-03-10 14:22:08 +0900

    Added some of whitelisted gems for testing

--- test/rubygems/helper.rb
+++ test/rubygems/helper.rb
@@ -65,7 +65,7 @@ end
 
 class Gem::Specification < Gem::BasicSpecification
   remove_const :DEFAULT_GEMS_LIST
-  DEFAULT_GEMS_LIST = %w[systemgem default a b]
+  DEFAULT_GEMS_LIST = %w[systemgem default bundler a b c z]
 end
 
 ##

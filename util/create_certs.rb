74776bd20f7d8cd359ff6c5a01e601dc73c3dbea
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-03-17 18:36:42 +0900

    util/rubocop -A --only Layout/EmptyLineAfterMagicComment

--- util/create_certs.rb
+++ util/create_certs.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require "openssl"
 
 class CertificateBuilder
--- util/create_encrypted_key.rb
+++ util/create_encrypted_key.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require "openssl"
 
 test_path = File.expand_path("../test/rubygems", __dir__)

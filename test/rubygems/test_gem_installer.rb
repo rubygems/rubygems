e0321f56d3e68d1012b39f2b13e7e233941617a8
David Rodríguez <deivid.rodriguez@riseup.net>
2023-03-24 10:56:04 +0000

    Merge pull request #6533 from MSP-Greg/00-mswin-test-gem-ext-builder
    remove test skips/pends on mswin platform
    
    (cherry picked from commit 3586d716d0f8b6707afdafab37a6eb26033fffd2)

--- test/rubygems/test_gem_installer.rb
+++ test/rubygems/test_gem_installer.rb
@@ -1541,7 +1541,7 @@ end
 
   def test_install_extension_and_script
     pend "Makefile creation crashes on jruby" if Gem.java_platform?
-    pend if RUBY_PLATFORM.include?("mswin") && ENV.key?("GITHUB_ACTIONS") # not working from the beginning
+    pend "terminates on mswin" if vc_windows? && ruby_repo?
 
     @spec = setup_base_spec
     @spec.extensions << "extconf.rb"

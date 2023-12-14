ff5f3feef9a4738b06efb9f0aa058f26a92849bd
Samuel Giddins <segiddins@segiddins.me>
2023-12-14 16:35:19 +0800

    Print QR code for webauthn verification link

---
+++ lib/rubygems/vendor/rqrcode_core/lib/rqrcode_core/qrcode.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "./qrcode/qr_8bit_byte"
+require_relative "./qrcode/qr_alphanumeric"
+require_relative "./qrcode/qr_bit_buffer"
+require_relative "./qrcode/qr_code"
+require_relative "./qrcode/qr_math"
+require_relative "./qrcode/qr_multi"
+require_relative "./qrcode/qr_numeric"
+require_relative "./qrcode/qr_polynomial"
+require_relative "./qrcode/qr_rs_block"
+require_relative "./qrcode/qr_segment"
+require_relative "./qrcode/qr_util"

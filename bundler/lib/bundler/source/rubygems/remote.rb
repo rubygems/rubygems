f055b17f8c48b0d7f643068e0fae103c91dd728d
Martin Emde <martin.emde@gmail.com>
2024-01-03 17:44:18 -0800

    Update how the slug is created

--- bundler/lib/bundler/source/rubygems/remote.rb
+++ bundler/lib/bundler/source/rubygems/remote.rb
@@ -16,8 +16,8 @@ module Bundler
           @anonymized_uri = remove_auth(@uri).freeze
         end
 
-        MAX_CACHE_SLUG_SIZE = 255
-        private_constant :MAX_CACHE_SLUG_SIZE
+        MAX_CACHE_SLUG_HOST_SIZE = 255 - 1 - 32 # 255 minus dot minus MD5 length
+        private_constant :MAX_CACHE_SLUG_HOST_SIZE
 
         # @return [String] A slug suitable for use as a cache key for this
         #         remote.
@@ -31,13 +31,15 @@ module Bundler
             host = cache_uri.to_s.start_with?("file://") ? nil : cache_uri.host
 
             uri_parts = [host, cache_uri.user, cache_uri.port, cache_uri.path]
-            uri_digest = SharedHelpers.digest(:MD5).hexdigest(uri_parts.compact.join("."))
+            uri_parts.compact!
+            uri_digest = SharedHelpers.digest(:MD5).hexdigest(uri_parts.join("."))
 
-            uri_parts_max = MAX_CACHE_SLUG_SIZE - uri_digest.size - 1
-            shortened_uri_parts = uri_parts[0..-2].compact.join(".")[0..uri_parts_max - 1]
-            return uri_digest if shortened_uri_parts.empty?
+            uri_parts.pop
+            host_parts = uri_parts.join(".")
+            return uri_digest if host_parts.empty?
 
-            [shortened_uri_parts, uri_digest].join(".")
+            shortened_host_parts = host_parts[0...MAX_CACHE_SLUG_HOST_SIZE]
+            [shortened_host_parts, uri_digest].join(".")
           end
         end
 

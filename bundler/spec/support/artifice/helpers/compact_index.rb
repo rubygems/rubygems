8039d23b05b4a6b39275df7ac8e796e7022f8f16
David Rodríguez <deivid.rodriguez@riseup.net>
2024-05-21 18:22:10 +0200

    Avoid line conversions when generating test server responses
    To support Windows.

--- bundler/spec/support/artifice/helpers/compact_index.rb
+++ bundler/spec/support/artifice/helpers/compact_index.rb
@@ -68,7 +68,7 @@ class CompactIndexAPI < Endpoint
       @gems[gem_repo] ||= begin
         specs = Bundler::Deprecate.skip_during do
           %w[specs.4.8 prerelease_specs.4.8].map do |filename|
-            Marshal.load(File.open(gem_repo.join(filename)).read).map do |name, version, platform|
+            Marshal.load(File.binread(gem_repo.join(filename))).map do |name, version, platform|
               load_spec(name, version, platform, gem_repo)
             end
           end.flatten

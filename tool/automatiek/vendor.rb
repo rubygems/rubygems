3d039780ad45ab21fdaf99a0040ab21ddbcc2659
David Rodríguez <deivid.rodriguez@riseup.net>
2024-12-11 19:24:42 +0100

    Bump vendored securerandom to 0.4.0

--- tool/automatiek/vendor.rb
+++ tool/automatiek/vendor.rb
@@ -78,19 +78,6 @@ VendoredGem = Struct.new(:name, :extra_dependencies, :namespace, :prefix, :vendo
   end
 end
 
-class VendoredSecureRandom < VendoredGem
-  def namespace_file(file, contents, namespace)
-    contents = super(file, contents, namespace)
-
-    contents.gsub!(/Random::/, "#{prefix}::Random::")
-
-    entrypoint = "random"
-    contents.gsub!(/^require (["'])#{Regexp.escape entrypoint}/, "require_relative \\1#{relative_require_target_from(file, "random")}")
-    contents.gsub!(/require (["'])#{Regexp.escape entrypoint}/, "require \\1#{require_target}/#{entrypoint}")
-    contents
-  end
-end
-
 ignore = ["bundler"]
 
 vendored_gems = [
@@ -101,7 +88,7 @@ vendored_gems = [
   VendoredGem.new(name: "net-protocol", namespace: "Net", prefix: "Gem", vendor_lib: "lib/rubygems/vendor/net-protocol", license_path: "LICENSE.txt"),
   VendoredGem.new(name: "optparse", namespace: "OptionParser", prefix: "Gem", vendor_lib: "lib/rubygems/vendor/optparse", license_path: "COPYING", extra_dependencies: %w[uri/lib/rubygems/vendor/uri], patch_name: "optparse-v0.4.0.patch"),
   VendoredGem.new(name: "resolv", namespace: "Resolv", prefix: "Gem", vendor_lib: "lib/rubygems/vendor/resolv", license_path: "COPYING", extra_dependencies: %w[securerandom/lib/rubygems/vendor/securerandom timeout], patch_name: "resolv-v0.4.0.patch"),
-  VendoredSecureRandom.new(name: "securerandom", namespace: "SecureRandom", prefix: "Gem", vendor_lib: "lib/rubygems/vendor/securerandom", license_path: "COPYING"),
+  VendoredGem.new(name: "securerandom", namespace: "SecureRandom", prefix: "Gem", vendor_lib: "lib/rubygems/vendor/securerandom", license_path: "COPYING"),
   VendoredGem.new(name: "timeout", namespace: "Timeout", prefix: "Gem", vendor_lib: "lib/rubygems/vendor/timeout", license_path: "COPYING", patch_name: "timeout-v0.4.1.patch"),
   VendoredGem.new(name: "tsort", namespace: "TSort", prefix: "Gem", vendor_lib: "lib/rubygems/vendor/tsort", license_path: "LICENSE.txt"),
   VendoredGem.new(name: "uri", namespace: "URI", prefix: "Gem", vendor_lib: "lib/rubygems/vendor/uri", license_path: "COPYING"),
@@ -112,7 +99,7 @@ vendored_gems = [
   VendoredGem.new(name: "thor", namespace: "Thor", prefix: "Bundler", vendor_lib: "bundler/lib/bundler/vendor/thor", license_path: "LICENSE.md", patch_name: "thor-v1.3.0.patch"),
   VendoredGem.new(name: "tsort", namespace: "TSort", prefix: "Bundler", vendor_lib: "bundler/lib/bundler/vendor/tsort", license_path: "LICENSE.txt"),
   VendoredGem.new(name: "uri", namespace: "URI", prefix: "Bundler", vendor_lib: "bundler/lib/bundler/vendor/uri", license_path: "COPYING"),
-  VendoredSecureRandom.new(name: "securerandom", namespace: "SecureRandom", prefix: "Bundler", vendor_lib: "bundler/lib/bundler/vendor/securerandom", license_path: "COPYING"),
+  VendoredGem.new(name: "securerandom", namespace: "SecureRandom", prefix: "Bundler", vendor_lib: "bundler/lib/bundler/vendor/securerandom", license_path: "COPYING"),
 ].group_by(&:name)
 
 Bundler.definition.resolve.materialized_for_all_platforms.reject {|s| ignore.include?(s.name) }.each do |s|

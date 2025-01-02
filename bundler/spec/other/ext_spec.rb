df9473e0254978c98ea55c5dd6128cb5a2bf4a22
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2025-01-02 23:15:59 +0900

    Make mingw generic

--- bundler/spec/other/ext_spec.rb
+++ bundler/spec/other/ext_spec.rb
@@ -45,8 +45,13 @@ RSpec.describe "Bundler::GemHelpers#generic" do
     expect(generic(pl("x86_64-mingw32"))).to eq(pl("x64-mingw32"))
   end
 
-  it "converts 64-bit mingw UCRT platform variants into x64-mingw-ucrt" do
-    expect(generic(pl("x64-mingw-ucrt"))).to eq(pl("x64-mingw-ucrt"))
+  it "converts 64-bit mingw UCRT platform variants into mingw" do
+    expect(generic(pl("x64-mingw-ucrt"))).to eq(pl("mingw"))
+  end
+
+  it "converts aarch64 mingw platform variants into mingw" do
+    expect(generic(pl("aarch64-mingw"))).to eq(pl("mingw"))
+    expect(generic(pl("aarch64-mingw-ucrt"))).to eq(pl("mingw"))
   end
 end
 

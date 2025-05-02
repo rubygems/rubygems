040ce257046db70269bc44e3d4c45a8f4a447e79
Randy Stauner <randy.stauner@shopify.com>
2025-05-02 10:05:10 -0700

    Validate dependencies when doing bundle install

--- bundler/lib/bundler/definition.rb
+++ bundler/lib/bundler/definition.rb
@@ -66,8 +66,10 @@ module Bundler
         @unlocking_ruby = false
         @explicit_unlocks = []
         conservative = false
+        strict = false
       else
         @unlocking_all = false
+        strict = unlock.delete(:strict)
         @unlocking_bundler = unlock.delete(:bundler)
         @unlocking = unlock.any? {|_k, v| !Array(v).empty? }
         @sources_to_unlock = unlock.delete(:sources) || []
@@ -97,7 +99,7 @@ module Bundler
 
       if lockfile_exists?
         @lockfile_contents = Bundler.read_file(lockfile)
-        @locked_gems = LockfileParser.new(@lockfile_contents)
+        @locked_gems = LockfileParser.new(@lockfile_contents, strict: strict)
         @locked_platforms = @locked_gems.platforms
         @most_specific_locked_platform = @locked_gems.most_specific_locked_platform
         @platforms = @locked_platforms.dup

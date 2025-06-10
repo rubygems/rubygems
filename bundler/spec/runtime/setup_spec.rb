1188554710d2493b48a2a24e86366677d97c05b5
David Rodríguez <deivid.rodriguez@riseup.net>
2025-06-10 16:11:57 +0200

    Skip to Bundler 4 directly

--- bundler/spec/runtime/setup_spec.rb
+++ bundler/spec/runtime/setup_spec.rb
@@ -1540,7 +1540,7 @@ end
       expect(out).to eq("myrack-1.0.0")
     end
 
-    it "keeps Kernel#gem private", bundler: "3" do
+    it "keeps Kernel#gem private", bundler: "4" do
       install_gemfile <<-G
         source "https://gem.repo1"
         gem "myrack"

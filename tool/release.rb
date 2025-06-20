3992b6d9c384f52df430f632108550bc89f7d987
David Rodríguez <deivid.rodriguez@riseup.net>
2025-06-20 18:54:21 +0200

    Fix running `dbundle` alias in bundler 4 mode
    In Bundler 4, running `bundle` only should print a help text rather than
    installing gems. This works if Bundler is installed normally, but not in
    development when running using the `bundler/spec/support/bundle.rb` script.
    
    This is because building the help text relies on the executable name to
    be `bundle$` while `bundler/spec/support/bundle.rb` does not match that
    name.
    
    This commit fixes the issue by moving `bundler/spec/support/bundle.rb`
    to a `bundler/bin/bundle` binstub. This has the expected name and also
    seems more standard and it's closer to running an "installed Bundler".

--- tool/release.rb
+++ tool/release.rb
@@ -111,10 +111,8 @@ class Release
 
   def self.install_dependencies!
     system(
-      "ruby",
-      "-I",
-      File.expand_path("../lib", __dir__),
-      File.expand_path("../bundler/spec/support/bundle.rb", __dir__),
+      { "RUBYOPT" => "-I#{File.expand_path("../lib", __dir__)}" },
+      File.expand_path("../bundler/bin/bundle", __dir__),
       "install",
       "--gemfile=#{File.expand_path("bundler/release_gems.rb", __dir__)}",
       exception: true

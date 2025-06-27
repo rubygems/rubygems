dfd7287f18686da8049a8144f81b10883e06af9c
David Rodríguez <deivid.rodriguez@riseup.net>
2025-06-27 12:16:31 +0200

    Remove `print_only_version_number` setting
    I don't think it makes sense to make this tiny behavior change
    configurable. If someone wants to parse version output, and we have a
    public setting, they are going to need to accommodate their regexps to
    both values of the setting.
    
    In addition to this, I plan to enhance version output with a note about
    "simulated version", and in that case, "print_only_version_number" would
    no longer hold, since what we print will be more than that anyways.
    
    So, I'd like to remove the setting and change the output in Bundler 4
    with no way to opt out.

--- bundler/lib/bundler/settings.rb
+++ bundler/lib/bundler/settings.rb
@@ -39,7 +39,6 @@ module Bundler
       path.system
       plugins
       prefer_patch
-      print_only_version_number
       setup_makes_kernel_gem_public
       silence_deprecations
       silence_root_warning

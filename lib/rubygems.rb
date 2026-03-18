cb2f39b4e6004a4230fd63667786ee2aa36ac60c
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2026-03-19 07:35:24 +0900

    Merge pull request #9409 from BurdetteLamar/link_fix
    [DOC] Fix link
    
    (cherry picked from commit 0e9dd7b322e646482f77ab63193d2ac9deb44023)

--- lib/rubygems.rb
+++ lib/rubygems.rb
@@ -37,7 +37,7 @@ require_relative "rubygems/win_platform"
 # Further RubyGems documentation can be found at:
 #
 # * {RubyGems Guides}[https://guides.rubygems.org]
-# * {RubyGems API}[https://www.rubydoc.info/github/ruby/rubygems] (also available from
+# * {RubyGems API}[https://guides.rubygems.org/rubygems-org-api/] (also available from
 #   <tt>gem server</tt>)
 #
 # == RubyGems Plugins
